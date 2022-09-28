7db0b5497e6e3679acadace7a6ab3e29853deb6e
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2022-09-28 22:50:52 +0300

    First HW for 28/09

---
+++ src/main/java/students/student_igor_budnitski/Task11.java
@@ -0,0 +1,9 @@
+package students.student_igor_budnitski;
+
+public class Task11 {
+    public static void main(String[] args) {
+        int x = 74;
+        int y = 36;
+        System.out.println("Sum of " + x + " and " + y + " = " + x+y);
+    }
+}
---
+++ src/main/java/students/student_igor_budnitski/Task12.java
@@ -0,0 +1,9 @@
+package students.student_igor_budnitski;
+
+public class Task12 {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 3;
+        System.out.println("Multiplication of " + x + " and " + y + " = " + x*y);
+    }
+}
