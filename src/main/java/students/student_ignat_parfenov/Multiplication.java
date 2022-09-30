f200b2e82ba8fe6d4146641439bbec1ba35502a4
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-09-30 15:43:33 +0300

    initial commit

---
+++ src/main/java/students/student_ignat_parfenov/Multiplication.java
@@ -0,0 +1,10 @@
+package students.student_ignat_parfenov;
+
+public class Multiplication {public static void main(String[] args) {
+    int a = 64;
+    int b = 13;
+    int c = a * b;
+    System.out.println("Multiplication result = " + c);
+
+}
+}
