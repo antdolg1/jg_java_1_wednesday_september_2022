8387342fc6e4438b2b4a85f683edcc0ec636c57f
ArtGobl1n <artjoms122@gmail.com>
2022-09-30 14:12:04 +0300

    Task_11

---
+++ src/main/java/students/student_artjoms_trosihins/Multiplication.java
@@ -0,0 +1,10 @@
+package students.student_artjoms_trosihins;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int x = 29;
+        int y = 30;
+        int sum = x * y;
+        System.out.println(sum);
+    }
+}
