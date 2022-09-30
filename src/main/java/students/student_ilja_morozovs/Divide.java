5b9cfa2da15ff8237f8696038abeba3b59f06c0c
iljamorozovs <ilja.morozovs@gmail.com>
2022-09-30 10:05:29 +0300

    Done all tasks

---
+++ src/main/java/students/student_ilja_morozovs/Divide.java
@@ -0,0 +1,12 @@
+package students.student_ilja_morozovs;
+
+public class Divide {
+    private int a=50;
+    private int b=3;
+
+    public static void main(String[] args) {
+        Divide divide=new Divide();
+        int c=divide.a/divide.b;
+        System.out.println("Expected Output: "+c);
+    }
+}
---
+++ src/main/java/students/student_ilja_morozovs/Greeting.java
@@ -0,0 +1,7 @@
+package students.student_ilja_morozovs;
+
+public class Greeting {
+    public static void main(String[] args) {
+    System.out.println("Hello world");
+    }
+}
