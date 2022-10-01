08616c4fc80c2768b14bae1356ae5f78b9b10678
TheStewe <s.veveris@gmail.com>
2022-10-01 21:22:15 +0300

    initial commit

---
+++ src/main/java/students/student_stivens_veveris/hello_world.java
@@ -0,0 +1,14 @@
+package students.student_stivens_veveris;
+
+public class hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+
+    }
+
+
+
+}
