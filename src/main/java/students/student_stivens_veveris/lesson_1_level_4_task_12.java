5796f043a7e518354a0a2405438069b4e4ec8de4
TheStewe <s.veveris@gmail.com>
2022-10-01 21:41:19 +0300

    initial commit

---
+++ src/main/java/students/student_stivens_veveris/lesson_1_level_4_task_12.java
@@ -0,0 +1,19 @@
+package students.student_stivens_veveris;
+
+public class lesson_1_level_4_task_12 {
+
+    public static void main(String[] args) {
+
+        int num1 = 50;
+        int num2 = 3;
+
+        int multiplication = num1 * num2;
+
+        System.out.println("Numbers multiplication: " + multiplication);
+
+
+
+
+
+    }
+}
