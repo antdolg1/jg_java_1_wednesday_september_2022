8b23c7de51ef14dec9098101ab025f06bb0f0e8e
Antons Dolgopolovs <antdolg@gmail.com>
2022-10-02 21:48:27 +0300

    Test build

---
+++ src/main/java/teacher/lesson_1/Test.java
@@ -0,0 +1,14 @@
+package teacher.lesson_1;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        int number = 12.2;
+
+        System.out.println(number);
+
+    }
+
+}
