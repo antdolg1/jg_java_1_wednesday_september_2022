384d3ad6c87b05035cbbcf6318fad94668291542
viktors1989 <viktors@4k.lv>
2022-10-02 20:21:43 +0300

    All ok

--- src/main/java/students/students_viktors_matjukovskis/level_4_task_11.java
+++ src/main/java/students/students_viktors_matjukovskis/level_4_task_11.java
@@ -5,10 +5,23 @@ import java.util.Scanner;
 public class level_4_task_11 {
     public static void main(String[]args) {
 
-        short num1 = 45, num2 =76;
+        short num1 = 74, num2 =36;
         int res = num1 + num2;
-        System.out.println(num1 + "+" + num2 + "=" + res);
+        System.out.println(num1 + " + " + num2 + " = " + res);
+        System.out.println("Результат: " + res);
+        System.out.println("___________________");
 
-        System.out.println("result: " + res);
+        short num3 = 3, num4 =50;
+        int res2 = num3 * num4;
+        System.out.println(num3 + " * " + num4 + " = " + res2);
+        System.out.println("Результат: " + res2);
+        System.out.println("___________________");
+
+        short num5 = 50, num6 =3;
+        int res3 = num5 / num6;
+        System.out.println(num5 + " * " + num6 + " = " + res3);
+        System.out.println("Результат: " + res3);
+        System.out.println("___________________");
     }
+
 }
