412d973ea880becf43a9ae362f28003d22416cae
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-03 16:51:41 +0200

    Sum

---
+++ src/main/java/students/student_jurij_hlebnikov/Sum.java
@@ -0,0 +1,11 @@
+package students.student_jurij_hlebnikov;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 88;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+
+}
