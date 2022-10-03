c7f66dd90a36c5b01eb9f0ce37c75afc7ca779fc
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-03 16:55:01 +0200

    Multiply

---
+++ src/main/java/students/student_jurij_hlebnikov/Multiply.java
@@ -0,0 +1,10 @@
+package students.student_jurij_hlebnikov;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 67;
+        int b = 83;
+        int result = a * b;
+        System.out.println(result);
+    }
+}
