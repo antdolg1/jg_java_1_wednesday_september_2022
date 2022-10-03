65648672c9e971e51e125a93cbe4c3b426ba9e0a
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-03 13:04:34 +0200

    home work

--- src/main/java/students/student_jurij_hlebnikov/hello_world.java
+++ src/main/java/students/student_jurij_hlebnikov/hello_world.java
@@ -3,6 +3,9 @@ package students.student_jurij_hlebnikov;
 public class hello_world {
      public static void main(String[] args) {
     System.out.println("Hello World!");
-
+         int a = 20;
+         int b = 11;
+         System.out.println(a+b);
+         System.out.println(a*b);
     }
 }
