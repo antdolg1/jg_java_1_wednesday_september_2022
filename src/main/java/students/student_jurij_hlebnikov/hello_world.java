957ba7a38393c093e66e4115dc1127c417e6b6ab
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-03 12:50:03 +0200

    initial commit home work

---
+++ src/main/java/students/student_jurij_hlebnikov/hello_world.java
@@ -0,0 +1,11 @@
+package students.student_jurij_hlebnikov;
+
+public class hello_world {
+     public static void main(String[] args) {
+    System.out.println("Hello World!");
+    int a = 20;
+    int b = 11;
+    System.out.println(a+b);
+    System.out.println(a*b);
+    }
+}
