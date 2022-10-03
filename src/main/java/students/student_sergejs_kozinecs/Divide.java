752727269f5632f95923b1351ac6f95ce84a56e0
Sergejs Kozinecs <sergei.kozinecs@gmail.com>
2022-10-03 22:26:16 +0300

    Homework tasks

---
+++ src/main/java/students/student_sergejs_kozinecs/Divide.java
@@ -0,0 +1,12 @@
+package students.student_sergejs_kozinecs;
+
+public class Divide {
+    private static int divide(int x, int y) {
+        return x / y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(divide(12, 6));
+        System.out.println(divide(4, 9));
+    }
+}
---
+++ src/main/java/students/student_sergejs_kozinecs/Multiply.java
@@ -0,0 +1,12 @@
+package students.student_sergejs_kozinecs;
+
+public class Multiply {
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply(3, 50));
+        System.out.println(multiply(4, 9));
+    }
+}
