da3b155ac6252835520b77c7ebb9be2fa9350455
Evroleg <evroleg@gmail.com>
2022-10-04 12:14:13 +0300

    initial commit 2 Program

---
+++ src/main/java/students/student_oleg_gryazev/Math.java
@@ -0,0 +1,23 @@
+package students.student_oleg_gryazev;
+
+ class Math {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+        int c;
+
+        System.out.println("Result 1 = " + (c = a/b));
+       int z = 50;
+       int y = 3;
+       int x;
+       System.out.println("Result 2 = " + (x = z/y));}
+
+
+
+
+
+
+
+
+}
