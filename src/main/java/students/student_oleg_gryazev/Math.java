6bc1a8f51d6047af4e767dd6572548e3ed2e435a
Evroleg <evroleg@gmail.com>
2022-10-04 14:58:03 +0300

    initial commit lesson 11

--- src/main/java/students/student_oleg_gryazev/Math.java
+++ src/main/java/students/student_oleg_gryazev/Math.java
@@ -1,23 +1,35 @@
 package students.student_oleg_gryazev;
 
- class Math {
+ public class Math {
+     int summa(int var1, int var2) {
+         int result = var1 + var2;
+         return result;
+     }
+    int divide(int var3, int var4){
+         int result1 = var3/var4;
+         return result1;
+     }
+ }
 
-    public static void main(String[] args) {
-        int a = 20;
-        int b = 4;
-        int c;
+ class TestMath{
+     public static void main(String[] args) {
+         Math sum = new Math();
+         int finSum = sum.summa(90,10);
+         System.out.println(finSum);
+         System.out.println(sum.summa(45,21));
 
-        System.out.println("Result 1 = " + (c = a/b));
-       int z = 50;
-       int y = 3;
-       int x;
-       System.out.println("Result 2 = " + (x = z/y));}
-
-
-
+         Math div = new Math();
+         int finDiv = div.divide(50,3);
+         System.out.println(finDiv);
+         System.out.println(div.divide(50,3));
 
 
+     }
+
+    }
 
 
 
-}
+
+
+
