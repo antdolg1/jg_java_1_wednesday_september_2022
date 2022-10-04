908108235306830d2d104e6759471a466985df6b
OskarsP <oskars@popens.lv>
2022-10-04 10:43:32 +0300

    Initial commit

---
+++ src/main/java/students/student_oskars_popens/Summ.java
@@ -0,0 +1,10 @@
+package students.student_oskars_popens;
+
+public class Summ {
+    public static void main(String[] args) {
+        int aa = 55;
+        int bb = 75;
+
+        System.out.println(aa+bb);
+    }
+}
