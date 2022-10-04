f1fbc1bb09bf806e1b387ce4ce9ff91adcb299bf
Vlad-Rix <vladislavs.saksins@ya.ru>
2022-10-04 22:23:29 +0300

    Initial commit

---
+++ src/main/java/students/student_vladislav_saksin/Divison.java
@@ -0,0 +1,10 @@
+package students.student_vladislav_saksin;
+
+public class Divison {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 3;
+        int c = a / b;
+        System.out.println(c);
+    }
+}
