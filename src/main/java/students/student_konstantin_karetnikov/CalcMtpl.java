f98653aa1df176c0cd0ca4473857ca0222ed9e55
konstantinkaretnikov <kskaretnikov@gmail.com>
2022-10-04 17:44:25 +0300

    HaloWorld and calculators done

---
+++ src/main/java/students/student_konstantin_karetnikov/CalcMtpl.java
@@ -0,0 +1,14 @@
+package students.student_konstantin_karetnikov;
+
+public class CalcMtpl {
+
+    public static void main(String[] args) {
+
+        int a = 50;
+        int b = 3;
+
+        System.out.println(a*b);
+
+    }
+}
+
---
+++ src/main/java/students/student_konstantin_karetnikov/CalcSum.java
@@ -0,0 +1,14 @@
+package students.student_konstantin_karetnikov;
+
+public class CalcSum {
+
+    public static void main(String[] args) {
+
+        int a = 74;
+        int b = 36;
+
+        System.out.println(a+b);
+
+    }
+}
+
