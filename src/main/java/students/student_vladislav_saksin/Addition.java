ac7911b65aef3e618e8be4ec6a37935d1bf4b0a7
Vlad-Rix <vladislavs.saksins@ya.ru>
2022-10-04 22:16:32 +0300

    Initial commit

---
+++ src/main/java/students/student_vladislav_saksin/Addition.java
@@ -0,0 +1,10 @@
+package students.student_vladislav_saksin;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
