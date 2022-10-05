d1c035d2ebe838a7c488c291d67556cd329f9568
OkeyStar <dmitry.sochkov@gmail.com>
2022-10-05 22:52:35 +0300

    Add lessoncode

---
+++ src/main/java/students/student_dmitrijs_sockovs/ArithmeticOperators.java
@@ -0,0 +1,21 @@
+package students.student_dmitrijs_sockovs;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int age = 18;
+
+        int weight;
+        weight = 18;
+        weight = 70;
+
+        String firstName = "John";
+
+        System.out.println("HelloWorld!" + " This is my first Java program!");
+        System.out.println("Hello, my name is " + firstName + "." );
+        System.out.println("I am " + age + " years old.");
+        System.out.println("---------------------------------------------");
+        System.out.println("Hello, my name is " + firstName + "." + "\nI am " + age + " age old.");
+        System.out.println("My weight is " + weight + " kg.");
+    }
+}
---
+++ src/main/java/students/student_dmitrijs_sockovs/PrimitiveVariables.java
@@ -0,0 +1,30 @@
+package students.student_dmitrijs_sockovs;
+
+public class PrimitiveVariables {
+
+    public static void  main(String[] args) {
+
+        byte myByte = 1;
+        short myShort = 12;
+        int number = 100;
+        long myLong = 1000000;
+
+        float myFloat = 12.0F;
+        float myFloatSecond = 12.0f;
+
+        double myDouble = 13.00303030;
+
+        char myChar = '0';
+        char myCharSecond = '\u20ac';
+
+        boolean myBooleanTrue = true;
+        boolean myBooleanFalse = false;
+
+        //camelCase
+        //PascalCase
+        //UPPERCASE
+        //lowercase
+        //snake_case
+        //kebab-case
+    }
+}
