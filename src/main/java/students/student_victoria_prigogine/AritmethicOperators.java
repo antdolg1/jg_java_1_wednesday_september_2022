def82520308d1adebd41f9224ac00a4e404f7766
alluvvi <vprigogine@gmail.com>
2022-10-05 22:01:10 +0300

    Add lessoncode

---
+++ src/main/java/students/student_victoria_prigogine/AritmethicOperators.java
@@ -0,0 +1,26 @@
+package students.student_victoria_prigogine;
+
+public class AritmethicOperators {
+
+    public static void main(String[] args) {
+
+        int age = 18;
+
+        int weight;
+        weight = 18;
+
+        String firstName = "John";
+
+        System.out.println("HelloWorld!" + " This is my first Java program!");
+        System.out.println();
+        System.out.print("Hello, my name is " + firstName + ". I am " + age + " years old.");
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/students/student_victoria_prigogine/ScannerDemo.java
@@ -0,0 +1,24 @@
+package students.student_victoria_prigogine;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+    public static void main(String[] args) {
+
+        int userNumber;
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter any number: ");
+        userNumber = scanner.nextInt();
+        System.out.println("______________");
+
+        System.out.println("User entered number: " + userNumber);
+
+
+
+    }
+
+}
+
