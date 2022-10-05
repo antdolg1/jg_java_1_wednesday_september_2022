ac199badb8a45c4630bbcc8846fa42e353af0940
Manakids <2maxx2@inbox.lv>
2022-10-05 10:59:48 +0300

    Homework_1_lesson

---
+++ src/main/java/students/student_maksims_cerkasovs/Task_11.java
@@ -0,0 +1,10 @@
+package students.student_maksims_cerkasovs;
+
+public class Task_11 {
+
+    public static void main(String[] args) {
+        System.out.println(74 + 36);
+
+        System.out.println(50 / 3);
+    }
+}
---
+++ src/main/java/students/student_maksims_cerkasovs/Task_12.java
@@ -0,0 +1,7 @@
+package students.student_maksims_cerkasovs;
+
+public class Task_12 {
+    public static void main(String[] args) {
+        System.out.println(50 * 3);
+    }
+}
