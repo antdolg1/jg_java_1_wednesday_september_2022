83bc73f4de43c0898ff9a5a6df08d01e3861050d
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-07 21:45:40 +0300

    Update README.md

---
+++ src/main/java/students/student_ignat_parfenov/lesson2_hw_level2_intern/Phone.java
@@ -0,0 +1,12 @@
+package students.student_ignat_parfenov.lesson2_hw_level2_intern;
+
+public class Phone {
+
+    public int number;
+    public String model;
+    public int weight;
+
+    public static void main(String[] args) {
+
+    }
+}
