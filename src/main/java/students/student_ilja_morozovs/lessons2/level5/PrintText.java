c29e13c151d6230e1c7212e1aebbf00aab8fcec5
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-07 12:27:28 +0300

    Add level 5 programs

---
+++ src/main/java/students/student_ilja_morozovs/lessons2/level5/PrintText.java
@@ -0,0 +1,7 @@
+package students.student_ilja_morozovs.lessons2.level5;
+
+public class PrintText {
+    public static void main(String[] args) {
+        System.out.print("text "+"text"+" text");
+    }
+}
