5607fc8322c4d5eb0170c2e4279c613a88ad8ded
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-07 21:40:49 +0300

    Update README.md

--- src/main/java/students/student_ignat_parfenov/lesson2_hw_level2_intern/Book.java
+++ src/main/java/students/student_ignat_parfenov/lesson2_hw_level2_intern/Book.java
@@ -1,4 +1,12 @@
 package students.student_ignat_parfenov.lesson2_hw_level2_intern;
 
 public class Book {
+
+    private String title;
+    private int pages;
+
+    public static void main(String[] args) {
+
+    }
 }
+
