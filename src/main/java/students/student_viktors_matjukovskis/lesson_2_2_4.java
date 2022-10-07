c23db2dfaae00adbc7d50640684167a81a44af16
viktors1989 <viktors@4k.lv>
2022-10-08 00:18:41 +0300

    Home work

---
+++ src/main/java/students/student_viktors_matjukovskis/lesson_2_2_4.java
@@ -0,0 +1,6 @@
+package students.student_viktors_matjukovskis;
+
+public class lesson_2_2_4 {
+    static class Phone {
+    }
+}
---
+++ src/main/java/students/student_viktors_matjukovskis/lesson_2_2_6.java
@@ -0,0 +1,9 @@
+package students.student_viktors_matjukovskis;
+
+public class lesson_2_2_6 {
+    static class LoginService {
+        public static void main(String[] args) {
+            System.out.println("You can login");
+        }
+    }
+}
