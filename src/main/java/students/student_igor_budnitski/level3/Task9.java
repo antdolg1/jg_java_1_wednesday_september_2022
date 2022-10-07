db4874d63fdac78fe679e983350ca698426e2cf4
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2022-10-07 15:41:23 +0300

    Lesson2 - level3 homework

---
+++ src/main/java/students/student_igor_budnitski/level3/Task9.java
@@ -0,0 +1,19 @@
+package students.student_igor_budnitski.level3;
+
+import java.util.Scanner;
+
+/*
+* Напишите программу, которая запрашивает у пользователя целое число
+и печатает на консоль таблицу умножения для этого числа до 10.
+* */
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the number from 1 to 10: ");
+        int number = scanner.nextInt();
+        for (int i = 1; i < 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
