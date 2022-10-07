ff544a6125886839b992c205dae93e912dcb120f
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-07 22:12:40 +0300

    Update README.md

---
+++ src/main/java/students/student_ignat_parfenov/lesson2_hw_level2_intern/Task8.java
@@ -0,0 +1,14 @@
+package students.student_ignat_parfenov.lesson2_hw_level2_intern;
+
+import javax.naming.Name;
+import java.util.Scanner;
+
+public class Task8 {public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(" Enter your name please :");
+    String yourName = scanner.next();
+
+    System.out.println("Hello, " + yourName + "!");
+}
+}
