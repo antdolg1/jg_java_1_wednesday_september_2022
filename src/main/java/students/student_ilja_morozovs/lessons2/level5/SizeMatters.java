690f3a23549bc6df33a9255c87b35b744848e83d
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-07 12:27:28 +0300

    Add level 5 programs

---
+++ src/main/java/students/student_ilja_morozovs/lessons2/level5/SizeMatters.java
@@ -0,0 +1,8 @@
+package students.student_ilja_morozovs.lessons2.level5;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
