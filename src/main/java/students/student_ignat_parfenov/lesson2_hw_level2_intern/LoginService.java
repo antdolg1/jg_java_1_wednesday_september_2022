34a96da560292e182543b7f47b841b85d67fed15
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-07 21:56:21 +0300

    Update README.md

---
+++ src/main/java/students/student_ignat_parfenov/lesson2_hw_level2_intern/LoginService.java
@@ -0,0 +1,10 @@
+package students.student_ignat_parfenov.lesson2_hw_level2_intern;
+
+public class LoginService {
+    
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+
+}
