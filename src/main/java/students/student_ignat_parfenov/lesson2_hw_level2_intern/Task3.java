e110cb27eb319150fe39b1d265a5b8172ecde63b
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-07 20:13:10 +0300

    Update README.md

--- src/main/java/students/student_ignat_parfenov/lesson2_hw_level2_intern/Task3.java
+++ src/main/java/students/student_ignat_parfenov/lesson2_hw_level2_intern/Task3.java
@@ -2,5 +2,32 @@ package students.student_ignat_parfenov.lesson2_hw_level2_intern;
 
 public class Task3 {public static void main(String[] args){
 
+    class Book {
+        private int pages;
+        private String name;
+        private float weight;
+
+        public void getInfoBook () {
+            System.out.print("This book " + name + "consists of " + pages + " pages ");
+            System.out.print("While it weighs in a " + weight + "pounds!");
+            System.out.println("");
+        }
+
+        Book (int pages, float weight) {
+            this.pages = pages;
+            this.weight = weight;
+        }
+
+        Book(int pages, float weight, String name) {
+            this.pages = pages;
+            this.weight = weight;
+            this.name = name;
+        }
+
+        int getPages = 154;
+        float getWeight = 5.75f;
+        String getName = "Apocalypse now";
+
+    }
 }
 }
