3be3fa28185c5b5014755d7552367f5ea68be8af
vasjache <vcherenkoff@gmail.com>
2022-10-08 23:21:52 +0300

    TwoNumberDivide

---
+++ src/main/java/students/student_vasilij_cherenkov/TwoNumbersDivide.java
@@ -0,0 +1,12 @@
+package students.student_vasilij_cherenkov;
+
+public class TwoNumbersDivide {
+    public static void main(String[] args) {
+        int a=50;
+        int b=3;
+        int c=a/b;
+        System.out.println( c );
+
+
+    }
+}
