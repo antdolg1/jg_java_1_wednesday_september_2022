3ba9ba93a30baa2f80ea0e071a056588453b8eb4
TheStewe <s.veveris@gmail.com>
2022-10-08 17:17:29 +0300

    level 5 commit)

---
+++ src/main/java/students/student_stivens_veveris/Task_15.java
@@ -0,0 +1,18 @@
+package students.student_stivens_veveris;
+
+import java.util.Scanner;
+
+public class Task_15 {
+
+    public static void main(String[] args) {
+
+        String login;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Write your login: ");
+        login = scanner.nextLine();
+        System.out.println("------------------");
+        System.out.print("Your login is: " + login);
+
+    }
+}
