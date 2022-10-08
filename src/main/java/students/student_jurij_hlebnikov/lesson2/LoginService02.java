d84502cb322f15e18e8f93e45c0597f273018167
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-08 17:19:17 +0200

    Task 7

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson2/LoginService02.java
@@ -0,0 +1,10 @@
+package students.student_jurij_hlebnikov.lesson2;
+
+public class LoginService02 {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+
+    }
+
+}
