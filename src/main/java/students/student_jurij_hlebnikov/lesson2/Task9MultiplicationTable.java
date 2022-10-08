d101708057d11ee2118fe82e10d3ba4d949a9a02
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-08 18:17:44 +0200

    Task 9

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson2/Task9MultiplicationTable.java
@@ -0,0 +1,41 @@
+package students.student_jurij_hlebnikov.lesson2;
+
+import java.util.Scanner;
+
+public class Task9MultiplicationTable {
+    public static void main(String[] args) {
+
+        System.out.println("Please Give me a Number");
+
+        Scanner scanner = new Scanner(System.in);
+        int userNumber = scanner.nextInt();
+        int x1 = userNumber * 1;
+        int x2 = userNumber * 2;
+        int x3 = userNumber * 3;
+        int x4 = userNumber * 4;
+        int x5 = userNumber * 5;
+        int x6 = userNumber * 6;
+        int x7 = userNumber * 7;
+        int x8 = userNumber * 8;
+        int x9 = userNumber * 9;
+        int x10 = userNumber * 10;
+
+        System.out.println(userNumber + " x 1 = " + x1);
+        System.out.println(userNumber + " x 2 = " + x2);
+        System.out.println(userNumber + " x 3 = " + x3);
+        System.out.println(userNumber + " x 4 = " + x4);
+        System.out.println(userNumber + " x 5 = " + x5);
+        System.out.println(userNumber + " x 6 = " + x6);
+        System.out.println(userNumber + " x 7 = " + x7);
+        System.out.println(userNumber + " x 8 = " + x8);
+        System.out.println(userNumber + " x 8 = " + x8);
+        System.out.println(userNumber + " x 9 = " + x9);
+        System.out.println(userNumber + " x 10 = " + x10);
+
+        System.out.println("Thank You");
+
+
+
+
+    }
+}
