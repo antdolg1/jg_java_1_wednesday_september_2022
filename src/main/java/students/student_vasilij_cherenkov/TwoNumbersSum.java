e6ca0d77807d7ac33cdfebe6c37da7d2fed98023
vasjache <vcherenkoff@gmail.com>
2022-10-08 23:00:01 +0300

    TwoNumberSum

---
+++ src/main/java/students/student_vasilij_cherenkov/TwoNumbersSum.java
@@ -0,0 +1,12 @@
+package students.student_vasilij_cherenkov;
+
+public class TwoNumbersSum {
+
+    public static void main(String[] args) {
+        int a=74;
+        int b=36;
+        int result=a+b;
+        System.out.println( result );
+
+    }
+}
