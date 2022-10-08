3c26741eda10bdb20eb966bd9d3ac4e9e7ac3af0
vasjache <vcherenkoff@gmail.com>
2022-10-08 23:32:00 +0300

    TwoNumbersMultiplication

---
+++ src/main/java/students/student_vasilij_cherenkov/TwoNunumbersMultiplication.java
@@ -0,0 +1,11 @@
+package students.student_vasilij_cherenkov;
+
+public class TwoNunumbersMultiplication {
+    public static void main(String[] args) {
+        int numOne=50;
+        int numTwo=3;
+        int result=numOne*numTwo;
+        System.out.println(result);
+    }
+
+}
