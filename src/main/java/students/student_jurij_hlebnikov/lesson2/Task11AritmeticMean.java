aa23e495fc228d88c740f36ff108d78d8bb65097
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-09 13:35:02 +0200

    Task 11

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson2/Task11AritmeticMean.java
@@ -0,0 +1,18 @@
+package students.student_jurij_hlebnikov.lesson2;
+
+import java.util.Scanner;
+
+public class Task11AritmeticMean {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double numberOne = scanner.nextDouble();
+        double numberTwo = scanner.nextDouble();
+        double numberThree = scanner.nextDouble();
+
+        System.out.println((numberOne + numberTwo + numberThree) / 3);
+
+
+
+    }
+}
