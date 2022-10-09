f3657d783414ec96963fb4b8e5c268887ff94f08
Raivuha <onemoreraivuha@gmail.com>
2022-10-09 22:30:21 +0300

    add homework lesson-2 level-1

---
+++ src/main/java/students/student_raivis_urbevics/Lesson2_Level1/Task_1.java
@@ -0,0 +1,35 @@
+package students.student_raivis_urbevics.Lesson2_Level1;
+
+import java.sql.SQLOutput;
+
+public class Task_1 {
+
+    public static void main(String[] args) {
+        int n1=8;
+        int n2=2;
+        int sum=n1+n2;
+        System.out.println(sum);
+        int subtract=n1-n2;
+        System.out.println(subtract);
+        int multiply=n1*n2;
+        System.out.println(multiply);
+        int division=n1/n2;
+        System.out.println(division);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/students/student_raivis_urbevics/Lesson2_Level1/Task_1_Scanner.java
@@ -0,0 +1,35 @@
+package students.student_raivis_urbevics.Lesson2_Level1;
+
+import java.util.Scanner;
+
+public class Task_1_Scanner{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Input the first number");
+        int n1=scanner.nextInt();
+        System.out.println("Input the second number");
+        int n2=scanner.nextInt();
+        int sum=n1+n2;
+        int subtract=n1-n2;
+        int division=n1/n2;
+        int multiply=n1*n2;
+        System.out.println(sum);
+        System.out.println(subtract);
+        System.out.println(division);
+        System.out.println(multiply);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
