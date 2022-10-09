6804ff89f34175c5ddabd8bff58aed4144520d78
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-09 18:30:37 +0200

    Task 15

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson2/Task15PrintNoLn.java
@@ -0,0 +1,12 @@
+package students.student_jurij_hlebnikov.lesson2;
+
+public class Task15PrintNoLn {
+    public static void main(String[] args) {
+        System.out.print("Наша Маша");
+        System.out.println(" горько плачет");
+        System.out.print("уронила ");
+        System.out.print("в речку мячик");
+
+
+    }
+}
