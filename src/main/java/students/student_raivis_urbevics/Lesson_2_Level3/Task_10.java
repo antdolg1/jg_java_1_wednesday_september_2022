81007ae77a464693923834581ca61f02af81c79c
Raivuha <onemoreraivuha@gmail.com>
2022-10-09 22:39:09 +0300

    add homework lesson-2 level-3

---
+++ src/main/java/students/student_raivis_urbevics/Lesson_2_Level3/Task_10.java
@@ -0,0 +1,23 @@
+package students.student_raivis_urbevics.Lesson_2_Level3;
+
+import java.util.Scanner;
+
+public class Task_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the radius of the circle: ");
+        double radius = scanner.nextDouble();
+        System.out.println("Perimeter is = " + (2 * radius * Math.PI));
+        System.out.println("Area is = " + (Math.PI * radius * radius));
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/students/student_raivis_urbevics/Lesson_2_Level3/Task_9.java
@@ -0,0 +1,44 @@
+package students.student_raivis_urbevics.Lesson_2_Level3;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task_9 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Input the any number");
+        int n=scanner.nextInt();
+        for (int i=1; i<=10; i++) {
+            System.out.println("8*" + i + "=" + 8*i);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
