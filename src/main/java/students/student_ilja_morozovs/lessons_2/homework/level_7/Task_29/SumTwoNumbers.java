2855f3b2af47eb6eaae328177891a8e6924285f2
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-09 22:45:05 +0300

    Add level 7 programs

---
+++ src/main/java/students/student_ilja_morozovs/lessons_2/homework/level_7/Task_29/SumTwoNumbers.java
@@ -0,0 +1,14 @@
+package students.student_ilja_morozovs.lessons_2.homework.level_7.Task_29;
+
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 20;
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+        int sum = numberOne + numberTwo;
+        System.out.println("Sum = " + sum);
+    }
+
+}
---
+++ src/main/java/students/student_ilja_morozovs/lessons_2/homework/level_7/Task_32/Comment.java
@@ -0,0 +1,15 @@
+package students.student_ilja_morozovs.lessons_2.homework.level_7.Task_32;
+
+import students.student_konstantin_karetnikov.Hello_World;
+
+public class Comment {
+
+    public static void main(String[] args) {
+        System.out.println("Test");// однострочный комментарий
+        System.out.println("Test");/*
+        многострочный комментарий
+        System.out.println("Test");*/
+        System.out.println("Test");
+
+    }
+}
