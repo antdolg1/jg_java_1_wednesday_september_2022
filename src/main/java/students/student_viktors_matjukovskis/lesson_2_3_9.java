a1f64f1b8d2e57cf8e526d862718f685081ef309
viktors1989 <viktors@4k.lv>
2022-10-09 23:21:49 +0300

    Home work

---
+++ src/main/java/students/student_viktors_matjukovskis/lesson_2_3_9.java
@@ -0,0 +1,25 @@
+package students.student_viktors_matjukovskis;
+
+import java.util.Scanner;
+
+public class lesson_2_3_9 {
+
+    public static void main(String[] args) {
+        Scanner umn = new Scanner(System.in);
+        System.out.println("введите цифру");
+        int num1 = umn.nextInt();
+        int res0 = num1 * 0;
+        int res1 = num1 * 1;
+        int res2 = num1 * 2;
+        int res3 = num1 * 3;
+        int res4 = num1 * 4;
+        int res5 = num1 * 5;
+        int res6 = num1 * 6;
+        int res7 = num1 * 7;
+        int res8 = num1 * 8;
+        int res9 = num1 * 9;
+        System.out.println("0*" + num1 + "=" + res0 + "\n" + "1*" + num1 + "=" + res1 + "\n" + "2*" + num1 + "=" + res2 + "\n" + "3*" + num1 + "=" + res3 + "\n" + "4*" + num1 + "=" + res4 + "\n" + "5*" + num1 + "=" + res5 + "\n" + "6*" + num1 + "=" + res6 + "\n" + "7*" + num1 + "=" + res7 + "\n" + "8*" + num1 + "=" + res8 + "\n" +"9*" + num1 + "=" + res9 + "\n");
+        }
+    }
+
+
