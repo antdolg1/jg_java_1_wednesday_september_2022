51b8e7f48d84895d38c9281a6a43ef188c850ec2
ArtGobl1n <artjoms122@gmail.com>
2022-10-09 15:22:15 +0300

    Task1 and task2

---
+++ src/main/java/students/student_artjoms_trosihins/lesson_2_level_1/FractionalMath.java
@@ -0,0 +1,25 @@
+package students.student_artjoms_trosihins.lesson_2_level_1;
+
+import java.util.Scanner;
+
+public class FractionalMath {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double firstDoubleNumber;
+        double secondDoubleNumber;
+        System.out.println("Please write first fractional number :");
+          firstDoubleNumber = scanner.nextDouble();
+        System.out.println(firstDoubleNumber);
+        System.out.println("Please write second fractional number :");
+          secondDoubleNumber = scanner.nextDouble();
+        System.out.println(secondDoubleNumber);
+        double sum = firstDoubleNumber + secondDoubleNumber;
+        System.out.println("Sum is " + sum);
+        double sub = firstDoubleNumber - secondDoubleNumber;
+        System.out.println("Subtraction is " + sub);
+        double multi = firstDoubleNumber * secondDoubleNumber;
+        System.out.println("Multiplication is " + multi);
+        double div = firstDoubleNumber / secondDoubleNumber;
+        System.out.println("Division is " + div);
+    }
+}
