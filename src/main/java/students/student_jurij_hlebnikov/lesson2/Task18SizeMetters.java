e76cfb67ba539330474e65de2b1d9c2996380e62
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-09 19:16:59 +0200

    Task 18

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson2/Task18SizeMetters.java
@@ -0,0 +1,10 @@
+package students.student_jurij_hlebnikov.lesson2;
+
+class Task18SizeMetters{
+
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+
+}
