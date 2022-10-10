382e2ffdb56d70b1d036eab184ba0f4756a7d411
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-10 13:38:29 +0200

    Task 24

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson2/Task24NoSleepNoFocus.java
@@ -0,0 +1,9 @@
+package students.student_jurij_hlebnikov.lesson2;
+
+public class Task24NoSleepNoFocus {
+
+        public static void main(String[] args) {
+            System.out.println("I wanna coffee");
+            System.out.println("So bad");
+}
+}
