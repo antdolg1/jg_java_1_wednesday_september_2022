3513d2a1447c9d22d62e802cff6e1023f1a81724
Ilya <ilya.tihonov@inbox.lv>
2022-10-10 21:18:49 +0300

    Scanner_nextInt().java

---
+++ src/main/java/students/student_ilya_tihonov/ScannerHomework.java
@@ -0,0 +1,28 @@
+package students.student_ilya_tihonov;
+
+import java.util.Scanner;
+
+public class ScannerHomework  {
+
+    public static void main(String[] args) {
+
+        int a , b , sum , sum1 , sum2 , sum3;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter eny numbers: ");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println("______________");
+        sum = a + b;
+        sum1 = a - b;
+        sum2 = a * b;
+        sum3 = a / b;
+        System.out.println("Addition- " + sum);
+        System.out.println("Subtraction- " + sum1);
+        System.out.println("Multiplication- " + sum2);
+        System.out.println("Division- " + sum3);
+
+
+
+    }
+}
