f8b3687dd02537b20834b1bb0fa7ba4fcd6d4168
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-10 13:34:10 +0200

    Task 23

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson2/Task23AttentionPlease.java
@@ -0,0 +1,10 @@
+package students.student_jurij_hlebnikov.lesson2;
+
+public class Task23AttentionPlease {
+
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+
+    }
+}
+
