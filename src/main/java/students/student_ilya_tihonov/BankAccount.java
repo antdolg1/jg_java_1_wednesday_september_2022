49c1fa2a15da714b76db02300e813434f01506ef
Ilya <ilya.tihonov@inbox.lv>
2022-10-10 22:52:55 +0300

    lvl_2_t_3-7.java

---
+++ src/main/java/students/student_ilya_tihonov/BankAccount.java
@@ -0,0 +1,9 @@
+package students.student_ilya_tihonov;
+
+public class BankAccount {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/students/student_ilya_tihonov/LoginService1.java
@@ -0,0 +1,9 @@
+package students.student_ilya_tihonov;
+
+public class LoginService1 {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+}
