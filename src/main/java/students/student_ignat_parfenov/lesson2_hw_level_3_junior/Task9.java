2cfc43b75743d1fefcd2bff012f2768e1f752b8d
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-10 12:57:20 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson2_hw_level_3_junior/Task9.java
+++ src/main/java/students/student_ignat_parfenov/lesson2_hw_level_3_junior/Task9.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task9 {public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Test Data: " + "\nInput a number:");
+    System.out.print("Test Data: " + "\nInput a number: ");
     int yourNumber = scanner.nextInt();
     System.out. println("____________________");
     System.out.println("Expected result is :");
