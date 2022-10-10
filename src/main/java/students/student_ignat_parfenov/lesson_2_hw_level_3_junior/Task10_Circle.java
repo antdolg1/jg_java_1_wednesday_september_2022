a9519fc5e3ea09c4ab6addab97a4bce03740e14e
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-10 12:55:55 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_2_hw_level_3_junior/Task10_Circle.java
+++ src/main/java/students/student_ignat_parfenov/lesson_2_hw_level_3_junior/Task10_Circle.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task10_Circle {public static void main(String[] args){
 
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Test Data: " + "\nRadius = ");
+    System.out.print("Test Data: " + "\nRadius = ");
     double yourRadius = scanner.nextDouble();
 
     System.out.println("Perimeter is = " + 2*Math.PI*yourRadius);
