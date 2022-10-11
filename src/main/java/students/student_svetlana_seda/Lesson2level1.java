af1e620d2f0129a0970f0ba6723d0805a4eb2576
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-11 20:42:06 +0300

    Homework - lesson 2 - task 1

---
+++ src/main/java/students/student_svetlana_seda/Lesson2level1.java
@@ -0,0 +1,18 @@
+package students.student_svetlana_seda;
+
+import java.util.Scanner;
+
+public class Lesson2level1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int firstNumber=scanner.nextInt();
+        System.out.println("Please enter the first number:");
+
+
+
+
+
+    }
+
+}
