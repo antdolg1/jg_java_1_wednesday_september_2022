762e145001622305126b83f4116cf101ca096dce
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-11 15:19:32 +0200

    Task_32

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson_2/homework/Task_32.java
@@ -0,0 +1,11 @@
+package students.student_jurij_hlebnikov.lesson_2.homework;
+
+public class Task_32 {
+    //однострочный комментарий
+
+    /*
+          многострочный
+
+          комментарий
+
+     */
