01d8068f3c5a36992ff87ee688f17f09524f132c
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-12 09:53:17 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson2HwLevel3Junior/Task9.java
+++ src/main/java/students/student_ignat_parfenov/lesson2HwLevel3Junior/Task9.java
@@ -2,22 +2,22 @@ package students.student_ignat_parfenov.lesson2HwLevel3Junior;
 
 import java.util.Scanner;
 
-public class Task9 {public static void main(String[] args) {
+public class Task9 {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Test Data: " + "\nInput a number: ");
-    int yourNumber = scanner.nextInt();
-    System.out. println("____________________");
-    System.out.println("Expected result is :");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Test Data: " + "\nInput a number: ");
+        int yourNumber = scanner.nextInt();
+        System.out.println("____________________");
+        System.out.println("Expected result is :");
 
-    int i = 1;
-    while (i <= 10) {
-    int result = yourNumber * i;
-    System.out.print(yourNumber + " x " + i + " = " + result + "\n");
-        i++;
+        int i = 1;
+        while (i <= 10) {
+            int result = yourNumber * i;
+            System.out.print(yourNumber + " x " + i + " = " + result + "\n");
+            i++;
+        }
     }
-
-}
 }
 
 
