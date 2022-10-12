39097b82ea629b46082f4bd91e0462ef7886bee6
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-12 10:03:52 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson2HwLevel2Intern/Book.java
+++ src/main/java/students/student_ignat_parfenov/lesson2HwLevel2Intern/Book.java
@@ -1,12 +1,6 @@
 package students.student_ignat_parfenov.lesson2HwLevel2Intern;
 
-public class Book {
-
-    private String title;
-    private int pages;
-
-    public static void main(String[] args) {
+    public class Book {
 
     }
-}
 
--- src/main/java/students/student_ignat_parfenov/lesson2HwLevel2Intern/Phone.java
+++ src/main/java/students/student_ignat_parfenov/lesson2HwLevel2Intern/Phone.java
@@ -1,12 +1,5 @@
 package students.student_ignat_parfenov.lesson2HwLevel2Intern;
 
-public class Phone {
-
-    public int number;
-    public String model;
-    public int weight;
-
-    public static void main(String[] args) {
+    public class Phone {
 
     }
-}
