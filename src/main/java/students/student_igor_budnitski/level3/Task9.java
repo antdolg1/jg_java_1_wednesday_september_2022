f95972ea926fe7e657aea95aaa31f16cc3310d48
eigobud <igor.budnitski@ericsson.com>
2022-10-12 07:32:25 +0300

    homework

--- src/main/java/students/student_igor_budnitski/level3/Task9.java
+++ src/main/java/students/student_igor_budnitski/level3/Task9.java
@@ -12,7 +12,7 @@ public class Task9 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter the number from 1 to 10: ");
         int number = scanner.nextInt();
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 11; i++) {
             System.out.println(number + " * " + i + " = " + (number * i));
         }
     }
