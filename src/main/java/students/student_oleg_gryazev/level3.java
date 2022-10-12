edb4350a0bbff9b79f8a4ee8c7c9be255e863944
Evroleg <evroleg@gmail.com>
2022-10-12 14:35:49 +0300

    initial commit lesson 11 + 12

---
+++ src/main/java/students/student_oleg_gryazev/level3.java
@@ -0,0 +1,13 @@
+package students.student_oleg_gryazev;
+import java.util.Scanner;
+public class level3 {
+    public static void main(String[] args) {
+        int userNumber;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number");
+       userNumber = scanner.nextInt();
+
+        System.out.println("User entered number " + userNumber);
+    }
+
+}
