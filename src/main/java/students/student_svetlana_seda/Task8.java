a3aa9c043714bb12a623e13a556d86dcb034609f
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-12 18:15:48 +0300

    Homework - lesson2 - level2 - please check

---
+++ src/main/java/students/student_svetlana_seda/Task8.java
@@ -0,0 +1,20 @@
+package students.student_svetlana_seda;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Task8 {
+
+    public static void main(String[] args) {
+
+    String name;
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter your username: ");
+          name = scanner.nextLine();
+
+        System.out.println("HELLO, " + name + "!");
+
+    }
+
+}
