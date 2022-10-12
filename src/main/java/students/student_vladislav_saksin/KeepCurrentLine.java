0b1fac965b731529f9c7fba6e303a982d8f8e9ae
Vlad-Rix <vladislavs.saksins@ya.ru>
2022-10-12 14:05:35 +0300

    Task 15 - stay in same line

---
+++ src/main/java/students/student_vladislav_saksin/KeepCurrentLine.java
@@ -0,0 +1,9 @@
+package students.student_vladislav_saksin;
+
+public class KeepCurrentLine {
+
+    public static void main(String[] args) {
+
+        System.out.print("Words like violence, break the silence");
+    }
+}
