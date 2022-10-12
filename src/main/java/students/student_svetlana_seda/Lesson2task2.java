0a8ed28eb003909dcb6e0ff4f6b30140fdf4a8c0
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-12 17:00:27 +0300

    Homework - lesson2 - level1 - task2 please check

--- src/main/java/students/student_svetlana_seda/Lesson2task2.java
+++ src/main/java/students/student_svetlana_seda/Lesson2task2.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class Lesson2task2 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        double firstDoubleNumber = scanner.nextDouble();
         System.out.println("Please enter the first double number:");
-        double secondDoubleNumber =scanner.nextDouble();
+        double firstDoubleNumber = scanner.nextDouble();
         System.out.println("Please enter the second double number:");
+        double secondDoubleNumber =scanner.nextDouble();
 
-
-
+        System.out.println(firstDoubleNumber + secondDoubleNumber);
+        System.out.println(firstDoubleNumber - secondDoubleNumber);
+        System.out.println(firstDoubleNumber / secondDoubleNumber);
+        System.out.println(firstDoubleNumber * secondDoubleNumber);
 
     }
 }
+
