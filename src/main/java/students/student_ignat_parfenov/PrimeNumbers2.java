b0257459ecf9bedecfbb92761975895b09ab3484
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-12 10:07:09 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/PrimeNumbers2.java
+++ src/main/java/students/student_ignat_parfenov/PrimeNumbers2.java
@@ -1,23 +1,21 @@
 package students.student_ignat_parfenov;
 
-    public class PrimeNumbers2 {
-        public static void main(String[] args) {
+public class PrimeNumbers2 {
+    public static void main(String[] args) {
 
-            for (int a = 2; a <= 30; a++) {
-                boolean isPrime = true;
-
-                for (int k = 2; k < a; k++)
-                    if (a % k == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                if (isPrime) {
-                    System.out.println(a);
+        for (int a = 2; a <= 30; a++) {
+            boolean isPrime = true;
+            for (int k = 2; k < a; k++)
+                if (a % k == 0) {
+                    isPrime = false;
+                    break;
                 }
+            if (isPrime) {
+                System.out.println(a);
             }
         }
-
     }
+}
 
 
 
