42032bf6ba40857211ab02302de0f88debc28274
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-13 18:02:42 +0300

    Homework - lesson2 - level2 done

---
+++ src/main/java/students/student_svetlana_seda/lesson2/level2/HelloLana.java
@@ -0,0 +1,16 @@
+package students.student_svetlana_seda.lesson2.level2;
+
+import java.util.Scanner;
+
+public class HelloLana {
+    public static void main(String[] args) {
+
+            String n;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please, enter your username: ");
+            n = scanner.nextLine();
+                 System.out.println("HELLO, " + n + "!");
+
+    }
+
+}
