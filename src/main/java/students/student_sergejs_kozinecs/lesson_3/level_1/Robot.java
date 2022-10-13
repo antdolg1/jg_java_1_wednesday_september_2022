50a359394447b1626e0e9d0c1deb84dbbb5c82c5
Sergejs Kozinecs <sergei.kozinecs@gmail.com>
2022-10-13 14:45:01 +0300

    Lesson 3 level 1 done

---
+++ src/main/java/students/student_sergejs_kozinecs/lesson_3/level_1/Robot.java
@@ -0,0 +1,18 @@
+package students.student_sergejs_kozinecs.lesson_3.level_1;
+
+public class Robot {
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+        // тут напишите команду распечатки на консоли строки "Hello!"
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        // тут напишите команду распечатки на консоли строки "My name is ROBO"
+        System.out.println("My name is ROBO");
+    }
+}
