e5c7af864e1a40290a64d3da7192660b96a1d96e
eigobud <igor.budnitski@ericsson.com>
2022-10-13 16:22:55 +0300

    homework

---
+++ src/main/java/students/student_igor_budnitski/lesson_3/homework/level_1/Robot.java
@@ -0,0 +1,17 @@
+package students.student_igor_budnitski.lesson_3.homework.level_1;
+
+public class Robot {
+    public void sayHello() {
+        // тут напишите команду распечатки на консоли строки "Hello!"
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        // тут напишите команду распечатки на консоли строки "My name is ROBO"
+        System.out.println("My name is ROBO");
+    }
+
+    public Robot() {
+
+    }
+}
---
+++ src/main/java/students/student_igor_budnitski/lesson_3/homework/level_1/RobotDemo.java
@@ -0,0 +1,21 @@
+package students.student_igor_budnitski.lesson_3.homework.level_1;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot robot1 = new Robot(); // creating robot object
+        robot1.sayHello(); // calling method to say Hello
+        robot1.sayYourName(); // calling method to print name
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+
+        Robot robot3 = new Robot();
+        robot3.sayHello();
+        robot3.sayYourName();
+
+        Robot robot4 = new Robot();
+        robot4.sayHello();
+        robot4.sayYourName();
+    }
+}
