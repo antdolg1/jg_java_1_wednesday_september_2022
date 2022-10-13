af1f850e0cee02a994b37aba56cc14bf3fb9ce2e
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2022-10-13 18:00:19 +0300

    Lesson_3_level_2

---
+++ src/main/java/students/student_igor_budnitski/lesson_3/homework/level_2/Task_9.java
@@ -0,0 +1,12 @@
+package students.student_igor_budnitski.lesson_3.homework.level_2;
+
+import java.util.Scanner;
+
+public class Task_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String myName = sc.nextLine();
+        System.out.println("Hello " + myName + "!");
+    }
+}
