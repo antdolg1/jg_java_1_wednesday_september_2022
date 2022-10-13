0bf7bfccf806556d7194ff94680597c750554b16
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-13 17:50:53 +0300

    Homework - lesson2 - level2 done

--- src/main/java/students/student_svetlana_seda/lesson2/level2/HelloWorld.java
+++ src/main/java/students/student_svetlana_seda/lesson2/level2/HelloWorld.java
@@ -1,6 +1,6 @@
 package students.student_svetlana_seda.lesson2.level2;
 
-public class hello_world {
+public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
--- src/main/java/students/student_svetlana_seda/lesson2/level2/LoginService.java
+++ src/main/java/students/student_svetlana_seda/lesson2/level2/LoginService.java
@@ -2,6 +2,7 @@ package students.student_svetlana_seda.lesson2.level2;
 
 public class LoginService {
     public static void main(String[] args) {
+
         System.out.println("You can login!");
 
     }
