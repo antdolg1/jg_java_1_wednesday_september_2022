d600c3fbaa931507cab66d1da01bd84271fa34e4
OskarsP <oskars@popens.lv>
2022-10-13 16:06:34 +0300

    Add level 1 tasks

---
+++ src/main/java/students/student_oskars_popens/lesson_3/level1_intern/Robot.java
@@ -0,0 +1,16 @@
+package students.student_oskars_popens.lesson_3.level1_intern;
+
+public class Robot {
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
---
+++ src/main/java/students/student_oskars_popens/lesson_3/level1_intern/RobotDemo.java
@@ -0,0 +1,14 @@
+package students.student_oskars_popens.lesson_3.level1_intern;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot myRobot = new Robot();
+        Robot myRobot2 = new Robot();
+
+        myRobot.sayHello();
+        myRobot.sayYourName();
+
+        myRobot2.sayHello();
+        myRobot2.sayYourName();
+    }
+}
