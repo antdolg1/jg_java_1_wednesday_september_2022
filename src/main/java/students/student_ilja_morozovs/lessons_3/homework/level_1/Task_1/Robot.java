5ca98d46070f39e48984d75248c12ef39982fe8c
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-13 12:34:35 +0300

    Add army of robot :) Lessons 3 level_1

---
+++ src/main/java/students/student_ilja_morozovs/lessons_3/homework/level_1/Task_1/Robot.java
@@ -0,0 +1,8 @@
+package students.student_ilja_morozovs.lessons_3.homework.level_1.Task_1;
+
+public class Robot {
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/main/java/students/student_ilja_morozovs/lessons_3/homework/level_1/Task_4/RobotDemo.java
@@ -0,0 +1,10 @@
+package students.student_ilja_morozovs.lessons_3.homework.level_1.Task_4;
+
+import students.student_ilja_morozovs.lessons_3.homework.level_1.Task_2.Robot1;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot1 verter = new Robot1();
+        verter.sayHello();
+    }
+}
