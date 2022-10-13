c987ad14e3963181fa97448f9e694a25a9666500
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2022-10-13 21:59:34 +0300

    Lesson_3_level_3

--- src/main/java/students/student_igor_budnitski/lesson_3/homework/level_1/Robot.java
+++ src/main/java/students/student_igor_budnitski/lesson_3/homework/level_1/Robot.java
@@ -1,6 +1,8 @@
 package students.student_igor_budnitski.lesson_3.homework.level_1;
 
 public class Robot {
+
+    String name;
     public void sayHello() {
         // тут напишите команду распечатки на консоли строки "Hello!"
         System.out.println("Hello!");
@@ -8,10 +10,14 @@ public class Robot {
 
     public void sayYourName() {
         // тут напишите команду распечатки на консоли строки "My name is ROBO"
-        System.out.println("My name is ROBO");
+        System.out.println("My name is " + this.name);
     }
 
     public Robot() {
 
+    }
+
+    public Robot(String robotName){
+        this.name = robotName;
     }
 }
