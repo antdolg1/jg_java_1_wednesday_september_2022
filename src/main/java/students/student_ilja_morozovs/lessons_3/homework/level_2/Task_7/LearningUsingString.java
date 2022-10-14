57ad7d17e081291614833f7f8eaa967b3456285d
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-14 09:22:11 +0300

    Lessons 3 level_2 removed public

--- src/main/java/students/student_ilja_morozovs/lessons_3/homework/level_2/Task_7/LearningUsingString.java
+++ src/main/java/students/student_ilja_morozovs/lessons_3/homework/level_2/Task_7/LearningUsingString.java
@@ -1,8 +1,8 @@
 package students.student_ilja_morozovs.lessons_3.homework.level_2.Task_7;
 
-public class LearningUsingString {
+class LearningUsingString {
     public static void main(String[] args) {
-        String myName="Илья";
+        String myName = "Илья";
         System.out.println(myName);
     }
 }
--- src/main/java/students/student_ilja_morozovs/lessons_3/homework/level_2/Task_9/ReadName.java
+++ src/main/java/students/student_ilja_morozovs/lessons_3/homework/level_2/Task_9/ReadName.java
@@ -2,11 +2,11 @@ package students.student_ilja_morozovs.lessons_3.homework.level_2.Task_9;
 
 import java.util.Scanner;
 
-public class ReadName {
+class ReadName {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please set your name: ");
         String name = scanner.nextLine();
-        System.out.println("Hello! " +name+"!");
+        System.out.println("Hello! " + name + "!");
     }
 }
