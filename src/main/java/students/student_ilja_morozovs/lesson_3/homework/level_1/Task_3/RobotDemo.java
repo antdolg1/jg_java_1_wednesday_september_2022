b546546da0ece664222275ee3dd417a467bddb9f
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-14 09:20:14 +0300

    Lessons 3 level_1 removed public and refactor all class

---
+++ src/main/java/students/student_ilja_morozovs/lesson_3/homework/level_1/Task_3/RobotDemo.java
@@ -0,0 +1,10 @@
+package students.student_ilja_morozovs.lesson_3.homework.level_1.Task_3;
+
+
+class RobotDemo {
+    public static void main(String[] args) {
+        Robot verter;
+        verter = new Robot();
+
+    }
+}
--- src/main/java/students/student_ilja_morozovs/lessons_3/homework/level_1/Task_1/Robot.java
+++
@@ -1,8 +0,0 @@
-package students.student_ilja_morozovs.lessons_3.homework.level_1.Task_1;
-
-public class Robot {
-
-    public void sayHello() {
-        System.out.println("Hello!");
-    }
-}
