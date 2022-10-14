eb9246069814b47b36bb4b481ae7bc0edb55dc87
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-14 09:35:20 +0300

    Lessons 3 level_2 removed public

--- src/main/java/students/student_ilja_morozovs/lesson_2/homework/level_2/Task_5/BankAccount.java
+++ src/main/java/students/student_ilja_morozovs/lesson_2/homework/level_2/Task_5/BankAccount.java
@@ -1,4 +1,4 @@
-package students.student_ilja_morozovs.lessons_2.homework.level_2.Task_5;
+package students.student_ilja_morozovs.lesson_2.homework.level_2.Task_5;
 
 public class BankAccount {
     public static void main(String[] args) {
--- src/main/java/students/student_ilja_morozovs/lesson_2/homework/level_2/Task_7/LoginService1.java
+++ src/main/java/students/student_ilja_morozovs/lesson_2/homework/level_2/Task_7/LoginService1.java
@@ -1,4 +1,4 @@
-package students.student_ilja_morozovs.lessons_2.homework.level_2.Task_7;
+package students.student_ilja_morozovs.lesson_2.homework.level_2.Task_7;
 
 public class LoginService1 {
     public static void main(String[] args) {
