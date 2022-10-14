76fe48accdb244e7c1b0e15aafc3664ac82feb2a
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2022-10-14 16:41:17 +0300

    Lesson_3_level_5 - Dog

---
+++ src/main/java/students/student_igor_budnitski/lesson_3/homework/level_5/DogDemo.java
@@ -0,0 +1,12 @@
+package students.student_igor_budnitski.lesson_3.homework.level_5;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Charlie", 4, "black");
+        dog1.voice();
+        dog1.happyBirthday();
+        dog1.voice();
+        dog1.changeColor("gold");
+        dog1.voice();
+    }
+}
