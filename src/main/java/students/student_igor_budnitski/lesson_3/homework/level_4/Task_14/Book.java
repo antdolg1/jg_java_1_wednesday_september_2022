4fec342556d28ddf12eb9c1e1bc48484289ad41c
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2022-10-14 11:40:20 +0300

    Lesson_3_level_4

---
+++ src/main/java/students/student_igor_budnitski/lesson_3/homework/level_4/Task_14/Book.java
@@ -0,0 +1,13 @@
+package students.student_igor_budnitski.lesson_3.homework.level_4.Task_14;
+
+public class Book {
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+}
