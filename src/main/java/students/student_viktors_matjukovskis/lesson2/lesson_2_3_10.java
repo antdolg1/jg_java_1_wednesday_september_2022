10422a768338e27733fb2cbe17952ec402d7809b
viktors1989 <viktors@4k.lv>
2022-10-15 21:31:34 +0300

    Home work lesson2

--- src/main/java/students/student_viktors_matjukovskis/lesson2/lesson_2_3_10.java
+++ src/main/java/students/student_viktors_matjukovskis/lesson2/lesson_2_3_10.java
@@ -6,19 +6,16 @@ public class lesson_2_3_10 {
 
    public static void main(String[] args) {
 
-   Scanner radius = new Scanner(System.in);
-   System.out.println("Введите радиус круга: ");
+      Scanner radius = new Scanner(System.in);
+      System.out.println("Введите радиус круга: ");
 
-   double r = radius.nextDouble();
-   double res = (r * 2 * 3.14);
-   System.out.println("------------------------");
-   System.out.println("Периметр круга: " + res);
+      double r = radius.nextDouble();
+      double res = (r * 2 * Math.PI);
+      System.out.println("------------------------");
+      System.out.println("Периметр круга: " + res);
 
-   double res2 = (r * 2 * 3.14);
-   double a1 = r;
-   double b1 = 2;
-   double res3 = (Math.pow(a1, b1) * 3.14);
-       System.out.println("Площадь круга: " + res3);
+      double res3 = (Math.pow(r,2) * Math.PI);
+      System.out.println("Площадь круга: " + res3);
 
-     }
+   }
 }
