40b093f8ee2b90386de11580081ea37c41b0a03b
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-15 16:34:55 +0300

    Homework - lesson2 - level5 done

---
+++ src/main/java/students/student_svetlana_seda/lesson2/level5/TextCommand.java
@@ -0,0 +1,11 @@
+package students.student_svetlana_seda.lesson2.level5;
+
+public class TextCommand {
+
+    public static void main(String[] args) {
+
+        System.out.println("Text");
+
+    }
+
+}
