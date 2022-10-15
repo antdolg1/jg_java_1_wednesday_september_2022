37ab2a03dd97ee9e5c55e43d278a25974a568fb8
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-15 20:45:01 +0300

    Homework - lesson2 - level6 done

---
+++ src/main/java/students/student_svetlana_seda/lesson_2/level_4/Main.java
@@ -0,0 +1,11 @@
+package students.student_svetlana_seda.lesson_2.level_4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
