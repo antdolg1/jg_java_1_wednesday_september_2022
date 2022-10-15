2f8e17db11f2df9f6d2cb4dc9509830be0b56450
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-15 23:16:07 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson3/homework/RobotDemo.java
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/RobotDemo.java
@@ -3,12 +3,13 @@ package students.student_ignat_parfenov.lesson3.homework;
 public class RobotDemo {
     public static void main(String[] args) {
 
-    }
-    public RobotDemo() {
-
         Robot firstRobot = new Robot();
         firstRobot.sayHello();
         firstRobot.sayYourName();
+    }
+
+    public RobotDemo() {
+
     }
 
 }
