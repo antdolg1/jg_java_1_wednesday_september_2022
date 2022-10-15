e550fbff8f717aaff784c9b3a21e45c0757256d7
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-15 21:52:20 +0300

    Homework - lesson2 - level7 done

---
+++ src/main/java/students/student_svetlana_seda/lesson_2/level_7/task_32/SingleMultiline.java
@@ -0,0 +1,12 @@
+package students.student_svetlana_seda.lesson_2.level_7.task_32;
+
+public class SingleMultiline {
+
+    public static void main(String[] args) {
+
+        System.out.println("Singleline comment");
+            System.out.println("My name \n" + "is \n" + "Lana \n");
+
+    }
+
+}
