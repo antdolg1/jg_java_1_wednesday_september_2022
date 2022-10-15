adcbf5e54122983771435435359da0acf4c7ddce
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-15 16:58:29 +0200

    Task_15

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/Phone.java
@@ -0,0 +1,18 @@
+package students.student_jurij_hlebnikov.lesson_3.homework;
+
+public class Phone {
+
+    String model;
+
+    Phone(String model) {
+
+        this.model = model;
+
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
+
---
+++ src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/PhoneDemo.java
@@ -0,0 +1,9 @@
+package students.student_jurij_hlebnikov.lesson_3.homework;
+
+public class PhoneDemo {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+}
