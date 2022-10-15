95105ed9ef8b84ce313d1d903ec9f60850855ee2
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-15 15:50:21 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson3/BankAccount.java
@@ -0,0 +1,30 @@
+package students.student_ignat_parfenov.lesson3;
+
+public class BankAccount {
+
+    String owner;
+    int accountNumber;
+    double moneyAmount;
+    String country;
+
+    public BankAccount(String owner, int accountNumber, double moneyAccount, String country) {
+
+        this.owner = owner;
+        this.accountNumber = accountNumber;
+        this.moneyAmount = moneyAccount;
+        this.country = country;
+
+    }
+
+    void addMoneyToAccount(double moneyToAdd){
+        moneyAmount = moneyAmount + moneyToAdd;
+    }
+
+    void withdrawMoneyToAccount(double moneyToWithdraw){
+        moneyAmount = moneyAmount - moneyToWithdraw;
+    }
+
+    void checkTheBalance(){
+        System.out.println("Current balance is = " + moneyAmount);
+    }
+}
