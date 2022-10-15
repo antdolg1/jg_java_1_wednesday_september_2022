8119b2c05508703146024a54af0e0844e1a78809
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-15 16:29:40 +0300

    Homework - lesson2 - level5 done

---
+++ src/main/java/students/student_svetlana_seda/lesson2/level5/LettersBigSmall.java
@@ -0,0 +1,12 @@
+package students.student_svetlana_seda.lesson2.level5;
+
+public class LettersBigSmall {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sorry, I have an error.");
+        System.out.println("Please fix me!");
+
+    }
+
+}
---
+++ src/main/java/students/student_svetlana_seda/lesson2/level5/TextCommand.java
@@ -0,0 +1,10 @@
+package students.student_svetlana_seda.lesson2.level5;
+
+public class TextCommand {
+
+    public static void main(String[] args) {
+        System.out.println("Text");
+
+    }
+
+}
