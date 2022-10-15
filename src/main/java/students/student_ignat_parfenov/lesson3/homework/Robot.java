120b2274b68e5b15a2305ac39094aed9235b1720
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-15 22:02:44 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/Robot.java
@@ -0,0 +1,12 @@
+package students.student_ignat_parfenov.lesson3.homework;
+
+public class Robot {
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public Robot() {
+    }
+
+}
---
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/RobotDemo.java
@@ -0,0 +1,16 @@
+package students.student_ignat_parfenov.lesson3.homework;
+
+public class RobotDemo {
+
+    public RobotDemo() {
+
+        Robot firstRobot = new Robot();
+        firstRobot.sayHello();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
