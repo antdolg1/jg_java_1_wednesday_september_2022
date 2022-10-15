536c1179a27c6b0096aa0e9409b58921ebafb155
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-15 13:28:36 +0300

    Homework - lesson2 - level4 done

---
+++ src/main/java/students/student_svetlana_seda/lesson2/level4/Main.java
@@ -0,0 +1,9 @@
+package students.student_svetlana_seda.lesson2.level4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Privet, ja zapustilsja :)");
+    }
+
+}
