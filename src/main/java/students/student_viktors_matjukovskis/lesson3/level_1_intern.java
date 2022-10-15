d9e96cefc190a0b7926d15e022c0b33b730fd286
viktors1989 <viktors@4k.lv>
2022-10-15 20:40:35 +0300

    Home work lesson2

---
+++ src/main/java/students/student_viktors_matjukovskis/lesson3/level_1_intern.java
@@ -0,0 +1,12 @@
+package students.student_viktors_matjukovskis.lesson3;
+
+public class level_1_intern {
+
+    public class Robot {
+
+        public void sayHello() {System.out.println("Hello");}
+
+
+    }
+
+}
