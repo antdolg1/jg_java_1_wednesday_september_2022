29d43f701f7d21954ab075350475eafbb0358e0b
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-16 12:16:44 +0200

    Book Homework_3 Bug Fix

--- src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/Book.java
+++ src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/Book.java
@@ -2,21 +2,16 @@ package students.student_jurij_hlebnikov.lesson_3.homework;
 
 public class Book {
 
-String title;
+    String title;
 
-        Book(String bookTitle) {
-
-            this.title = bookTitle;
-
-        }
-
-        String getTitle() {
-
-            return this.title;
-
-        }
-
+    Book(String bookTitle) {
+        this.title = bookTitle;
     }
+
+    String getTitle() {
+        return this.title;
+    }
+}
 
 
 
