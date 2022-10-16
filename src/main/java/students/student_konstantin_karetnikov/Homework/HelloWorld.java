f3a852426256340cdba0e9e025f6fd8c3b884553
konstantinkaretnikov <kskaretnikov@gmail.com>
2022-10-16 17:27:25 +0300

    Lesson 2 Level 7 done. Bug fixing done

---
+++ src/main/java/students/student_konstantin_karetnikov/Homework/HelloWorld.java
@@ -0,0 +1,6 @@
+package students.student_konstantin_karetnikov.Homework;
+
+public class HelloWorld {public static void main(String[] args)
+{System.out.println("Hello World!");
+}
+}
--- src/main/java/students/student_konstantin_karetnikov/Homework/Hello_World.java
+++
@@ -1,6 +0,0 @@
-package students.student_konstantin_karetnikov.Homework;
-
-public class Hello_World {public static void main(String[] args) {System.out.println("Hello World!");
-
-}
-}
