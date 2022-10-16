bc5137442cc38911c82fb6bea91516c1155dc6f3
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-16 15:54:44 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson3/homework/Boris.java
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/Boris.java
@@ -2,5 +2,15 @@ package students.student_ignat_parfenov.lesson3.homework;
 
 public class Boris {
 
+    public class Robot {
 
+        String name;
+
+        public Robot(String name) {
+        }
+
+        {
+        this.name = name;
+        }
+    }
 }
---
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/BorisDemo.java
@@ -0,0 +1,13 @@
+package students.student_ignat_parfenov.lesson3.homework;
+
+public class BorisDemo {
+
+    public static void main(String[] args){
+
+        Robot robot = new Robot();
+        robot.name = "Boris";
+
+        System.out.println("Hi, my name is: " + robot.name);
+
+    }
+}
