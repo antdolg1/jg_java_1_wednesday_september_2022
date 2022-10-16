bfd4948cbe67230c6668ffd699446521301f76ca
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-16 12:14:02 +0200

    Task_9 Homework_1 Bug Fix

--- src/main/java/students/student_jurij_hlebnikov/lesson_2/homework/Task_9.java
+++ src/main/java/students/student_jurij_hlebnikov/lesson_2/homework/Task_9.java
@@ -9,28 +9,28 @@ public class Task_9 {
 
         Scanner scanner = new Scanner(System.in);
         int userNumber = scanner.nextInt();
-        int x1 = userNumber * 1;
-        int x2 = userNumber * 2;
-        int x3 = userNumber * 3;
-        int x4 = userNumber * 4;
-        int x5 = userNumber * 5;
-        int x6 = userNumber * 6;
-        int x7 = userNumber * 7;
-        int x8 = userNumber * 8;
-        int x9 = userNumber * 9;
-        int x10 = userNumber * 10;
+        int forX1 = userNumber * 1;
+        int forX2 = userNumber * 2;
+        int forX3 = userNumber * 3;
+        int forX4 = userNumber * 4;
+        int forX5 = userNumber * 5;
+        int forX6 = userNumber * 6;
+        int forX7 = userNumber * 7;
+        int forX8 = userNumber * 8;
+        int forX9 = userNumber * 9;
+        int forX10 = userNumber * 10;
 
-        System.out.println(userNumber + " x 1 = " + x1);
-        System.out.println(userNumber + " x 2 = " + x2);
-        System.out.println(userNumber + " x 3 = " + x3);
-        System.out.println(userNumber + " x 4 = " + x4);
-        System.out.println(userNumber + " x 5 = " + x5);
-        System.out.println(userNumber + " x 6 = " + x6);
-        System.out.println(userNumber + " x 7 = " + x7);
-        System.out.println(userNumber + " x 8 = " + x8);
-        System.out.println(userNumber + " x 8 = " + x8);
-        System.out.println(userNumber + " x 9 = " + x9);
-        System.out.println(userNumber + " x 10 = " + x10);
+        System.out.println(userNumber + " x 1 = " + forX1);
+        System.out.println(userNumber + " x 2 = " + forX2);
+        System.out.println(userNumber + " x 3 = " + forX3);
+        System.out.println(userNumber + " x 4 = " + forX4);
+        System.out.println(userNumber + " x 5 = " + forX5);
+        System.out.println(userNumber + " x 6 = " + forX6);
+        System.out.println(userNumber + " x 7 = " + forX7);
+        System.out.println(userNumber + " x 8 = " + forX8);
+        System.out.println(userNumber + " x 8 = " + forX8);
+        System.out.println(userNumber + " x 9 = " + forX9);
+        System.out.println(userNumber + " x 10 = " + forX10);
 
         System.out.println("Thank You");
 
