a5728ad76aed5f48e5b2c236389e092723942504
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-16 09:09:38 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/MyName.java
@@ -0,0 +1,13 @@
+package students.student_ignat_parfenov.lesson3.homework;
+
+public class MyName {
+
+    public static void main(String[] args){
+
+        String myName = "Ignat";
+        System.out.println(myName);
+
+        System.out.println("_______________________");
+        System.out.println("Hi " + myName + "!");
+    }
+}
---
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/MyNameScanner.java
@@ -0,0 +1,16 @@
+package students.student_ignat_parfenov.lesson3.homework;
+
+import java.util.Scanner;
+
+public class MyNameScanner {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String yourName = scanner.next();
+
+        System.out.println("Hello " + yourName + "!");
+
+    }
+}
