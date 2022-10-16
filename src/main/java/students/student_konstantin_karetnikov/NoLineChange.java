c3c681584c4363ff73bccae202aa5f108ee7aafd
konstantinkaretnikov <kskaretnikov@gmail.com>
2022-10-16 17:23:32 +0300

    Lesson 2 Level 5 done. Bug fixing done

--- src/main/java/students/student_konstantin_karetnikov/NoLineChange.java
+++ src/main/java/students/student_konstantin_karetnikov/NoLineChange.java
@@ -1,19 +1,10 @@
 package students.student_konstantin_karetnikov;
-
 import java.util.Scanner;
-
 public class NoLineChange {
-
-    private static java.lang.String String;
-
     public static void main(String[] args) {
-
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
             System.out.print("Please enter text in this line, without change to the next line: ");
-            String = scanner.nextLine();
-
+            scanner.nextLine();
             System.out.println("It's working!");
         }
-    
-
 }
