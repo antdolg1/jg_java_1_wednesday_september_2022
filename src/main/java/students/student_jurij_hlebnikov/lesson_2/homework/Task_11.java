014c889566a63918c65bd4e209e6b18e4b3bfc0e
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-16 12:06:17 +0200

    Task_11 Homework_1 Bug Fix

--- src/main/java/students/student_jurij_hlebnikov/lesson_2/homework/Task_11.java
+++ src/main/java/students/student_jurij_hlebnikov/lesson_2/homework/Task_11.java
@@ -6,13 +6,11 @@ public class Task_11 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         double numberOne = scanner.nextDouble();
         double numberTwo = scanner.nextDouble();
         double numberThree = scanner.nextDouble();
 
         System.out.println((numberOne + numberTwo + numberThree) / 3);
-
-
-
     }
 }
