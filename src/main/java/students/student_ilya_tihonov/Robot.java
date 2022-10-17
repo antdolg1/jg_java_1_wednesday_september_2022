befa52cff6798741857f1a498ca2af56b3574903
Ilya <ilya.tihonov@inbox.lv>
2022-10-18 00:40:47 +0300

    lesson_3_lvl_1.java

---
+++ src/main/java/students/student_ilya_tihonov/Robot.java
@@ -0,0 +1,13 @@
+package students.student_ilya_tihonov;
+
+public class Robot {
+
+    public Robot(){
+
+    }
+
+    void sayHello() { System.out.println("Hello!");}
+
+    public void sayYourName() {System.out.println("My name is ROBO"); }
+
+}
---
+++ src/main/java/students/student_ilya_tihonov/RobotDemo.java
@@ -0,0 +1,25 @@
+package students.student_ilya_tihonov;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot firstRobot = new Robot();
+
+        firstRobot.sayHello();
+        firstRobot.sayYourName();
+
+        Robot secondRobot = new Robot();
+
+        secondRobot.sayHello();
+        secondRobot.sayYourName();
+
+        Robot thirdRobot = new Robot();
+
+        thirdRobot.sayHello();
+        thirdRobot.sayYourName();
+
+    }
+}
+
+
