aeb30b0f6d2176b5b316d6490cd4931917f0a706
eigobud <igor.budnitski@ericsson.com>
2022-10-17 14:11:27 +0300

    homework - level5

---
+++ src/main/java/students/student_igor_budnitski/lesson_3/homework/level_5/Dog.java
@@ -0,0 +1,29 @@
+package students.student_igor_budnitski.lesson_3.homework.level_5;
+
+class Dog {
+    String name;
+    int age;
+    String color;
+
+    public Dog(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public void voice() {
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println("Dog name is " + name + " and he is " + age + " y.o.");
+        System.out.println(name + " color is " + color);
+    }
+
+    public void happyBirthday() {
+        age += 1;
+    }
+
+    public void changeColor(String newColor) {
+        this.color = newColor;
+    }
+}
