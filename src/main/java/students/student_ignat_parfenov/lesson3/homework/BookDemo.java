893a6bf46263c38745d73f3658392dc4c87e8b2d
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-17 12:57:48 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/BookDemo.java
@@ -0,0 +1,11 @@
+package students.student_ignat_parfenov.lesson3.homework;
+
+public class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+
+}
