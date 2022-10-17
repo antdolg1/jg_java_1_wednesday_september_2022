d4f2d9871ef7ed13744c4c1cd26a276c2eb2de89
amadsss <amads@ddc.lv>
2022-10-17 20:42:18 +0300

    add lesson3 level4 homework Task 14,15,16,17

---
+++ src/main/java/students/student_aleksandrs_mads/lesson_3/homework/level_4/Task_15/Phone.java
@@ -0,0 +1,14 @@
+package students.student_aleksandrs_mads.lesson_3.homework.level_4.Task_15;
+
+class Phone {
+    String model;
+
+    Phone(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
---
+++ src/main/java/students/student_aleksandrs_mads/lesson_3/homework/level_4/Task_15/PhoneDemo.java
@@ -0,0 +1,9 @@
+package students.student_aleksandrs_mads.lesson_3.homework.level_4.Task_15;
+
+class PhoneDemo {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+}
