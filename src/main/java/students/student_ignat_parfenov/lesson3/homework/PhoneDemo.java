45f62ad58826e31dd450076e16dc99c56271cbc0
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-17 13:04:12 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/PhoneDemo.java
@@ -0,0 +1,11 @@
+package students.student_ignat_parfenov.lesson3.homework;
+
+public class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
