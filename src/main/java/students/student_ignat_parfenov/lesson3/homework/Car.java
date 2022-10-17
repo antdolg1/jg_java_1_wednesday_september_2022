e242f93e4b7ae62cc1fc33c4f80f77b21a007110
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-17 13:19:40 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson3/homework/Car.java
@@ -0,0 +1,17 @@
+package students.student_ignat_parfenov.lesson3.homework;
+
+public class Car {
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+    public Car() {
+    }
+}
+
