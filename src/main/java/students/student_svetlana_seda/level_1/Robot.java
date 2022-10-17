41ca5b65555eb61fc65c7357d1141c88eae12470
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-17 21:26:18 +0300

    Homework - lesson3 level 1 done

---
+++ src/main/java/students/student_svetlana_seda/level_1/Robot.java
@@ -0,0 +1,24 @@
+package students.student_svetlana_seda.level_1;
+
+public class Robot {
+    String name;
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+        Robot(String name) {
+        this.name = name;
+        }
+    public void sayYourName() {
+        System.out.println("My name is " + this.name + ".");
+    }
+    public void sayByeBye(){
+        System.out.println("Bye!");
+    }
+
+ public Robot(){
+
+    }
+
+
+}
