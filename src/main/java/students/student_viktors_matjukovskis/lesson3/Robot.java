9dc9ca5f3c5d23c0f51ee588d005214454546997
viktors1989 <viktors@4k.lv>
2022-10-17 23:23:38 +0300

    Home work lesson3

---
+++ src/main/java/students/student_viktors_matjukovskis/lesson3/Robot.java
@@ -0,0 +1,16 @@
+package students.student_viktors_matjukovskis.lesson3;
+
+public class Robot {
+
+        public void sayHello() {
+            System.out.println("Hello");
+        }
+
+    public Robot() {
+
+    }
+
+    }
+
+
+
---
+++ src/main/java/students/student_viktors_matjukovskis/lesson3/RobotDemo.java
@@ -0,0 +1,17 @@
+package students.student_viktors_matjukovskis.lesson3;
+
+
+import teacher.lesson_3.lessoncode.Dog;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot();
+
+        robot1.talk = "Hello";
+
+    }
+
+    }
+
