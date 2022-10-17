9d91140427821d07a2cac9ea067f5b3172958e9e
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-17 11:48:15 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_2_hw_level_3_junior/Task11_ArithmeticMean.java
+++ src/main/java/students/student_ignat_parfenov/lesson_2_hw_level_3_junior/Task11_ArithmeticMean.java
@@ -2,9 +2,10 @@ package students.student_ignat_parfenov.lesson_2_hw_level_3_junior;
 
 import java.util.Scanner;
 
-public class Task11_ArithmeticMean {public static void main(String[] args) {
+public class Task11_ArithmeticMean {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter your first numbers : ");
         double num1 = scanner.nextDouble();
 
@@ -16,10 +17,10 @@ public class Task11_ArithmeticMean {public static void main(String[] args) {
 
         double[] nums = new double[]{num1, num2, num3};
 
-    for (double m : nums) {
+        for (double m : nums) {
 
+        }
+
+        System.out.println("Arithmetic mean is = " + (num1 + num2 + num3) / nums.length);
     }
-
-    System.out.println("Arithmetic mean is = " + (num1+num2+num3)/ nums.length);
-}
 }
