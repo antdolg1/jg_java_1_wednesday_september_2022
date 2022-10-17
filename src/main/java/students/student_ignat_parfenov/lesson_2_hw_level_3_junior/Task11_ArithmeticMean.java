466d4485813ddda0a5d3eff72c2e2f73a95bd067
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-17 12:19:26 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_2_hw_level_3_junior/Task11_ArithmeticMean.java
+++ src/main/java/students/student_ignat_parfenov/lesson_2_hw_level_3_junior/Task11_ArithmeticMean.java
@@ -8,19 +8,19 @@ public class Task11_ArithmeticMean {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter your first numbers : ");
         double num1 = scanner.nextDouble();
-
         System.out.print("Enter your second number : ");
         double num2 = scanner.nextDouble();
-
         System.out.print("Enter your third number : ");
         double num3 = scanner.nextDouble();
 
-        double[] nums = new double[]{num1, num2, num3};
+ //       double[] nums = new double[]{num1, num2, num3};
+ //       for (double m : nums) {  // Вопрос, Антон. Как ввести не просто цифру 3 (количество цифр) ниже в строке sout,
+ //       }                        //а именно длину nums.lenght чтобы система автоматически выполняла деление суммы
+                                    //на кол-во введенных значений вверху? for не подходит... а что?
+ //       System.out.println("Arithmetic mean is = " + (num1 + num2 + num3) / nums.length);
 
-        for (double m : nums) {
-
-        }
-
-        System.out.println("Arithmetic mean is = " + (num1 + num2 + num3) / nums.length);
+        System.out.println("Arithmetic mean is = " + (double)(num1 + num2 + num3)/3);
     }
 }
+
+
