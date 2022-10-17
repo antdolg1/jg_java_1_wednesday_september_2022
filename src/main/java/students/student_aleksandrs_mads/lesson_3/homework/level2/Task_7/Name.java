967f6132e34a08edb10f2bd2dd9174f5f6bba2b6
amadsss <amads@ddc.lv>
2022-10-17 18:58:31 +0300

    add lesson3 level2 homework

---
+++ src/main/java/students/student_aleksandrs_mads/lesson_3/homework/level2/Task_7/Name.java
@@ -0,0 +1,10 @@
+package students.student_aleksandrs_mads.lesson_3.homework.level2.Task_7;
+
+public class Name {
+    public static void main(String[] args) {
+        String myName = "Aleksandrs";
+        System.out.println(myName);
+    }
+
+
+}
