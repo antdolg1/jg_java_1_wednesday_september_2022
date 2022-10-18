b4349ff1f6077b1b77e370a753e5fddf7f332669
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-18 19:35:17 +0200

    Task_19

--- src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/Dog.java
+++ src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/Dog.java
@@ -3,9 +3,11 @@ package students.student_jurij_hlebnikov.lesson_3.homework;
 public class Dog {
 
     String dogName;
+    int howOld;
 
-    Dog(String dogName){
+    Dog(String dogName, int howOld){
         this.dogName = dogName;
+        this.howOld = howOld;
     }
 
     public Dog(){
@@ -13,6 +15,7 @@ public class Dog {
 
     public void dogVoice(){
         System.out.println(dogName.repeat(3));
+        System.out.println(howOld + " Years Old");
     }
 
 }
--- src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/DogDemo.java
+++ src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/DogDemo.java
@@ -3,7 +3,7 @@ package students.student_jurij_hlebnikov.lesson_3.homework;
 public class DogDemo {
     public static void main(String[] args){
 
-        Dog sobaka = new Dog("Alfa");
+        Dog sobaka = new Dog("Alfa ",3);
         sobaka.dogVoice();
 
     }
