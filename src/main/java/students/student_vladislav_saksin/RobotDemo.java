6079dfe5e47246e04b4355fb0767013a4e3581c3
Vlad-Rix <vladislavs.saksins@ya.ru>
2022-10-18 12:35:38 +0300

    Task 1-6

---
+++ src/main/java/students/student_vladislav_saksin/RobotDemo.java
@@ -0,0 +1,19 @@
+package students.student_vladislav_saksin;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot();
+        myRobot.sayHello();
+        myRobot.sayYourName();
+
+        Robot robot1 = new Robot();
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+    }
+
+}
