79bb0e9c17dd6d30e5f4d394dfb95ba7918bfbe3
Manakids <2maxx2@inbox.lv>
2022-10-19 00:05:02 +0300

    lesson_3_homework_level_4

---
+++ src/main/java/students/student_maksims_cerkasovs/lesson_3/level_4/Book.java
@@ -0,0 +1,14 @@
+package students.student_maksims_cerkasovs.lesson_3.level_4;
+
+public class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+}
