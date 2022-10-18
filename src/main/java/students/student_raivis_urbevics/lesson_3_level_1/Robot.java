cf0cc4bee2b11115252e0bdb7b0823c14df4586a
Raivuha <onemoreraivuha@gmail.com>
2022-10-18 14:13:01 +0300

    add homework lesson- 3 level-1

---
+++ src/main/java/students/student_raivis_urbevics/lesson_3_level_1/Robot.java
@@ -0,0 +1,19 @@
+package students.student_raivis_urbevics.lesson_3_level_1;
+
+public class Robot {
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+
+
+
+
+
+
+
+
+    }
+
---
+++ src/main/java/students/student_raivis_urbevics/lesson_3_level_1/RobotDemo.java
@@ -0,0 +1,20 @@
+package students.student_raivis_urbevics.lesson_3_level_1;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot robot=new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+
+
+        Robot robot1=new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+
+        Robot robot2=new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+
+    }
+
+}
