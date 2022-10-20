daaa6e755dec13afee11bcebeeba5621f2b3e106
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-20 18:44:59 +0300

    Homework - lesson2 level 4 done

--- src/main/java/students/student_svetlana_seda/lesson_1/HelloWorld.java
+++ src/main/java/students/student_svetlana_seda/lesson_1/HelloWorld.java
@@ -1,11 +1,12 @@
 package students.student_svetlana_seda.lesson_1;
 
-public class Lesson1Hello_World {
+public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
 
     }
 }
+
 
 
--- src/main/java/students/student_svetlana_seda/lesson_2/level_4/Main.java
+++ src/main/java/students/student_svetlana_seda/lesson_2/level_4/Main.java
@@ -2,14 +2,11 @@ package students.student_svetlana_seda.lesson_2.level_4;
 
 public class Main {
 
-<<<<<<< HEAD
-        public static void main(String[] args) {
-            System.out.println("Privet, ja zapustilsja :)");
-        }
-=======
+
     public static void main(String[] args) {
+
         System.out.println("Privet, ja zapustilsja :)");
     }
->>>>>>> master
+
 
 }
