01692ff1f52af58ce476be77499f6b1a66799ee0
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-20 15:50:13 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/lessoncode/HumanAgeClassifier.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/lessoncode/HumanAgeClassifier.java
@@ -1,1 +1,20 @@
+package students.student_ignat_parfenov.lesson_4.lessoncode;
 
+public class HumanAgeClassifier {
+
+    public String clacify(int age) {
+        if (age > 0 && age <= 10) {
+            return "Baby";
+        } else if (age > 10 && age <= 17) {
+            return "Teenager";
+        } else if (age > 17 && age <= 30) {
+            return "Student";
+        } else if (age > 30 && age <= 60) {
+            return "Depressive hard working person";
+        } else if (age > 60 && age <= 129) {
+            return "Happy retired person";
+        } else {
+            return "Wooow, you're undead!";
+        }
+    }
+}
