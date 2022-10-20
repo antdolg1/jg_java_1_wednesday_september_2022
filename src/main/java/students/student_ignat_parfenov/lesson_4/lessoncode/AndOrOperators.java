5669fb637b10a9f398a59e2ab8634a929b25fc31
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-20 14:27:34 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/lessoncode/AndOrOperators.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/lessoncode/AndOrOperators.java
@@ -1,9 +1,24 @@
 package students.student_ignat_parfenov.lesson_4.lessoncode;
 
+import java.util.Scanner;
+
 public class AndOrOperators {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        System.out.print("Please input month number: ");
+        Scanner scanner = new Scanner(System.in);
 
+        int monthNumber = scanner.nextInt();
+
+        if (monthNumber == 12 || monthNumber == 1 || monthNumber == 2) {
+            System.out.println("WINTER");
+        } else if (monthNumber == 3 || monthNumber == 4 || monthNumber == 5) {
+            System.out.println("SPRING");
+        } else if (monthNumber == 6 || monthNumber == 7 || monthNumber == 8) {
+            System.out.println("SUMMER");
+        } else if (monthNumber == 9 || monthNumber == 10 || monthNumber == 11) {
+            System.out.println("AUTUMN");
+        }
     }
 }
