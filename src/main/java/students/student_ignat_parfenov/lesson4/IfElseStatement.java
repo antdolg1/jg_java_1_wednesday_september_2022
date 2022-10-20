41f816882af530817d01c70989eec27e8cdd6004
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-20 11:28:20 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson4/IfElseStatement.java
+++ src/main/java/students/student_ignat_parfenov/lesson4/IfElseStatement.java
@@ -4,12 +4,14 @@ public class IfElseStatement {
 
     public static void main(String[] args){
 
-        int number = 0;
+        int number = 7;
 
         if (number > 1) {
             System.out.println("Number is greater than 1");
+        } else if (number == 0) {
+            System.out.println("Number is less than 0!");
+        } else if (number > 5) {
+            System.out.println("Number is bigger than 5");
         }
-
-
     }
 }
