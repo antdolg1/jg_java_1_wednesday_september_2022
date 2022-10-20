e763042efaf175e502bc8eaeb34060294052e9ff
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-20 11:08:01 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson4/AndOrOperator.java
+++ src/main/java/students/student_ignat_parfenov/lesson4/AndOrOperator.java
@@ -1,23 +1,3 @@
-package students.student_ignat_parfenov.lesson4;
-
-import java.util.Scanner;
-
-public class AndOrOperator {
-
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        int mounthNumber = scanner.nextInt();
-
-        if (mounthNumber = mounthNumber || 12 = 1 || mounthNumber = 2) {
-            System.out.println("WHINTER");
-        } else if (mounthNumber = 3 || mounthNumber = 4 || mounthNumber = 5) {
-            System.out.println("SPRING");
-        } else if (mounthNumber = 6 || mounthNumber = 7 || mounthNumber = 8) {
-            System.out.println("SUMMER");
-        } else if (mounthNumber = 9 || mounthNumber = 10 || mounthNumber = 11) System.out.println("AUTUMN");
-
-
 
 
 
