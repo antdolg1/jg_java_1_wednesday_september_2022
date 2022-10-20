5de1e5f43203d08402d50d185ee635f3996eb406
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-20 19:41:20 +0300

    Homework - Lesson 1 done

---
+++ src/main/java/students/student_svetlana_seda/lesson_2/lesson_1/HelloWorld.java
@@ -0,0 +1,9 @@
+package students.student_svetlana_seda.lesson_2.lesson_1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
