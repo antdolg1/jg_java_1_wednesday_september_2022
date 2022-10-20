68d48371f72793ae08c7d0aed909ca05f8b5b9e9
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-20 13:08:58 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson4/SwitchStatement.java
+++ src/main/java/students/student_ignat_parfenov/lesson4/SwitchStatement.java
@@ -10,22 +10,47 @@ public class SwitchStatement {
         Scanner scanner = new Scanner(System.in);
         int level = scanner.nextInt();
 
-        if (level == 1) {
-            System.out.println("Intern");
-        } else if (level == 2) {
-            System.out.println("Junior");
-        } else if (level == 3) {
-            System.out.println("Middle");
-        } else if (level == 4) {
-            System.out.println("Senior");
-        } else if (level == 5) {
-            System.out.println("Architect");
-        } else if (level == 6){
-            System.out.println("CTO");
-        } else {
-            System.out.println("Input the correct number");
+//        if (level == 1) {
+//            System.out.println("Intern");
+//        } else if (level == 2) {
+//            System.out.println("Junior");
+//        } else if (level == 3) {
+//            System.out.println("Middle");
+//        } else if (level == 4) {
+//            System.out.println("Senior");
+//        } else if (level == 5) {
+//            System.out.println("Architect");
+//        } else if (level == 6){
+//            System.out.println("CTO");
+//        } else {
+//            System.out.println("Input the correct number");
+//        }
+//    }
+
+        switch (level) {
+            case 1:
+                System.out.println("Intern");
+                break;
+            case 2:
+                System.out.println("Junior");
+                break;
+            case 3:
+                System.out.println("Middle");
+                break;
+            case 4:
+                System.out.println("Senior");
+                break;
+            case 5:
+                System.out.println("Architect");
+                break;
+            case 6:
+                System.out.println("CTO");
+                break;
+            default:
+                System.out.println("Input the correct number");
         }
     }
 }
+
 
 
