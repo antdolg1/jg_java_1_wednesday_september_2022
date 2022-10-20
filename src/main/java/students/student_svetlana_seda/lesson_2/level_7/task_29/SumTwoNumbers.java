ccd38e431d8f993f5cc57f966c46392d98a98c49
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-20 18:50:07 +0300

    Homework - lesson2 level 7 done

--- src/main/java/students/student_svetlana_seda/lesson_2/level_7/task_29/SumTwoNumbers.java
+++ src/main/java/students/student_svetlana_seda/lesson_2/level_7/task_29/SumTwoNumbers.java
@@ -6,13 +6,14 @@ public class SumTwoNumbers {
 
         int numberOne = 10;
         int numberTwo = 20;
-            System.out.println("Number 1 = " + numberOne);
-            System.out.println("Number 2 = " + numberTwo);
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
 
         int sum = numberOne + numberTwo;
 
-            System.out.println("Sum = " + sum);
+        System.out.println("Sum = " + sum);
 
     }
 
 }
+
--- src/main/java/students/student_svetlana_seda/lesson_2/level_7/task_32/SingleMultiline.java
+++ src/main/java/students/student_svetlana_seda/lesson_2/level_7/task_32/SingleMultiline.java
@@ -5,8 +5,9 @@ public class SingleMultiline {
     public static void main(String[] args) {
 
         System.out.println("Singleline comment");
-            System.out.println("My name \n" + "is \n" + "Lana \n");
+        System.out.println("My name \n" + "is \n" + "Lana \n");
 
     }
 
 }
+
