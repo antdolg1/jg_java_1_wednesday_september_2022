dc902e8edd5d96d040788b41d3d3a16ea5fecab9
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-20 16:08:03 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/lessoncode/HumanAgeClassifierTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/lessoncode/HumanAgeClassifierTest.java
@@ -2,13 +2,25 @@ package students.student_ignat_parfenov.lesson_4.lessoncode;
 
 public class HumanAgeClassifierTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         HumanAgeClassifierTest test = new HumanAgeClassifierTest();
 
     }
 
-    void shouldClassifyBaby(){
+    void shouldClassifyBaby() {
+        HumanAgeClassifier victim = new HumanAgeClassifier();
+
+        int age = 2;
+        String expectedResult = "Baby";
+
+        String actualResult = victim.classify(age);
+
+        if (expectedResult.equals(actualResult) {
+            System.out.println("Test shouldClassifyBaby IS PASSED! ");
+        } else {
+            System.out.println(" Test shouldClassifyBaby is FAILED");
+        }
 
     }
 }
