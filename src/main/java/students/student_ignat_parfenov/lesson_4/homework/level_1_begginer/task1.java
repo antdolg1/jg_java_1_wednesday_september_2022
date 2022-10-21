e8b5370ba380f8a8f82551499f096207de2ab19b
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-21 11:34:07 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_1_begginer/task1.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_1_begginer/task1.java
@@ -1,9 +1,19 @@
 package students.student_ignat_parfenov.lesson_4.homework.level_1_begginer;
 
+import java.util.Scanner;
+
 public class task1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        System.out.println("Please input any number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
+        if (number >= 0){
+            System.out.println("Entered number is positive");
+        } else {
+            System.out.println("Entered number is negative");
+        }
     }
 }
