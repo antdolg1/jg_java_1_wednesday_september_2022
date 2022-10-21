3e22afb548c19daa343deec4535045efbb6130a0
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-21 15:07:55 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_3_junior/Task8.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_3_junior/Task8.java
@@ -1,8 +1,23 @@
 package students.student_ignat_parfenov.lesson_4.homework.level_3_junior;
 
+import java.util.Scanner;
+
 public class Task8 {
 
     public static void main(String[] args) {
 
+        double a, b, c;
+
+        System.out.println("Please enter three numbers: ");
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
+
+        if (a == b && b == c) {
+            System.out.println("All numbers are equal");
+        } else if (a != b || b != c ){
+            System.out.println("All numbers are different");
+        }
     }
 }
