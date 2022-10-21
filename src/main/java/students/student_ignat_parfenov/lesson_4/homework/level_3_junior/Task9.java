6617cbbbb07c46a4a278cd2b1a3d4ad79304fc6b
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-21 15:50:15 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_3_junior/Task9.java
@@ -0,0 +1,20 @@
+package students.student_ignat_parfenov.lesson_4.homework.level_3_junior;
+
+import java.util.Scanner;
+
+public class Task9 {
+
+    public static void main(String[] args){
+
+        double a, b, c;
+
+        System.out.println("Please enter three numbers");
+
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
+
+        
+    }
+}
