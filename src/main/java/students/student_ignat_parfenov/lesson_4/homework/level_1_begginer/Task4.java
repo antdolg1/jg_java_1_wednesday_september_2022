b20969345b641a40bf394d731df282be67d9e109
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-21 13:21:46 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_1_begginer/Task4.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_1_begginer/Task4.java
@@ -1,4 +1,22 @@
 package students.student_ignat_parfenov.lesson_4.homework.level_1_begginer;
 
+import java.util.Scanner;
+
 public class Task4 {
+
+    public static void main(String[] args) {
+
+        int number3;
+        System.out.println("Please enter your number: ");
+        Scanner scanner = new Scanner(System.in);
+        number3 = scanner.nextInt();
+
+        if (number3 % 2 == 0) {
+            System.out.println("Your entered number is even number");
+        } else {
+            System.out.println("Your entered number is uneven number");
+        }
+    }
+
 }
+
