793b18cd5a86b2832efaf7756caf7c1755091a04
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-21 15:57:21 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_3_junior/Task10.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_3_junior/Task10.java
@@ -2,19 +2,22 @@ package students.student_ignat_parfenov.lesson_4.homework.level_3_junior;
 
 import java.util.Scanner;
 
-public class Task9 {
+public class Task10 {
 
     public static void main(String[] args) {
 
         double a, b, c;
 
-        System.out.println("Please enter three numbers");
-
+        System.out.println("Please enter three numbers: ");
         Scanner scanner = new Scanner(System.in);
         a = scanner.nextDouble();
         b = scanner.nextDouble();
         c = scanner.nextDouble();
 
+        if (a > b && b > c){
+            System.out.println("The largest number is " + a);
+        } else if () {
 
+        }
     }
 }
