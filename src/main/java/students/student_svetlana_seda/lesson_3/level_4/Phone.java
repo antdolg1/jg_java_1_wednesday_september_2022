ed7fae9a15170fb555ae13544d8a8dc0db2f95f4
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-21 19:22:05 +0300

    Homework - Lesson 3 level 4 - done too - please check

---
+++ src/main/java/students/student_svetlana_seda/lesson_3/level_4/Phone.java
@@ -0,0 +1,17 @@
+package students.student_svetlana_seda.lesson_3.level_4;
+
+public class Phone {
+
+    String model;
+
+
+        Phone(String newModel) {
+        this.model = newModel;
+    }
+
+        String getModel () {
+        return this.model;
+    }
+
+}
+
---
+++ src/main/java/students/student_svetlana_seda/lesson_3/level_4/PhoneDemo.java
@@ -0,0 +1,13 @@
+package students.student_svetlana_seda.lesson_3.level_4;
+
+public class PhoneDemo {
+
+    public static void main(String[] args) {
+
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+
+    }
+
+}
