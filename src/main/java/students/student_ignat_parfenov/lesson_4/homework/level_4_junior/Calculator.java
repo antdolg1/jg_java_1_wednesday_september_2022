048e3b2acf86e1879149d1a498d485435d8c4545
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-22 13:44:40 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_4_junior/Calculator.java
@@ -0,0 +1,13 @@
+package students.student_ignat_parfenov.lesson_4.homework.level_4_junior;
+
+public class Calculator {
+
+    class Calculator {
+
+        public int sum(int firstNumber, int secondNumber) {
+            return firstNumber + secondNumber;
+            return firstNumber - secondNumber;
+            return firstNumber * secondNumber;
+            return firstNumber / secondNumber;
+        }
+    }
