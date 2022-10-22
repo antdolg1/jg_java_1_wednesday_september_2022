f9f33d8146cba7347dfd3c787bef1b5f893b5062
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-22 20:33:20 +0300

    Lesson 4, Level 4,Task_11 add Calculator method

---
+++ src/main/java/students/student_ilja_morozovs/lesson_4/homework/level_4/Task_11/Calculator.java
@@ -0,0 +1,16 @@
+package students.student_ilja_morozovs.lesson_4.homework.level_4.Task_11;
+
+class Calculator {
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    public int sub(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+    public int inc(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+    public int dec(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
