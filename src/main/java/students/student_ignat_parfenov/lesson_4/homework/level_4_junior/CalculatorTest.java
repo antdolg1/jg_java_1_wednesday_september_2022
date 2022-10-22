08f3530f6a6a738c806c1caf2a68a2e3daf6c486
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-22 14:00:43 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_4_junior/CalculatorTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_4_junior/CalculatorTest.java
@@ -2,8 +2,31 @@ package students.student_ignat_parfenov.lesson_4.homework.level_4_junior;
 
 public class CalculatorTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        CalculatorTest calculatorTest = new CalculatorTest();
+        calculatorTest.sumTest();
+        calculatorTest.subtractionTest();
+        calculatorTest.multiplicationTest();
+        calculatorTest.divisionTest();
+
+    }
 
 
+    private void sumTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+    }
+
+    private void subtractionTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+
+    }
+
+    private void multiplicationTest() {
+    }
+
+    private void divisionTest() {
     }
 }
