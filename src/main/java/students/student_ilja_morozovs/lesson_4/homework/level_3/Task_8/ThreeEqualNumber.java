372ccdd4f401f8f26516cccf7541fca9377b57eb
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-22 14:16:13 +0300

    Lesson 4, Level 3,Task_8 program who compare tree numbers

---
+++ src/main/java/students/student_ilja_morozovs/lesson_4/homework/level_3/Task_8/ThreeEqualNumber.java
@@ -0,0 +1,22 @@
+package students.student_ilja_morozovs.lesson_4.homework.level_3.Task_8;
+
+import java.util.Scanner;
+
+public class ThreeEqualNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter number one: ");
+        int userNumber1 = scanner.nextInt();
+        System.out.println("Please enter number two: ");
+        int userNumber2 = scanner.nextInt();
+        System.out.println("Please enter number three: ");
+        int userNumber3 = scanner.nextInt();
+        if (userNumber1 == userNumber2 && userNumber1 == userNumber3) {
+            System.out.println("All numbers are equal");
+        } else if (userNumber1 != userNumber2 && userNumber1 != userNumber3 && userNumber2 != userNumber3) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
