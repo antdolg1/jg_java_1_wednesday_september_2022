773b86ccc9809087213ff759d2c5ef3992c07cd0
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-22 09:25:37 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_3_junior/Task9.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_3_junior/Task9.java
@@ -3,18 +3,22 @@ package students.student_ignat_parfenov.lesson_4.homework.level_3_junior;
 import java.util.Scanner;
 
 public class Task9 {
-
     public static void main(String[] args) {
 
         double a, b, c;
 
-        System.out.println("Please enter three numbers");
-
+        System.out.println("Please enter three different numbers: ");
         Scanner scanner = new Scanner(System.in);
         a = scanner.nextDouble();
         b = scanner.nextDouble();
         c = scanner.nextDouble();
 
-
+        if (a > b && b >= c) {
+            System.out.println("Number are increasing");
+        } else if (c > b && b >= a) {
+            System.out.println("Numbers are decreasing");
+        } else {
+            System.out.println("Enter please three different!! numbers");
+        }
     }
 }
