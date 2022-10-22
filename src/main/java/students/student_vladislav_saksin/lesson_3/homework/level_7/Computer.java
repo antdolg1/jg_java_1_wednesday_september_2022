ab96db53d169d66985a536742968fb34b97e33f3
Vlad-Rix <vladislavs.saksins@ya.ru>
2022-10-22 16:00:03 +0300

    Lesson 3 Level 7 Tasks 28, 30 - 33

---
+++ src/main/java/students/student_vladislav_saksin/lesson_3/homework/level_7/Computer.java
@@ -0,0 +1,19 @@
+package students.student_vladislav_saksin.lesson_3.homework.level_7;
+
+class Computer {
+
+    String manufacturer;
+    String model;
+
+    Computer(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+    String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+}
