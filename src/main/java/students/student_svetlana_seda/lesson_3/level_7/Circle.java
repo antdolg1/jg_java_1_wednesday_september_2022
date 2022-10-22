fb77968a47dc79b893b3333dc5c39ba95098dca8
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-22 13:48:08 +0300

    Homework - Lesson_3 level_7 - task_30 done

---
+++ src/main/java/students/student_svetlana_seda/lesson_3/level_7/Circle.java
@@ -0,0 +1,14 @@
+package students.student_svetlana_seda.lesson_3.level_7;
+
+public class Circle {
+    double radius;
+    double areaNumber;//r*r
+    double calculateArea;
+    double pi;
+
+    void calculateArea() {
+        System.out.println("Circle area is " + (pi * areaNumber) + " meters.");
+
+    }
+
+}
---
+++ src/main/java/students/student_svetlana_seda/lesson_3/level_7/CircleDemo.java
@@ -0,0 +1,16 @@
+package students.student_svetlana_seda.lesson_3.level_7;
+
+public class CircleDemo {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle();
+        circle.radius = 7.5;
+        circle.pi = 3.14;
+        circle.areaNumber = 56.25;//radius*radius
+
+        circle.calculateArea();
+
+    }
+
+}
