c0d4feae91b5e9a9b4e4a3ce6443b676c59fca22
TheStewe <s.veveris@gmail.com>
2022-10-23 19:17:44 +0300

    Commit level 7.

---
+++ src/main/java/students/student_stivens_veveris/lesson3/level7/Circle.java
@@ -0,0 +1,11 @@
+package students.student_stivens_veveris.lesson3.level7;
+
+public class Circle {
+
+    double radius = 5.525;
+
+    void calculateArea(){System.out.println("Circle Area = " + Math.PI * (radius * radius));}
+
+    public Circle (){
+    }
+}
