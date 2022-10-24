8fe82f19d570f4824fa28186b17d9d0b0b79ee93
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-24 14:53:00 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_6_middle/Stock.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_6_middle/Stock.java
@@ -2,10 +2,24 @@ package students.student_ignat_parfenov.lesson_4.homework.level_6_middle;
 
 public class Stock {
 
-    private String name = "Globe Ltd.";
-    private int initialValue = 235000;
-    private int currentValue;
-    private int minimumCost;
-    private int maximumCost;
+//    private String name = "Globe Ltd.";
+//    private int initialValue = 235000;
+//    private int currentValue;
+//    private int minimumCost;
+//    private int maximumCost;
+
+    public String name = "GOOG";
+    public int currenValue = 10;
+
+    Stock google = new Stock("GOOG", 10);
+    String priceInformation = google.getPriceInformation();
+    System.out.println(priceInformation);
+
+    google.updatePrice(15);
+    google.updatePrice(7);
+    google.updatePrice(14);
+
+    priceInformation = google.getPriceInformation();
+    System.out.println(priceInformation);
 
 }
