5aca99891bec823c6bc6f8f5ba8592ca4d803398
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-24 13:51:11 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_6_middle/Stock.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_6_middle/Stock.java
@@ -2,9 +2,9 @@ package students.student_ignat_parfenov.lesson_4.homework.level_6_middle;
 
 public class Stock {
 
-    String name = "Company name";
-    int currentValue;
-    int minimumCost;
-    int maximumCost;
+    private String name = "Company name";
+    private int currentValue;
+    private int minimumCost;
+    private int maximumCost;
 
 }
