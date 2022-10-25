dbf784527a9d3a994298382b5c7f2187640a79b7
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-25 17:11:30 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_7_senior/FizzBuzz.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_7_senior/FizzBuzz.java
@@ -1,15 +1,13 @@
 package students.student_ignat_parfenov.lesson_4.homework.level_7_senior;
 
-class FizzBuzz {
+public class FizzBuzz {
 
     public String detect(int number) {
 
-        if (number / 3 && number % == 0) {
+        if (number % 3 == 0) {
             return "Fizz";
-        } else if ((number / 5) && (number % == 0)) {
+        } else if (number % 5 == 0) {
             return "Buzz";
-        } else if (number / 3 && number / 5 && number % == 0) {
-            return "FizzBuzz";
         } else {
             return "number";
         }
---
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_7_senior/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package students.student_ignat_parfenov.lesson_4.homework.level_7_senior;
+
+public class FizzBuzzTest {
+
+    public static void main(String[] args) {
+
+        FizzBuzzTest test = new FizzBuzzTest();
+        {
+
+        }
+    }
+}
