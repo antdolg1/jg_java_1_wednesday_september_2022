25597fa7d5ad36837e7b47b5cf0bc55ec22e37e1
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-25 17:17:40 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_7_senior/FizzBuzzTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_7_senior/FizzBuzzTest.java
@@ -5,8 +5,22 @@ public class FizzBuzzTest {
     public static void main(String[] args) {
 
         FizzBuzzTest test = new FizzBuzzTest();
-        {
+        test.FizzBuzzTestIsDivisionByThree();
 
+    }
+
+            void FizzBuzzTestIsDivisionByThree() {
+
+            FizzBuzz victim = new FizzBuzz();
+            int number = 12;
+            String expectedResult = "Fizz";
+
+            String actualResult = victim.detect(number);
+
+            if (expectedResult.equals(actualResult)) {
+                System.out.println("Fizz!");
+            } else {
+                System.out.println("Test is FAILED");
+            }
         }
     }
-}
