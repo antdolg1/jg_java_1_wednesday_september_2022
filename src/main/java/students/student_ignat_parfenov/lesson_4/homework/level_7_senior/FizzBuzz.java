d6d69e1a0583e8931cbddfbf9c34c2bbbe764528
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-25 18:21:44 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_7_senior/FizzBuzz.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_7_senior/FizzBuzz.java
@@ -8,7 +8,7 @@ public class FizzBuzz {
             return "Fizz";
         } else if (number % 5 == 0) {
             return "Buzz";
-        } else if ((number % 3 != 1) && (number % 5 != 1)) {
+        } else if ((number % 3 == 0) && (number % 5 == 0)) {
             return "FizzBuzz";
         } else {
             return "number";
