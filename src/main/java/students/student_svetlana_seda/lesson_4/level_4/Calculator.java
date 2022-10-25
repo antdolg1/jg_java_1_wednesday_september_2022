eb0a8ea04fd8ac90a308a27bfe4afb476094b9c2
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-25 21:01:56 +0300

    Homework of Lesson_4_level_4_task_11

---
+++ src/main/java/students/student_svetlana_seda/lesson_4/level_4/Calculator.java
@@ -0,0 +1,22 @@
+package students.student_svetlana_seda.lesson_4.level_4;
+
+public class Calculator {
+
+    public int sum (int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int mult (int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int subtr (int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int div(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+    // реализуйте по аналогии оставшиеся методы
+
+}
