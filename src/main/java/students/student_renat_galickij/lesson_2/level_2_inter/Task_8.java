4a0168ed6da254c2c55337932c834ef1ddd52d70
Renat <renat.galickij@gmail.com>
2022-10-26 22:03:14 +0300

    Task 8 and 9. Lesson 3

--- src/main/java/students/student_renat_galickij/lesson_2/level_2_inter/Task_8.java
+++ src/main/java/students/student_renat_galickij/lesson_2/level_2_inter/Task_8.java
@@ -1,13 +1,14 @@
 package students.student_renat_galickij.lesson_2.level_2_inter;
+
 import java.util.Scanner;
 
 public class Task_8 {
     public static void main(String[] args) {
-         String name;
+        String name;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
-       name = scanner.nextLine();
-       System.out.println("Hello " + name);
+        name = scanner.nextLine();
+        System.out.println("Hello " + name);
 
 
     }
---
+++ src/main/java/students/student_renat_galickij/lesson_4/level_1_intern/Task_1.java
@@ -0,0 +1,20 @@
+package students.student_renat_galickij.lesson_4.level_1_intern;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter integer");
+        int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("Number is positive ");
+            else if (number > 0) {
+                System.out.println("Number is negative");
+            }
+        }
+
+
+    }
+}
