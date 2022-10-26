1c1484f5f1995f785e251d4fe9bbc8443234fc0c
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-26 13:47:06 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_6_middle/StockTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_6_middle/StockTest.java
@@ -6,17 +6,37 @@ public class StockTest {
 
         StockTest test = new StockTest();
         test.testSeventySeven();
+        test.nineNinetyNineGreater();
 
     }
 
     void testSeventySeven() {
-    Stock Roche = new Stock("Roche", 10, 10, 10);
+        Stock Roche = new Stock("Roche", 10, 10, 10);
 
-    Roche.getPriceInformation();
-    Roche.updatePrice(12);
-    Roche.getPriceInformation();
-    Roche.updatePrice(5);
-
+        Roche.getPriceInformation();
+        Roche.updatePrice(12);
+        Roche.getPriceInformation();
+        Roche.updatePrice(5);
+        Roche.getPriceInformation();
+        Roche.updatePrice(7);
+        Roche.getPriceInformation();
+        Roche.updatePrice(99);
+        Roche.getPriceInformation();
+        Roche.updatePrice(77);
+        Roche.getPriceInformation();
 
     }
+
+    void nineNinetyNineGreater() {
+        int expectedResult = 999;
+        Stock Roche = new Stock("Roche", 10, 10, 10);
+        Roche.updatePrice(999);
+        int realResult = Roche.updatePrice(Roche.maximumPrice);
+        if (realResult == expectedResult) {
+            System.out.println("999 Greater test is OK");
+        } else {
+            System.out.println("999 Greater test is FAIL");
+        }
+    }
 }
+
