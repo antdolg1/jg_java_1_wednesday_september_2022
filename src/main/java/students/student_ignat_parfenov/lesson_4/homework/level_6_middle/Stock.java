291ad97f5849b7f2a4681e434ff8d8e58ee3e756
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-26 14:29:38 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_6_middle/Stock.java
+++ src/main/java/students/student_ignat_parfenov/lesson_4/homework/level_6_middle/Stock.java
@@ -3,13 +3,13 @@ package students.student_ignat_parfenov.lesson_4.homework.level_6_middle;
 public class Stock {
 
     String companyName;
-    int currentCost;
+    int price;
     int minimumPrice;
     int maximumPrice;
 
     public Stock(String companyName, int price, int minimumPrice, int maximumPrice) {
         this.companyName = companyName;
-        this.currentCost = price;
+        this.price = price;
         this.minimumPrice = minimumPrice;
         this.maximumPrice = maximumPrice;
     }
