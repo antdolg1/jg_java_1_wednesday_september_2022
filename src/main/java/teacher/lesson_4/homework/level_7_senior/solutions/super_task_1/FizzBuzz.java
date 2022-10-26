14dad8e3284412daa988b8ca1861465ac3544477
Evroleg <evroleg@gmail.com>
2022-10-26 23:40:20 +0300

    no comment

--- src/main/java/teacher/lesson_4/homework/level_7_senior/solutions/super_task_1/FizzBuzz.java
+++ src/main/java/teacher/lesson_4/homework/level_7_senior/solutions/super_task_1/FizzBuzz.java
@@ -1,15 +1,25 @@
+package teacher.lesson_4.homework.level_7_senior.solutions.super_task_1;
 
-class Calculator {
+public class FizzBuzz {
 
-    public int sum(int firstNumber, int secondNumber) {
-        return firstNumber + secondNumber;
+    public String detect(int number){
+        if (isDividedBy3(number) && isDividedBy5(number)) {
+            return "FizzBuzz";
+        }
+        if(isDividedBy3(number)) {
+            return "Fizz";
+        }
+        if(isDividedBy5(number)) {
+            return "Buzz";
+        }
+        return "" + number;
     }
 
     private boolean isDividedBy3(int number) {
-    	return number % 3 == 0;
-	}
+        return number % 3 == 0;
+    }
 
-	private boolean isDividedBy5(int number) {
-		return number % 5 == 0;
-	}
+    private boolean isDividedBy5(int number) {
+        return number % 5 == 0;
+    }
 }
