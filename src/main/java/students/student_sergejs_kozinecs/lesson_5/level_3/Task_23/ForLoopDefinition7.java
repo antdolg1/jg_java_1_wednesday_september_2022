edede1da972ad6b00a9f0d8146b87c313c98637e
Sergejs Kozinecs <sergei.kozinecs@gmail.com>
2022-10-27 22:34:19 +0300

    Lesson 5 level 3 done

---
+++ src/main/java/students/student_sergejs_kozinecs/lesson_5/level_3/Task_23/ForLoopDefinition7.java
@@ -0,0 +1,11 @@
+package students.student_sergejs_kozinecs.lesson_5.level_3.Task_23;
+
+class ForLoopDefinition7 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ src/main/java/students/student_sergejs_kozinecs/lesson_5/level_3/Task_24/ForLoopDefinition8.java
@@ -0,0 +1,11 @@
+package students.student_sergejs_kozinecs.lesson_5.level_3.Task_24;
+
+class ForLoopDefinition8 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
