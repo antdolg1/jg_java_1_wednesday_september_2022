2721dc6d0de3c1edd8826cadd012e8eb3d9f5139
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-27 13:34:14 +0300

    Lesson_5 level_2 Task_12 remove public

--- src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_2/Task_12/RandomNumberArray.java
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_2/Task_12/RandomNumberArray.java
@@ -1,6 +1,6 @@
 package students.student_ilja_morozovs.lesson_5.homework.level_2.Task_12;
 
-public class RandomNumberArray {
+class RandomNumberArray {
     public static void main(String[] args) {
         int[] numbers = new int[5];
         for (int randomNumbers : numbers) {
