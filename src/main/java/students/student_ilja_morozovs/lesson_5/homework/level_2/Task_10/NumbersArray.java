2f15a9d5a5d7c26be7e45b340412bb1049e908e1
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-27 11:16:10 +0300

    Lesson_5 level_2 Task_10 Add program with numbers array.

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_2/Task_10/NumbersArray.java
@@ -0,0 +1,18 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_2.Task_10;
+
+class NumbersArray {
+
+    public static void main(String[] args) {
+
+        int[] userNumbers= new int[3];
+
+        userNumbers[0]=1;
+        userNumbers[1]=2;
+        userNumbers[2]=3;
+
+        System.out.println(userNumbers[0]);
+        System.out.println(userNumbers[1]);
+        System.out.println(userNumbers[2]);
+
+    }
+}
