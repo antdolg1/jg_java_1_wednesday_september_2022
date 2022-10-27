e8b74c3c7af1c7578ceb51024b189f79d77388c8
Raivuha <onemoreraivuha@gmail.com>
2022-10-27 13:45:52 +0300

    add homework lesson - 5 level - 2 / Task 10-15

---
+++ src/main/java/students/student_raivis_urbevics/lesson5_level3/task20/ForLoopDefinition5.java
@@ -0,0 +1,10 @@
+package students.student_raivis_urbevics.lesson5_level3.task20;
+
+       class ForLoopDefinition5 {
+
+           public static void main(String[] args) {
+               for (int i = 0; i < 10; i++) {
+                   System.out.println(i);
+               }
+           }
+}
---
+++ src/main/java/students/student_raivis_urbevics/lesson5_level3/task21/ForLoopDefinition5.java
@@ -0,0 +1,12 @@
+package students.student_raivis_urbevics.lesson5_level3.task21;
+
+    class ForLoopDefinition5 {
+
+        public static void main(String[] args) {
+                for (int i = 0; i < 10; i++)
+                    System.out.println(i);
+            }
+        }
+
+
+
