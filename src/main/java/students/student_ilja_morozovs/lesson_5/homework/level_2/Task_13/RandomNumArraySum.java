20034529fd6094f6750261227cdc2a7dca76323f
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-27 15:24:10 +0300

    Lesson_5 level_2 Task_13 Add program with random array numbers program can sum this numbers.

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_2/Task_13/RandomNumArraySum.java
@@ -0,0 +1,27 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_2.Task_13;
+
+import java.util.Scanner;
+
+class RandomNumArraySum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input array volume: ");
+        int arrayIndex = scanner.nextInt();
+        int[] numbers = new int[arrayIndex];
+        RandomNumArraySum randomNumArraySum = new RandomNumArraySum();
+        System.out.println("Sum of random numbers is: " + randomNumArraySum.arrayRandomNumbers(numbers));
+    }
+
+    int arrayRandomNumbers(int[] numbers) {
+        int randomNumberSum = 0;
+        for (int randomNumbers : numbers) {
+            randomNumbers = (int) (Math.random() * numbers.length);
+            System.out.println(randomNumbers);
+            randomNumberSum += randomNumbers;
+        }
+        return randomNumberSum;
+    }
+}
+
+
