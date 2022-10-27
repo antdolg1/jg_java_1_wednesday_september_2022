bbdb7b44d6bf66bda61a98d77150c068634e8e89
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-27 13:32:01 +0300

    Lesson_5 level_2 Task_12 Add program with numbers array that can generate random numbers.

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_2/Task_12/RandomNumberArray.java
@@ -0,0 +1,11 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_2.Task_12;
+
+public class RandomNumberArray {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        for (int randomNumbers : numbers) {
+            randomNumbers = (int) (Math.random() * numbers.length);
+            System.out.println("Random number is: " + randomNumbers);
+        }
+    }
+}
