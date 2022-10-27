e48800c54bba5084b0d4ba7a19fc5c1d7dcee579
Sergejs Kozinecs <sergei.kozinecs@gmail.com>
2022-10-27 19:09:36 +0300

    Lesson 5 level 1 done

---
+++ src/main/java/students/student_sergejs_kozinecs/lesson_5/level_1/Task_3/ArrayDefinition3.java
@@ -0,0 +1,9 @@
+package students.student_sergejs_kozinecs.lesson_5.level_1.Task_3;
+
+class ArrayDefinition3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+
+}
---
+++ src/main/java/students/student_sergejs_kozinecs/lesson_5/level_1/Task_4/ArrayDefinition4.java
@@ -0,0 +1,9 @@
+package students.student_sergejs_kozinecs.lesson_5.level_1.Task_4;
+
+class ArrayDefinition4 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+
+}
