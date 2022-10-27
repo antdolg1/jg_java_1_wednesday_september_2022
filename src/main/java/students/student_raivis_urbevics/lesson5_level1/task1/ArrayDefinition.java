a84143b661aac72770d8cf30ea4949b3931ae20c
Raivuha <onemoreraivuha@gmail.com>
2022-10-27 12:25:53 +0300

    add homework lesson - 5 level - 1 / Task 1-9

---
+++ src/main/java/students/student_raivis_urbevics/lesson5_level1/task1/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package students.student_raivis_urbevics.lesson5_level1.task1;
+
+         class ArrayDefinition {
+             public static void main(String[] args) {
+                 int[] numbers = new int[5];
+             }
+
+         }
+
---
+++ src/main/java/students/student_raivis_urbevics/lesson5_level1/task6/ArrayDefinition6.java
@@ -0,0 +1,11 @@
+package students.student_raivis_urbevics.lesson5_level1.task6;
+
+      class ArrayDefinition6 {
+          public static void main(String[] args) {
+              int[] numbers = new int[]{1,2,3};
+
+          }
+      }
+
+
+
