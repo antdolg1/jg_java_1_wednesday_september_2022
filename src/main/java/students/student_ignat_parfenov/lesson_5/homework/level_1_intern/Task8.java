eb19f8d33edf609dacb3e81dbbfa928a26f7b5cd
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 14:40:52 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task8.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task8.java
@@ -1,12 +1,15 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_1_intern;
 
-public class Task7 {
+public class Task8 {
 
-    class ArrayLength1 {
+    class ArrayLength2 {
 
         public static void main(String[] args) {
             int[] numbers = new int[2];
+            System.out.println(numbers[0]);
             System.out.println(numbers[1]);
+            System.out.println(numbers[2]);
         }
+
     }
 }
