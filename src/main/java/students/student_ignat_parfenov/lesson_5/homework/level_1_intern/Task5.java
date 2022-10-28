f2be50198a5faff51c1d7db575911c8dc83c67a8
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 12:41:42 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task5.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task5.java
@@ -5,7 +5,7 @@ public class Task5 {
     class ArrayDefinition5 {
 
         public static void main(String[] args) {
-            int [] numbers = {1, 2, 3};
+            int[] numbers = {1, 2, 3, 4, 5};
         }
     }
 }
