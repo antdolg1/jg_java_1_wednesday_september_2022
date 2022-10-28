ed2e8e8e69fef30ef528a3bbfb478045079da134
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 12:10:03 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/ArraysApp.java
@@ -0,0 +1,18 @@
+package students.student_ignat_parfenov.lesson_5.lessoncode;
+
+public class ArraysApp {
+
+    public static void main(String[] args) {
+
+        String [] vehicles = new String[5];
+
+    vehicles[0] = "Car";
+    vehicles[1] = "Bus";
+    vehicles[2] = "Bicycle";
+    vehicles[3] = "Motorbike";
+    vehicles[4] = "Truck";
+
+        String [] vehicles2 = {"Car", "Bus", "Truck"};
+
+    }
+}
