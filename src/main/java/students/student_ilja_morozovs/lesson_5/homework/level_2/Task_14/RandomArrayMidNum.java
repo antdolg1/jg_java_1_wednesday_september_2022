07f467f375f89eae5e50ff624e640f8dadf5575f
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-28 16:26:57 +0300

    Lesson_5 level_2 Task_14 Add program witch count average number in array.

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_2/Task_14/RandomArrayMidNum.java
@@ -0,0 +1,25 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_2.Task_14;
+
+import java.util.Scanner;
+
+class RandomArrayMidNum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input array volume: ");
+        int arrayIndex = scanner.nextInt();
+        int[] numbers = new int[arrayIndex];
+        RandomArrayMidNum randomArrayMidNum = new RandomArrayMidNum();
+        System.out.println("Array average number is: " + (randomArrayMidNum.randomArrayMid(numbers)));
+    }
+
+    double randomArrayMid(int[] numbers) {
+        double randomNum = 0.0;
+        for (int randomNumbers : numbers) {
+            randomNumbers = (int) (Math.random() * numbers.length);
+            randomNum += randomNumbers;
+            System.out.println("[" + randomNumbers + "]");
+        }
+        return randomNum / numbers.length;
+    }
+}
