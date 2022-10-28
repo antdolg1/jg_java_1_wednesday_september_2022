ef360dcc13abefae420e617351da42f488252069
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 13:18:36 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/Loops.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/Loops.java
@@ -10,5 +10,14 @@ public class Loops {
             System.out.println("Hello! Value is " + userNumberValue + "\n");
             userNumberValue++;
         }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello! Value is " + i);
+        }
+
+        do{
+            System.out.println("Hello! Values is" + userNumberValue);
+            userNumberValue++;
+        } while (userNumberValue <= 10);
     }
 }
