df541fdce4a2ec3991044d7ba661790c7258ca35
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-28 15:03:42 +0200

    Homework Leson 2

--- src/main/java/students/student_jurij_hlebnikov/lesson_3/homework/PhoneDemo.java
+++
@@ -1,9 +0,0 @@
-package students.student_jurij_hlebnikov.lesson_3.homework;
-
-public class PhoneDemo {
-    public static void main(String[] args) {
-        Phone myPhone = new Phone("Huawei");
-        String phoneModel = myPhone.getModel();
-        System.out.println("Phone model = " + phoneModel);
-    }
-}
