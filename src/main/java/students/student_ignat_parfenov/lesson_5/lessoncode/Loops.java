b41c2d0db4a1970bec44f46b313e7e55db99c938
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 14:19:04 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/Loops.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/Loops.java
@@ -30,14 +30,14 @@ public class Loops {
 //        } while (userNumberValue != 7);
 //        System.out.println("Got 7!!!");
 
-        for (int i = 0; i <= 100; i+=10) {
+        for (int i = 0; i <= 100; i += 10) {
             i++;
             System.out.println(i);
         }
 
-        for (int i = 100; i >= 0; i-=10) {
-            i--;
-            System.out.println(i);
+        for (int j = 100; j >= 0; j -= 10) {
+            j--;
+            System.out.println(j);
         }
     }
 }
