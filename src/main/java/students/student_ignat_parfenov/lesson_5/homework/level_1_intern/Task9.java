d60f9f99b1546efa319a0b467b5a997b0d05bfac
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 23:26:33 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task9.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task9.java
@@ -6,7 +6,7 @@ public class Task9 {
 
         public static void main(String[] args) {
             int[] numbers = new int[2];
-            System.out.println(numbers.length());
+            System.out.println(numbers.length);
         }
     }
 }
