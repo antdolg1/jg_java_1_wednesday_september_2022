508511049eb0f05ab14c07e5bf358c5f696596c7
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 14:23:18 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/Loops.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/Loops.java
@@ -8,27 +8,27 @@ public class Loops {
 
         int userNumberValue = 0;
 
-//        while (userNumberValue <= 10) {
-//            System.out.println("Hello! Value is " + userNumberValue + "\n");
-//            userNumberValue++;
-//        }
+        while (userNumberValue <= 10) {
+            System.out.println("Hello! Value is " + userNumberValue + "\n");
+            userNumberValue++;
+        }
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello! Value is " + i);
         }
 
-//        do {
-//            System.out.println("Hello! Values is " + userNumberValue);
-//            userNumberValue++;
-//        } while (userNumberValue <= 10);
+        do {
+            System.out.println("Hello! Values is " + userNumberValue);
+            userNumberValue++;
+        } while (userNumberValue <= 10);
 
         Scanner scanner = new Scanner(System.in);
 
-//        do {
-//            System.out.println("Please enter number: ");
-//            userNumberValue = scanner.nextInt();
-//        } while (userNumberValue != 7);
-//        System.out.println("Got 7!!!");
+        do {
+            System.out.println("Please enter number: ");
+            userNumberValue = scanner.nextInt();
+        } while (userNumberValue != 7);
+        System.out.println("Got 7!!!");
 
         for (int i = 0; i <= 100; i += 10) {
             i++;
