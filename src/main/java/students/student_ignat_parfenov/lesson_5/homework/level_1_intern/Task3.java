417ecd704f88763ce1466c3ef339ab93de967800
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 12:24:16 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task3.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task3.java
@@ -1,4 +1,12 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_1_intern;
 
 public class Task3 {
+
+    class ArrayDefinition3 {
+
+        public static void main(String[] args) {
+            boolean [] numbers = new boolean[10];
+        }
+
+    }
 }
