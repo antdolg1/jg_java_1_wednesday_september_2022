997b618149681194afc20486664282080bb56388
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-28 12:29:21 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task2.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_1_intern/Task2.java
@@ -2,5 +2,10 @@ package students.student_ignat_parfenov.lesson_5.homework.level_1_intern;
 
 public class Task2 {
 
+    class ArrayDefinition2 {
 
+        public static void main(String[] args) {
+            byte[] numbers = new byte[10];
+        }
+    }
 }
