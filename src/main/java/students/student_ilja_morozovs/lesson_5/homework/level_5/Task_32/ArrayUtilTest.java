d8b2a1629f03d17292a46f235f090633dc53f09e
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-29 23:59:14 +0300

    Lesson_5 level_5 Task_32 add method and test for array length

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_5/Task_32/ArrayUtilTest.java
@@ -0,0 +1,24 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_5.Task_32;
+
+class ArrayUtilTest {
+    public static void main(String[] args) {
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+    }
+
+    void shouldCreateArray() {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int arrayLength = 5;
+        int arrayLengthResult = 8;
+        int[] realArrayResult = arrayUtil.createArray(arrayLength);
+        checkArrayLength(realArrayResult, arrayLengthResult);
+    }
+
+    void checkArrayLength(int[] realArrayResult, int arrayLength) {
+        if (realArrayResult.length == arrayLength) {
+            System.out.println("Array length TEST = OK");
+        } else {
+            System.out.println("Array length TEST = FAIL");
+        }
+    }
+}
