62724b42c70edba4ddf8efa6a83cee9a5201696a
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-30 00:43:57 +0300

    Lesson_5 level_6 Task_34 add method who can find max number in array

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_6/Task_36/ArrayUtil.java
@@ -0,0 +1,30 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_6.Task_36;
+
+import java.util.Arrays;
+
+class ArrayUtil {
+    int findMaxNumber(int[] array){
+        int maxNumber=array[0];
+        for (int i = 0; i < array.length; i++) {
+            maxNumber=array[i];
+            if (maxNumber<array[i]){
+                maxNumber=array[i];
+            }
+        }
+        return maxNumber;
+    }
+
+    void printArrayToConsole(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    void fillArrayWithRandomNumber(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * array.length);
+        }
+    }
+
+    int[] createArray(int arrayLength) {
+        return new int[arrayLength];
+    }
+}
