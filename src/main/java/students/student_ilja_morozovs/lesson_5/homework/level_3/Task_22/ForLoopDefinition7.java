8472f6b2a2b060d10fc40429b9c07688d3f17c69
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-29 11:13:11 +0300

    Lesson_5 level_2 Task_22 loop parameters must be in brace

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_22/ForLoopDefinition7.java
@@ -0,0 +1,11 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_3.Task_22;
+
+class ForLoopDefinition7 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
