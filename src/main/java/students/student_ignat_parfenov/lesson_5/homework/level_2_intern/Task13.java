821272c0b7e9146a94bdfe7afaec4e98e98f6186
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-29 16:33:41 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task13.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task13.java
@@ -1,6 +1,5 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_2_intern;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Task13 {
@@ -9,14 +8,14 @@ public class Task13 {
         Random random = new Random();
         int[] randomArraySum = new int[3];
         for (int i = 0; i < randomArraySum.length; i++) {
-            randomArraySum[i] = random.nextInt(40);
+            randomArraySum[i] = random.nextInt(50);
             System.out.println(randomArraySum[i]);
         }
         int sum = arraySum(randomArraySum);
         System.out.print("\nThis array's sum is: " + sum);
     }
 
-    public static int arraySum(int array[]) {
+    public static int arraySum(int[] array) {
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] > 0)
