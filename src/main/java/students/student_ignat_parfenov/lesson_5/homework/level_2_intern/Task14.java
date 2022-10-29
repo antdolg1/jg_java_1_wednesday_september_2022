1731ab9d169242b002deab13ba6508c1421dbfe5
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-29 16:38:19 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task14.java
@@ -0,0 +1,13 @@
+package students.student_ignat_parfenov.lesson_5.homework.level_2_intern;
+
+import java.util.Random;
+
+public class Task14 {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] mas3 = new int[3];
+
+    }
+}
