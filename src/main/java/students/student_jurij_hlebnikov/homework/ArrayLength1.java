6cd453fa42a7b63b471ddf3572c1c979f397e0cf
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-29 13:44:46 +0200

    Task_7

---
+++ src/main/java/students/student_jurij_hlebnikov/homework/ArrayLength1.java
@@ -0,0 +1,10 @@
+package students.student_jurij_hlebnikov.homework;
+
+public class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[1]);
+
+    }
+}
