985fa57a772fa22c39a8c31ac9590ae5cef048b3
Sergejs Kozinecs <sergei.kozinecs@gmail.com>
2022-10-29 15:30:49 +0300

    Lesson 5 level 6 done

---
+++ src/main/java/students/student_sergejs_kozinecs/lesson_5/level_6/Task_34/ArrayUtil.java
@@ -0,0 +1,11 @@
+package students.student_sergejs_kozinecs.lesson_5.level_6.Task_34;
+
+class ArrayUtil {
+
+    public void printArrayToConsole(int[] array) {
+        for (int item : array) {
+            System.out.println(item);
+        }
+    }
+
+}
