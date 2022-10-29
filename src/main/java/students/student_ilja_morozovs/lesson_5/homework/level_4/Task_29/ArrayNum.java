ab096a8427086fa3295a7ccb8fe9fb0e5526a66b
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-29 18:50:37 +0300

    Lesson_5 level_4 Task_29 Add program who get from user array volume and show even number in array

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_4/Task_29/ArrayNum.java
@@ -0,0 +1,36 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_4.Task_29;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class ArrayNum {
+    public static void main(String[] args) {
+        System.out.println("Please input the array volume: ");
+        Scanner scanner = new Scanner(System.in);
+        int arrayIndex = scanner.nextInt();
+        int[] userNumbers = new int[arrayIndex];
+        ArrayNum arrayNum = new ArrayNum();
+        arrayNum.arrayRandomNumbers(userNumbers);
+        System.out.println("Array is: " + Arrays.toString(userNumbers));
+        arrayNum.isEvenNumber(userNumbers);
+    }
+
+    void arrayRandomNumbers(int[] userNumbers) {
+        for (int i = 0; i < userNumbers.length; i++) {
+            userNumbers[i] = (int) (Math.random() * userNumbers.length);
+        }
+    }
+
+    void isEvenNumber(int[] userNumbers) {
+        int evenNumber;
+        System.out.print("Even number is: ");
+        System.out.print("[");
+        for (int userNumber : userNumbers) {
+            if (userNumber % 2 == 0) {
+                evenNumber = userNumber;
+                System.out.print(" " + evenNumber + " ");
+            }
+        }
+        System.out.println("]");
+    }
+}
