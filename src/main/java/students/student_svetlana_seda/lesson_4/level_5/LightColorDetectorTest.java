29b4d045e6c8d79af261a569363377888794146c
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-29 20:10:02 +0300

    Homework of Lesson_4_level_5 - done

--- src/main/java/students/student_svetlana_seda/lesson_4/level_5/LightColorDetectorTest.java
+++ src/main/java/students/student_svetlana_seda/lesson_4/level_5/LightColorDetectorTest.java
@@ -1,24 +1,23 @@
 package students.student_svetlana_seda.lesson_4.level_5;
 
 public class LightColorDetectorTest {
-
     public static void main(String[] args) {
 
         LightColorDetectorTest lightColorDetectorTest = new LightColorDetectorTest();
         LightColorDetectorTest.detectTest();
 
-        public void detectTest () {
+    }
 
-            int waveLength = 600;
-            String expectedResult = "Orange";
-            LightColorDetector lightColorDetector = new LightColorDetector();
-            String realResult = lightColorDetector.detect(waveLength);
-            if (realResult.equals(expectedResult)) {
-                System.out.println("LightColorDetectorTest is OK");
-            } else {
-                System.out.println("LightColorDetectorTest is FAIL");
-            }
+    private void detectTest() {
 
+        int waveLength = 600;
+        String expectedResult = "Orange";
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realResult = lightColorDetector.detect(waveLength);
+        if (realResult.equals(expectedResult)) {
+            System.out.println("LightColorDetectorTest is OK");
+        } else {
+            System.out.println("LightColorDetectorTest is FAIL");
         }
 
     }
