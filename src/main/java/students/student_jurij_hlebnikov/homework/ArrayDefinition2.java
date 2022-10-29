71be6dd91ec469e90aa0674379a32380388acf09
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-29 13:40:45 +0200

    Task_5

---
+++ src/main/java/students/student_jurij_hlebnikov/homework/ArrayDefinition2.java
@@ -0,0 +1,9 @@
+package students.student_jurij_hlebnikov.homework;
+
+public class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+    }
+}
---
+++ src/main/java/students/student_jurij_hlebnikov/homework/ArrayDefinition3.java
@@ -0,0 +1,8 @@
+package students.student_jurij_hlebnikov.homework;
+
+public class ArrayDefinition3 {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+    }
+}
