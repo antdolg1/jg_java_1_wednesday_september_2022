a562cd7c785280e4f14dab082d5876b12589c542
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-29 22:36:00 +0300

    Homework - Lesson 4_level 6   corrected

--- src/main/java/students/student_svetlana_seda/lesson_4/level_6/StockDemo.java
+++ src/main/java/students/student_svetlana_seda/lesson_4/level_6/StockDemo.java
@@ -2,15 +2,14 @@ package students.student_svetlana_seda.lesson_4.level_6;
 
 class StockDemo {
     public static void main(String[] args) {
-
-        Stock microsoft = new Stock("Mic-o-stock", 666, 540, 999);
-        microsoft.priceInformation();
-        microsoft.updatePrice(777);
-        microsoft.updatePrice(45);
-        microsoft.updatePrice(999);
-        microsoft.priceInformation();
-
+        Stock google = new Stock("Google", 100, 190, 60);
+        google.priceInformation();
+        google.updatePrice(5);
+        google.updatePrice(300);
+        google.updatePrice(50);
+        google.priceInformation();
     }
 // последнее задание этого урока, признаюсь не допоняла, скопировала -  потом буду разбираться...
     //при его проверки - открывается class Calculator - выдает там ошибки, я их не понимаю - поэтому здесь ничего не могу проверить сейчас :(
 }
+
--- src/main/java/students/student_svetlana_seda/lesson_4/level_6/StockTest.java
+++ src/main/java/students/student_svetlana_seda/lesson_4/level_6/StockTest.java
@@ -2,46 +2,37 @@ package students.student_svetlana_seda.lesson_4.level_6;
 
 class StockTest {
     public static void main(String[] args) {
-
-        StockTest test = new StockTest();
-        test.shouldCurrentChange();
-
+        StockTest stockTest = new StockTest();
+        stockTest.updatePriceTest();
     }
 
-    void shouldCurrentChange() {
-
-        public static void main (String[]args){
-
-            StockTest test = new StockTest();
-            test.shouldCurrentChange();
-
+    void updatePriceTest() {
+        int setPrice = 150;
+        int maxPriceResult = 150;
+        int minPriceResult = 10;
+        int currentPrice = 20;
+        Stock stock = new Stock("Apple", 20, 150, 10);
+        int realPriceResult = stock.updatePrice(setPrice);
+        if (realPriceResult >= maxPriceResult) {
+            System.out.println("updatePrice Max.Price: TEST OK");
+        } else {
+            System.out.println("updatePrice Max.Price: TEST FAIL");
         }
-
-        void shouldCurrentChange () {
-
-            Stock victim = new Stock("Mic-o-stock", 100, 10, 150);
-            int realUpdatePrice = victim.updatePrice(victim.maxValue);
-
-            if (realUpdatePrice >= victim.maxValue) {
-                System.out.println("Updated price MAX test is OK");
-            } else {
-                System.out.println("Updated price MAX test is FAIL");
-            }
-            if (realUpdatePrice > victim.minValue) {
-                System.out.println("Updated price MIN test is OK");
-            } else {
-                System.out.println("Updated price MIN test is FAIL");
-            }
-            if (realUpdatePrice == victim.currentValue) {
-                System.out.println("Updated price CURRENT test is OK");
-                System.out.println(realUpdatePrice);
-            } else {
-                System.out.println("Updated price CURRENT test is OK");
-            }
+        if (realPriceResult < minPriceResult) {
+            System.out.println("updatePrice Min.Price: TEST OK");
+        } else {
+            System.out.println("updatePrice Min.Price: TEST FAIL");
         }
+        if (realPriceResult == currentPrice) {
+            System.out.println("updatePrice Current.Price: TEST OK");
+        } else {
+            System.out.println("updatePrice Current.Price: TEST FAIL");
+        }
+    }
 // последнее задание этого урока, признаюсь не допоняла, скопировала -  потом буду разбираться...
-        //при его проверки - открывается class Calculator - выдает там ошибки, я их не понимаю - поэтому здесь ничего не могу проверить сейчас :(
-    }
+    //при его проверки - открывается class Calculator - выдает там ошибки, я их не понимаю - поэтому здесь ничего не могу проверить сейчас :(
+}
+
 
 }
 
