d34e08b2219b08eacb37db96adc0b1a5948ac1c4
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-29 11:18:05 +0300

    Lesson_5 level_2 Task_24 loop show numbers from 0 to 100

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_24/ForLoopDefinition9.java
@@ -0,0 +1,11 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_3.Task_24;
+
+class ForLoopDefinition9 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+}
