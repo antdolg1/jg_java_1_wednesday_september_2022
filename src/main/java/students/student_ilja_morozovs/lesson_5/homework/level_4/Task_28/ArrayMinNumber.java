e97e06774acc4db64414360a9431b0168066aa8c
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-29 12:49:52 +0300

    Lesson_5 level_4 Task_28 Add program who get from user array volume and show min number in array

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_4/Task_28/ArrayMinNumber.java
@@ -0,0 +1,33 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_4.Task_28;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class ArrayMinNumber {
+    public static void main(String[] args) {
+        System.out.println("Please input array volume: ");
+        Scanner scanner = new Scanner(System.in);
+        int arrayIndex = scanner.nextInt();
+        int[] userNumbers = new int[arrayIndex];
+        ArrayMinNumber arrayMinNumber = new ArrayMinNumber();
+        arrayMinNumber.arrayRandomNumbers(userNumbers);
+        System.out.println("Array is:" + Arrays.toString(userNumbers));
+        System.out.println("Min number in array is " + arrayMinNumber.minArrayNumber(userNumbers));
+    }
+
+    void arrayRandomNumbers(int[] userNumbers) {
+        for (int i = 0; i < userNumbers.length; i++) {
+            userNumbers[i] = (int) (Math.random() * userNumbers.length);
+        }
+    }
+
+    int minArrayNumber(int[] userNumbers) {
+        int minNumber = userNumbers[0];
+        for (int userNumber : userNumbers) {
+            if (userNumber < minNumber) {
+                minNumber = userNumber;
+            }
+        }
+        return minNumber;
+    }
+}
