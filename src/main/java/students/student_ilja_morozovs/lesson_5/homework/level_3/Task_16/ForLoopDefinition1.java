b50ae997e821be4ff8b338a3109e8dfd36b3e1d4
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-29 10:59:46 +0300

    Lesson_5 level_2 Task_16 In loop "for" variable "i" do not initialized

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_16/ForLoopDefinition1.java
@@ -0,0 +1,9 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_3.Task_16;
+
+class ForLoopDefinition1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
