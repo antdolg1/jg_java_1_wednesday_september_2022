b756b81e0dacbf906af7b45539cf0d4666f209b6
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-29 16:49:15 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task14.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task14.java
@@ -1,5 +1,6 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_2_intern;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task14 {
@@ -8,6 +9,16 @@ public class Task14 {
 
         Random random = new Random();
         int[] mas3 = new int[3];
+        for (int i = 0; i < mas3.length; i++) {
+            mas3[i] = random.nextInt(20);
+        }
+        System.out.println("Random array is " + Arrays.toString(mas3));
 
+        double average = 0;
+        for (int j : mas3) {
+            average += j;
+        }
+        average /= 3;
+        System.out.println("Array arithmetic mean is " + average);
     }
 }
