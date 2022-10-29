15f1f23e9da4f43f5f8103e4e1ddd2f44c04746e
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-29 13:21:53 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task11.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task11.java
@@ -7,12 +7,12 @@ public class Task11 {
 
     public static void main(String[] args) {
 
-        System.out.print("Please enter three numbers: ");
+        System.out.println("Please enter three numbers: ");
         Scanner scanner = new Scanner(System.in);
         int[] number = new int[3];
         number[0] = scanner.nextInt();
         number[1] = scanner.nextInt();
         number[2] = scanner.nextInt();
-        System.out.println("The entered numbers array is" + Arrays.toString(number));
+        System.out.println("The entered numbers array is " + Arrays.toString(number));
     }
 }
