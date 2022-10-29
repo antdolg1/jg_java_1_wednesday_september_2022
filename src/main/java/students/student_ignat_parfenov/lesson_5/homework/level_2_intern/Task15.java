24af701780fd8e238a2b17e3143ad1772fbbc79d
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-29 21:44:06 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task15.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task15.java
@@ -8,17 +8,10 @@ public class Task15 {
     public static void main(String[] args) {
 
         Random random = new Random();
-        int[] arrayElementPlusTwo = new int[5];
+        int[] arrayElementPlusTwo = new int[3];
         for (int i = 0; i < arrayElementPlusTwo.length; i++) {
-            arrayElementPlusTwo[i] = random.nextInt(20);
-            System.out.println("Current random array: " + Arrays.toString(arrayElementPlusTwo));
-
-            int a = 2;
-            arrayElementPlusTwo[0] += a;
-            arrayElementPlusTwo[1] += a;
-            arrayElementPlusTwo[2] += a;
-            System.out.println("Current arrays elements plus 2: " + Arrays.toString(arrayElementPlusTwo));
+            arrayElementPlusTwo[i] = random.nextInt(99);
+            System.out.println(arrayElementPlusTwo[i]);
         }
     }
 }
-
