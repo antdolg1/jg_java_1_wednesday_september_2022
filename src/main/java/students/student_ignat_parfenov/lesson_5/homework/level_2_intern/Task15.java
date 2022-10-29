72819516d7938e01c0a60ea071c983cbc53febac
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-29 16:59:02 +0300

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task15.java
@@ -0,0 +1,15 @@
+package students.student_ignat_parfenov.lesson_5.homework.level_2_intern;
+
+import java.util.Random;
+
+public class Task15 {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] arrayElementPlusTwo = new int[3];
+        for (int i = 0; i < arrayElementPlusTwo.length; i++) {
+            arrayElementPlusTwo[i] = random.nextInt(20);
+        }
+    }
+}
