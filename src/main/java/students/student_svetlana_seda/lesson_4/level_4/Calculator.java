4efbd6050d3c24bb2c2778d85564243344b64350
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-29 21:11:19 +0300

    Homework - Lesson 4_level 5 corrected - done

--- src/main/java/students/student_svetlana_seda/lesson_4/level_4/Calculator.java
+++ src/main/java/students/student_svetlana_seda/lesson_4/level_4/Calculator.java
@@ -17,52 +17,72 @@ class Calculator {
     public int division(int firstNumber, int secondNumber) {
         return firstNumber / secondNumber;
     }
-
     public boolean isEven(int number) {
-
+    boolean result = true;
         if (number % 2 == 0) {
-            return true;
+            return result;
         }
-//непонятно, как правильно будет??? почему true ОРАНЖЕВОГО СВЕТА?
-    }
 
+    }
     public int maxNumberOfTwoNumbers(int firstNumber, int secondNumber) {
-        return firstNumber > secondNumber;
-
-        //the same
-        return secondNumber > firstNumber;
-        return firstNumber == secondNumber;
+        if (firstNumber > secondNumber){
+            return firstNumber;
+        }else if (secondNumber > firstNumber){
+            return secondNumber;
+        }else{
+            return firstNumber;
+        }
 
     }
-
     public int biggestOfThreeNumbers(int firstNumber, int secondNumber, int thirdNumber) {
-        int maxNumber;
-        return maxNumber;
+        if (firstNumber == secondNumber && firstNumber < thirdNumber){
+            return thirdNumber;
+        } else if (firstNumber == secondNumber && firstNumber > thirdNumber) {
+            return firstNumber;
+        }else if (secondNumber == thirdNumber && secondNumber < firstNumber) {
+            return firstNumber;
+        } else if (secondNumber == thirdNumber && secondNumber > firstNumber) {
+            return secondNumber;
+        } else if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+            return firstNumber;
+        } else if (secondNumber > thirdNumber) {
+            return secondNumber;
+        } else if (thirdNumber > firstNumber) {
+            return thirdNumber;
+        } else {
+            return firstNumber;
+        }
 
     }
-
     public int firstNumberTheBiggest(int firstNumber, int secondNumber, int thirdNumber) {
-        return firstNumber;
+        if ( firstNumber > secondNumber && firstNumber > thirdNumber){
+            return firstNumber;
+        }
 
     }
-
     public int secondNumberTheBiggest(int firstNumber, int secondNumber, int thirdNumber) {
-        return secondNumber;
+        if (secondNumber > firstNumber && secondNumber > thirdNumber){
+            return secondNumber;
+        }
 
     }
-
     public int thirdNumberTheBiggest(int firstNumber, int secondNumber, int thirdNumber) {
-        return thirdNumber;
+        if (thirdNumber > firstNumber && thirdNumber > secondNumber){
+            return thirdNumber;
+        }
 
     }
 
     public int firstAndSecondEqualBiggest(int firstNumber, int secondNumber, int thirdNumber) {
-        return firstNumber == secondNumber && firstNumber > thirdNumber;
+        if (firstNumber == secondNumber && firstNumber > thirdNumber){
+            return firstNumber;
+        }
 
     }
-
     public int allEquals(int firstNumber, int secondNumber, int thirdNumber) {
-        return firstNumber && secondNumber && thirdNumber;
+        if (firstNumber == secondNumber && secondNumber == thirdNumber){
+            return firstNumber;
+        }
 
     }
 
