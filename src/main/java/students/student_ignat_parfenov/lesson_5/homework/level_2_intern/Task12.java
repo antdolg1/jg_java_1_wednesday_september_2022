a2451575ee42727e8ae665eff2aed2a711635410
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-29 15:17:40 +0300

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task12.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_2_intern/Task12.java
@@ -1,12 +1,17 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_2_intern;
 
+import java.util.Random;
+
 public class Task12 {
 
     public static void main(String[] args) {
+        int[] randomArray = new int[10];
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = i;
+        }
 
-        int[] randomArray = new int[3];
-        for(int i = 0; i < 12; i++){
-            System.out.println(randomArray.length);
+        for (int i = 0; i < randomArray.length; i++) {
+            System.out.println(randomArray[i]);
         }
     }
 }
