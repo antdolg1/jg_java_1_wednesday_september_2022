ff7657ac662e09f034a6d32705f7ae6b68cf00e3
java0711 <114607998+java0711@users.noreply.github.com>
2022-10-29 21:52:31 +0300

    Homework - Lesson 4_level 6 corrected - done/ (will be checked after correction of level_5)

---
+++ src/main/java/students/student_svetlana_seda/lesson_4/level_6/StockDemo.java
@@ -0,0 +1,17 @@
+package students.student_svetlana_seda.lesson_4.level_6;
+
+class StockDemo {
+
+    public static void main(String[] args) {
+
+        Stock microsoft = new Stock("Mic-o-stock", 666, 540, 999);
+        microsoft.priceInformation();
+        microsoft.updatePrice(777);
+        microsoft.updatePrice(45);
+        microsoft.updatePrice(999);
+        microsoft.priceInformation();
+
+    }
+// последнее задание этого урока, признаюсь не допоняла, скопировала -  потом буду разбираться...
+    //при его проверки - открывается class Calculator - выдает там ошибки, я их не понимаю - поэтому здесь ничего не могу проверить сейчас :(
+}
--- src/main/java/students/student_svetlana_seda/lesson_4/level_6/StockTest.java
+++ src/main/java/students/student_svetlana_seda/lesson_4/level_6/StockTest.java
@@ -1,7 +1,43 @@
 package students.student_svetlana_seda.lesson_4.level_6;
-
 class StockTest {
+    public static void main(String[] args) {
 
+        StockTest test = new StockTest();
+        test.shouldCurrentChange();
 
+    }
+    void shouldCurrentChange() {
 
-}
+        public static void main (String[]args){
+
+            StockTest test = new StockTest();
+            test.shouldCurrentChange();
+
+        }
+
+        void shouldCurrentChange () {
+
+            Stock victim = new Stock("Mic-o-stock", 100, 10, 150);
+            int realUpdatePrice = victim.updatePrice(victim.maxValue);
+
+            if (realUpdatePrice >= victim.maxValue) {
+                System.out.println("Updated price MAX test is OK");
+            } else {
+                System.out.println("Updated price MAX test is FAIL");
+            }
+            if (realUpdatePrice > victim.minValue) {
+                System.out.println("Updated price MIN test is OK");
+            } else {
+                System.out.println("Updated price MIN test is FAIL");
+            }
+            if (realUpdatePrice == victim.currentValue) {
+                System.out.println("Updated price CURRENT test is OK");
+                System.out.println(realUpdatePrice);
+            } else {
+                System.out.println("Updated price CURRENT test is OK");
+            }
+        }
+// последнее задание этого урока, признаюсь не допоняла, скопировала -  потом буду разбираться...
+        //при его проверки - открывается class Calculator - выдает там ошибки, я их не понимаю - поэтому здесь ничего не могу проверить сейчас :(
+    }
+
