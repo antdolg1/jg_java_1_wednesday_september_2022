430505deabf68dcd543a73a4d905e0e408ccd2c6
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-29 15:21:09 +0200

    Task_13

---
+++ src/main/java/students/student_jurij_hlebnikov/homework/Task_13.java
@@ -0,0 +1,18 @@
+package students.student_jurij_hlebnikov.homework;
+
+import java.util.Arrays;
+
+public class Task_13 {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 100) + 1;
+        }
+        String intAString = Arrays.toString(numbers);
+        System.out.println(intAString);
+        int sum = numbers[0] + numbers[1] + numbers[2];
+        System.out.println(sum);
+
+    }
+}
