be3c0bd6d8d86b2412235c4c107b110e0d8f2387
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-29 11:05:11 +0300

    Lesson_5 level_2 Task_18 In loop parametr i be "--", need i++

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_19/ForLoopDefinition4.java
@@ -0,0 +1,11 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_3.Task_19;
+
+class ForLoopDefinition4 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
