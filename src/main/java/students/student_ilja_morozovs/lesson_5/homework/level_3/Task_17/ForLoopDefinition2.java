64d7c012476c8dccd108ce2f80adf7b76cbcb7bd
iljamorozovs <ilja.morozovs@gmail.com>
2022-10-29 11:01:04 +0300

    Lesson_5 level_2 Task_17 In loop "for" no ";"

---
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_17/ForLoopDefinition2.java
@@ -0,0 +1,11 @@
+package students.student_ilja_morozovs.lesson_5.homework.level_3.Task_17;
+
+class ForLoopDefinition2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
