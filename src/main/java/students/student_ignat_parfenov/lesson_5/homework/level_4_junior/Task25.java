b15c53dbdbdfb4db7575a494da14b242e211a9c6
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-30 17:52:46 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task25.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task25.java
@@ -12,9 +12,9 @@ public class Task25 {
         int userArray = scanner.nextInt();
         int[] userNumbers = new int[userArray];
         for (int i = 0; i < userNumbers.length; i++) {
-            System.out.print("Please enter array numbers: " + "[ " + userNumbers[i] + "] ");
+            System.out.println("Please enter your array's numbers : " + "[" + userNumbers[i] + "]");
             userNumbers[i] = scanner.nextInt();
         }
-        System.out.println("Your entered array is: " + Arrays.toString(userNumbers));
+        System.out.println("Your entered array numbers are : " + Arrays.toString(userNumbers));
     }
 }
