98ef1d90ae6b79a380c595447bfa19432152a6a1
Mihhail327 <mixa-amr@hotmail.com>
2022-10-30 19:54:09 +0200

    Lesson 2 homework

---
+++ src/main/java/students/student_mihhail_karasjov/lesson3/Circle.java
@@ -0,0 +1,13 @@
+package students.student_mihhail_karasjov.lesson3;
+
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        Scanner io = new Scanner(System.in);
+        System.out.println("Input the radius of the circle: ");
+        double radius = io.nextDouble();
+        System.out.println("Perimeter is = " + (2 * radius * Math.PI));
+        System.out.println("Area is = " + (Math.PI * radius * radius));
+    }
+}
