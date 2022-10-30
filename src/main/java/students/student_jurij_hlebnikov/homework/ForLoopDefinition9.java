c18d778216a7ca4597821ab49669d7f4f32a8344
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-30 10:07:43 +0100

    Task_24

---
+++ src/main/java/students/student_jurij_hlebnikov/homework/ForLoopDefinition9.java
@@ -0,0 +1,9 @@
+package students.student_jurij_hlebnikov.homework;
+
+public class ForLoopDefinition9 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i ++){
+            System.out.println(i);
+        }
+    }
+}
