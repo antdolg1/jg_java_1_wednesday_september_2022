d520b2c2e66a13e36d3befcf1f4501251d7919cd
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-30 15:14:32 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task25.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task25.java
@@ -1,9 +1,14 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_4_junior;
 
+import java.util.Scanner;
+
 public class Task25 {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please specify the array's length: ");
+        int num = scanner.nextInt();
 
     }
 }
