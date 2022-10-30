74d2c8e50bdb1b69954340ea65414b6dc2f4fe1e
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-30 14:57:42 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_3_junior/Task23.java
@@ -0,0 +1,14 @@
+package students.student_ignat_parfenov.lesson_5.homework.level_3_junior;
+
+public class Task23 {
+
+    class ForLoopDefinition7 {
+
+        public static void main(String[] args) {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
