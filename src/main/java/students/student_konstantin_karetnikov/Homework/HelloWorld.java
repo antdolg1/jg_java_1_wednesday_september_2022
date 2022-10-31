90322a8a8e1dff214f8b33def06b0f88efcc48c6
kskaretnikov <114775910+kskaretnikov@users.noreply.github.com>
2022-10-31 10:28:45 +0300

    Update HelloWorld.java

--- src/main/java/students/student_konstantin_karetnikov/Homework/HelloWorld.java
+++ src/main/java/students/student_konstantin_karetnikov/Homework/HelloWorld.java
@@ -1,6 +1,7 @@
 package students.student_konstantin_karetnikov.Homework;
 
-public class HelloWorld {public static void main(String[] args)
-{System.out.println("Hello World!");
-}
+public class HelloWorld {
+public static void main(String[] args)
+  {System.out.println("Hello World!");
+  }
 }
