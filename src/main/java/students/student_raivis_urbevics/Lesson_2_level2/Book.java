0892d037809a7beeb25a5598ec5ed6bc75e3db59
Raivuha <onemoreraivuha@gmail.com>
2022-10-31 13:23:25 +0200

    removed extra lines  / Lesson 2 Level 2

--- src/main/java/students/student_raivis_urbevics/Lesson_2_level2/Book.java
+++ src/main/java/students/student_raivis_urbevics/Lesson_2_level2/Book.java
@@ -2,6 +2,3 @@ package students.student_raivis_urbevics.Lesson_2_level2;
 
 public class Book {
 }
-
-
-
--- src/main/java/students/student_raivis_urbevics/Lesson_2_level2/Task_8.java
+++ src/main/java/students/student_raivis_urbevics/Lesson_2_level2/Task_8.java
@@ -2,13 +2,7 @@ package students.student_raivis_urbevics.Lesson_2_level2;
 
 public class Task_8 {
     public static void main(String[] args) {
-        String username="Raivis";
+        String username = "Raivis";
         System.out.println("Hello " + username);
-
     }
-
-    }
-
-
-
-
+}
