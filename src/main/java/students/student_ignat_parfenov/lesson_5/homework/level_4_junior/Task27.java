9aeff33f2bfc01d81c5e0d50455c69c29150dcf2
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-31 14:17:43 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task27.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task27.java
@@ -1,15 +1,19 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_4_junior;
 
 import java.util.Random;
+import java.util.Scanner;
 
 class Task27 {
 
     public static void main(String[] args) {
 
         Random random = new Random();
-        int[] randomArray = new int[5];
+        System.out.print("Please input your array length: ");
+        Scanner scanner = new Scanner(System.in);
+        int volume = scanner.nextInt();
+        int[] randomArray = new int[volume];
         for (int i = 0; i < randomArray.length; i++) {
-            randomArray[i] = random.nextInt(67);
+            randomArray[i] = random.nextInt(70);
             System.out.println(randomArray[i]);
         }
         int maxNum = randomArray[0];
--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task28.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task28.java
@@ -1,15 +1,19 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_4_junior;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Task28 {
 
     public static void main(String[] args) {
 
         Random random = new Random();
-        int[] randArray = new int[8];
+        System.out.print("Please input your array length: ");
+        Scanner scanner = new Scanner(System.in);
+        int volume = scanner.nextInt();
+        int[] randArray = new int[volume];
         for (int i = 0; i < randArray.length; i++) {
-            randArray[i] = random.nextInt(35);
+            randArray[i] = random.nextInt(50);
             System.out.println(randArray[i]);
         }
         int minNum = randArray[0];
