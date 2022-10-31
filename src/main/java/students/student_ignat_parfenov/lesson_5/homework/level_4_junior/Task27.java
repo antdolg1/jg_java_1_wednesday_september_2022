3b1cfc5340abe962817f611337e594072c383ad2
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-10-31 13:44:51 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task27.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task27.java
@@ -1,20 +1,19 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_4_junior;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task27 {
 
     public static void main(String[] args) {
 
-
         Random random = new Random();
-        int[] maximumNumber = new int[5];
-        for (int i = 0; i < maximumNumber.length; i++) {
-            maximumNumber[i] = random.nextInt(50);
-            System.out.println(maximumNumber[i]);
+        int[] randomArraySum = new int[3];
+        for (int i = 0; i < randomArraySum.length; i++) {
+            randomArraySum[i] = random.nextInt(50);
+            System.out.println(randomArraySum[i]);
+            int max = 0;
         }
-        int max = 0;
-
     }
 }
 
