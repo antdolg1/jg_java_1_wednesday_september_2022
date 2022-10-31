fd9392a6c19346be9ef2aeaf41b5805205cc351e
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-10-31 18:06:35 +0100

    Task_28

--- src/main/java/students/student_jurij_hlebnikov/homework/Task_28.java
+++ src/main/java/students/student_jurij_hlebnikov/homework/Task_28.java
@@ -2,26 +2,26 @@ package students.student_jurij_hlebnikov.homework;
 
 import java.util.Arrays;
 
-public class Task_27 {
+public class Task_28 {
     public static void main(String[] args) {
 
-        int arrayLength = (int) (Math.random() * 100 + 1);
+        int arrayLength = (int)(Math.random() * 50 + 1);
         int[] array = new int[arrayLength];
 
-        for (int i = 0; i < arrayLength; i++) {
-            array[i] = (int) (Math.random() * 100);
+        for(int i = 0; i < arrayLength; i++){
+            array[i] = (int) (Math.random() * 50);
         }
 
         System.out.println("Array Lang is: " + arrayLength);
         String allElements = Arrays.toString(array);
-        System.out.println("Array numbers: " + allElements);
+        System.out.println("Arrays numbers: " + allElements);
 
         Arrays.sort(array);
 
         System.out.println("Array after sorting: " + Arrays.toString(array));
 
-        int maxNum = array[array.length - 1];
-        System.out.println("Maximum number: " + maxNum);
+        int minNum = Arrays.stream(array).min().getAsInt();
+
+        System.out.println("Minimal number is: " + minNum);
     }
 }
-
