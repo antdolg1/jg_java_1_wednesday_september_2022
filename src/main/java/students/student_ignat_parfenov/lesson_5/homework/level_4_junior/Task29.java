8abda6fda7b9e7e9097407bca7b501a85bf09e68
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-01 14:08:25 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task29.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task29.java
@@ -1,5 +1,6 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_4_junior;
 
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -16,20 +17,8 @@ class Task29 {
             evenNumbers[i] = random.nextInt(50);
             System.out.println(evenNumbers[i]);
         }
-
-        int maxNum = evenNumbers[0];
-        for (int j : evenNumbers) {
-            if (j > maxNum)
-                maxNum = j;
-        }
-
-        for (int j : evenNumbers) {
-            if (j % 2 == 0) {
-                System.out.println(evenNumbers[j]);
-            }
-        }
-        System.out.println("]");
     }
 }
+
 
 
