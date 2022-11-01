9c283045e79ef105e22033b3366a7eb3ce79fad7
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-01 12:29:50 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task29.java
@@ -0,0 +1,22 @@
+package students.student_ignat_parfenov.lesson_5.homework.level_4_junior;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Task29 {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        System.out.print("Please input your array length: ");
+        Scanner scanner = new Scanner(System.in);
+        int volume = scanner.nextInt();
+        int[] evenNumbers = new int[volume];
+        for (int i = 0; i < evenNumbers.length; i++) {
+            evenNumbers[i] = random.nextInt(50);
+            System.out.println(evenNumbers[i]);
+        }
+
+    }
+}
+
