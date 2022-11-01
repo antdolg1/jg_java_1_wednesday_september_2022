3d0ccf2084405d4b71190565877d8172427a07d9
iljamorozovs <ilja.morozovs@gmail.com>
2022-11-01 20:13:13 +0200

    refactor level 3

--- src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_20/ForLoopDefinition5.java
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_20/ForLoopDefinition5.java
@@ -3,7 +3,6 @@ package students.student_ilja_morozovs.lesson_5.homework.level_3.Task_20;
 class ForLoopDefinition5 {
 
     public static void main(String[] args) {
-
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
--- src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_24/ForLoopDefinition9.java
+++ src/main/java/students/student_ilja_morozovs/lesson_5/homework/level_3/Task_24/ForLoopDefinition9.java
@@ -3,8 +3,7 @@ package students.student_ilja_morozovs.lesson_5.homework.level_3.Task_24;
 class ForLoopDefinition9 {
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < 101; i++) {
+        for (int i = 0; i <=100; i++) {
             System.out.println(i);
         }
     }
