9696a12fb9cc041278003726eea880ffb35b25af
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-01 14:36:26 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task29.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_4_junior/Task29.java
@@ -1,6 +1,7 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_4_junior;
 
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,17 +9,34 @@ class Task29 {
 
     public static void main(String[] args) {
 
-        Random random = new Random();
-        System.out.print("Please input your array length: ");
+        System.out.println("Please input your array length: ");
         Scanner scanner = new Scanner(System.in);
-        int volume = scanner.nextInt();
-        int[] numbers = new int[volume];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = random.nextInt(50);
-            System.out.println(numbers[i]);
+        int arrayIndex = scanner.nextInt();
+        int[] cloneArray = new int[arrayIndex];
+        Task29 arrayNum = new Task29();
+        arrayNum.randomArray(cloneArray);
+        System.out.println("Your array is: " + Arrays.toString(cloneArray));
+        arrayNum.isEvenNumber(cloneArray);
+    }
+
+    void randomArray(int[] cloneArray) {
+        for (int i = 0; i < cloneArray.length; i++) {
+            Random random = new Random();
+            cloneArray[i] = random.nextInt(50);
         }
     }
+
+    void isEvenNumber(int[] cloneArray) {
+        int evenNumber;
+        System.out.print("Array's even numbers are: ");
+        System.out.print("[");
+        for (int userNumber : cloneArray) {
+            if (userNumber % 2 == 0) {
+                evenNumber = userNumber;
+                System.out.print(" " + evenNumber + " ");
+            }
+        }
+        System.out.println("]");
+    }
 }
-
-
 
