bf25d5cfe92542e39babd1400bf9d3f0a7d45711
Evroleg <evroleg@gmail.com>
2022-11-02 18:21:41 +0200

    Why color detect?

---
+++ src/main/java/students/student_oleg_gryazev/lesson_4/level_5/Light.java
@@ -0,0 +1,27 @@
+package students.student_oleg_gryazev.lesson_4.level_5;
+
+class Light {
+public  void massiveLight(){
+
+}
+
+    public void violet() {
+        System.out.println("Violet");
+}
+public void blue() {
+    System.out.println("Blue");
+}
+public void green() {
+    System.out.println("Green");
+}
+public void yellow() {
+    System.out.println("Yellow");
+}
+public void orange() {
+            System.out.println("Orange");
+}
+public void red(){
+    System.out.println("Orange");
+}
+
+}
--- src/main/java/students/student_oleg_gryazev/lesson_4/level_5/LightColorDetector.java
+++ src/main/java/students/student_oleg_gryazev/lesson_4/level_5/LightColorDetector.java
@@ -2,52 +2,38 @@ package students.student_oleg_gryazev.lesson_4.level_5;
 
 import java.util.Scanner;
 
-public class LightColorDetector {
+class LightColorDetector {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int color = scanner.nextInt();
 
-        if (380 <= color & color <= 449  ){
-            System.out.println("Violet"); }
-        else if (450 <= color && color <= 494){
-            System.out.println("Blue");}
-        else if (495 <= color && color <= 569){
-            System.out.println("Green");}
-        else if (570 <= color && color <= 589){
-            System.out.println("Yellow");}
-        else if (590 <= color && color <= 619){
-            System.out.println("Orange");}
-        else if (620 <= color && color <= 750){
-            System.out.println("Red");}
-        System.out.println("Invisible Light");
+
+        Light light = new Light();
+        light.massiveLight();
+        {
+            int[] x = new int[1000];
+            int i;
+            for (i = 380; i <= 750; i++) {
+                x[i] = i;
+                int a = x[i];
+
+                if (a < 380 && a > 750) {
+
+                    System.out.println("Invisible spectr");
+                } else if (a >= 380 && a <= 449) {
+                    light.violet();
+                } else if (a >= 450 && a <= 494) {
+                    light.blue();
+                } else if (a >= 495 && a <= 569) {
+                    light.green();
+                } else if (a >= 570 && a <= 589) {
+                    light.yellow();
+                } else if (a >= 590 && a <= 619) {
+                    light.orange();
+                } else if (a >= 620 && a <= 750) {
+                    light.red();
+                }
+            }
+        }
     }
 }
-
-
-
-
-      //  switch (color) {
-
-
-
-          /*  case 1:
-                if (380 >= color && color <= 449) {
-
-                    light.violet = color;
-                    light.colorLightDetermination(light.violet);
-
-                    // System.out.println("Color violet confirmed.");
-                } else {
-                    System.out.println("Not violet");
-                }
-                break;*/
-
-       //
-        //  light.lightMassive();
-
-       //
-
-
-
 
