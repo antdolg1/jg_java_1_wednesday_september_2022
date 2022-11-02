3f0e44f5f0a839cbfb996a48872be98c0044d6bb
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-02 14:07:56 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_middle/ArrayUtilTest.java
@@ -0,0 +1,13 @@
+package students.student_ignat_parfenov.lesson_5.homework.level_5_middle;
+
+public class ArrayUtilTest {
+
+    public static void main(String[] args) {
+
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+    }
+    public void shouldCreateArray() {
+        int arrayLength = 7;
+    }
+}
