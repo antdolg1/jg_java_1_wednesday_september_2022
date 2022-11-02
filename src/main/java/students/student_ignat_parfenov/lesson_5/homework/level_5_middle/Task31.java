a7c354ff62d2c0c67cabefe4c953a92a3d50d937
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-02 13:38:41 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_middle/Task31.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_middle/Task31.java
@@ -2,4 +2,10 @@ package students.student_ignat_parfenov.lesson_5.homework.level_5_middle;
 
 public class Task31 {
 
+    class ArrayUtil {
+
+        public int[] createArray(int arrayLength) {
+        
+        }
+    }
 }
