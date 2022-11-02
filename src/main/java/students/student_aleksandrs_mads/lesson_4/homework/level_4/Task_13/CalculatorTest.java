f4fbf03864f1da7632bfdf0a9f7128454ce863fb
amads <amads@ddc.lv>
2022-11-02 10:08:27 +0200

    add homework lesson4 task13

---
+++ src/main/java/students/student_aleksandrs_mads/lesson_4/homework/level_4/Task_13/CalculatorTest.java
@@ -0,0 +1,77 @@
+package students.student_aleksandrs_mads.lesson_4.homework.level_4.Task_13;
+
+class CalculatorTest {
+    public static void main(String[] args) {
+        CalculatorTest calculatorTest = new CalculatorTest();
+//        calculatorTest.sumTest();
+//        calculatorTest.subtractionTest();
+//        calculatorTest.multiplicationTest();
+//        calculatorTest.divisionTest();
+        calculatorTest.isEvenTest();
+    }
+
+    public void sumTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 15;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Sum test = OK");
+        } else {
+            System.out.println("Sum test = FAIL");
+        }
+    }
+
+    public void subtractionTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 15;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Subtraction test = OK");
+        } else {
+            System.out.println("Subtraction test = FAIL");
+        }
+    }
+
+    public void multiplicationTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 15;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Multiplication test = OK");
+        } else {
+            System.out.println("Multiplication test = FAIL");
+        }
+    }
+
+    public void divisionTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 15;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Division test = OK");
+        } else {
+            System.out.println("Division test = FAIL");
+        }
+    }
+
+    public void isEvenTest() {
+        int testNumber = 0;
+        boolean result = true;
+        Calculator calculator = new Calculator();
+        boolean realResult = calculator.isEven(testNumber);
+        if (realResult == result) {
+            System.out.println("Even/odd test = OK");
+        } else {
+            System.out.println("Even/odd test = FAIL");
+        }
+    }
+}
+
