af30c0cf5330d31c8f60410898cae596f130c8a9
amads <amads@ddc.lv>
2022-11-02 11:10:12 +0200

    add homework lesson3 level2

--- src/main/java/students/student_aleksandrs_mads/lesson_3/homework/level_2/Task_9/Name.java
+++ src/main/java/students/student_aleksandrs_mads/lesson_3/homework/level_2/Task_9/Name.java
@@ -1,15 +1,12 @@
-package students.student_aleksandrs_mads.lesson_3.homework.level2.Task_9;
+package students.student_aleksandrs_mads.lesson_3.homework.level_2.Task_9;
 
 import java.util.Scanner;
 
-public class Name {
+class Name {
     public static void main(String[] args) {
         System.out.println("enter you name ");
         Scanner scanner = new Scanner(System.in);
         String myName = scanner.next();
-
         System.out.println("Hello " + myName + "!");
     }
-
-
 }
