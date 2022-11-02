d968ff0c92901b707206b5c5f0942d75b074db5d
Mihhail327 <mixa-amr@hotmail.com>
2022-11-02 22:05:27 +0200

    Lesson 2

--- src/main/java/students/student_mihhail_karasjov/lesson_2_level_3/Circle.java
+++ src/main/java/students/student_mihhail_karasjov/lesson_2_level_3/Circle.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Circle {
     public static void main(String[] args) {
-        Scanner io = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Input the radius of the circle: ");
-        double radius = io.nextDouble();
+        double radius = scanner.nextDouble();
         System.out.println("Perimeter is = " + (2 * radius * Math.PI));
         System.out.println("Area is = " + (Math.PI * radius * radius));
 
