d438064fa0f989d16a44b4c729a257abe1631615
Vlad-Rix <vladislavs.saksins@ya.ru>
2022-11-02 18:54:20 +0200

    Lesson 5 Level 3 Tasks 16-24

---
+++ src/main/java/students/student_vladislav_saksin/lesson_5/homework/level_3_junior/ForLoopDefinition1.java
@@ -0,0 +1,12 @@
+package students.student_vladislav_saksin.lesson_5.homework.level_3_junior;
+
+class ForLoopDefinition1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
