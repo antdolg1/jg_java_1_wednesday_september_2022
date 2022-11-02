ae36efc26f3e91795196c5ab4882a73a3115a14d
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-02 15:50:21 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/CustomArrayUtils.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/CustomArrayUtils.java
@@ -2,7 +2,9 @@ package students.student_ignat_parfenov.lesson_5.lessoncode;
 
 public class CustomArrayUtils {
 
-    static void printArrayToConsole(String[] array) {
-
+    static void element(String[] array) {
+        for (String element : array) {
+            System.out.println(element);
+        }
     }
 }
