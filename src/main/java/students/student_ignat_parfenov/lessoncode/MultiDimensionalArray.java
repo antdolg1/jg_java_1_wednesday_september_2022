19bccc8681fcb64bd3ba11d0e8326072b57ea681
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-02 21:38:20 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lessoncode/MultiDimensionalArray.java
+++ src/main/java/students/student_ignat_parfenov/lessoncode/MultiDimensionalArray.java
@@ -2,6 +2,29 @@ package students.student_ignat_parfenov.lessoncode;
 
 public class MultiDimensionalArray {
 
-    int[] values = {1, 25, 54, 76};
+    public static void main(String[] args) {
+        //  one dimensional array
+        int[] values = {1, 22, 333, 76};
+        //  multi dimensional array
+        int[][] table = {
+                {1, 22, 333, 444},
+                {2, 55},
+                {4, 56, 78, 76},
+                {3}
+        };
+        System.out.println(table[0][0]);
+        System.out.println(table[0][2]);
+        System.out.println(table[2][2]);
+
+
+
+        for (int row = 0; row < table.length; row++) {
+            for (int column = 0; column < table[row].length; column++);
+
+        }
+
+    }
+
 
 }
+
