36818c6ce2b21453adf6f4127feb0e464980528f
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-02 11:59:44 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/ArrayMinIndex.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/lessoncode/ArrayMinIndex.java
@@ -4,14 +4,21 @@ public class ArrayMinIndex {
 
     public static void main(String[] args) {
 
-        int[] minIndex = {2, 4, 7, 8, 1, 99};
+        int[] minIndex = {2, 4, 7, 8, -9, 5};
         int min = minIndex[0];
+        int max = minIndex[0];
 
         for (int i = 0; i < minIndex.length; i++) {
-            if (min < minIndex[i]) {
+            if (min > minIndex[i]) {
                 min = minIndex[i];
             }
         }
+        for (int j = 0; j < minIndex.length; j++) {
+            if (max < minIndex[j]) {
+                max = minIndex[j];
+            }
+        }
         System.out.println("The minimum = " + min);
+        System.out.println("The maximum = " + max);
     }
 }
