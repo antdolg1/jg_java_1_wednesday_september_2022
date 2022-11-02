06392fc4b748d3ba29b9048719eea3026b99dd7f
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-02 14:18:54 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_middle/ArrayUtil.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_middle/ArrayUtil.java
@@ -2,7 +2,7 @@ package students.student_ignat_parfenov.lesson_5.homework.level_5_middle;
 
 class ArrayUtil {
 
-    static int[] createArray(int arrayLength) {
+    public static int[] createArray(int arrayLength) {
         return new int[arrayLength];
     }
 }
