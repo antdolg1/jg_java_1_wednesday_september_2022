7dc81c57582d08fba69ff264d84006131ee41368
amads <amads@ddc.lv>
2022-11-02 11:51:18 +0200

    add homework lesson5 level1 task3

---
+++ src/main/java/students/student_aleksandrs_mads/lesson_5/homework/level_1/Task_3/ArryDefinition3.java
@@ -0,0 +1,8 @@
+package students.student_aleksandrs_mads.lesson_5.homework.level_1.Task_3;
+
+public class ArryDefinition3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+}
+
