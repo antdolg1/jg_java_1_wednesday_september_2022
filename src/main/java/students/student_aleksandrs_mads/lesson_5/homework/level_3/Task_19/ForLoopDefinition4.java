72dd4ec9748ff06c68b530ab54a1d7255959b087
amads <amads@ddc.lv>
2022-11-02 14:47:23 +0200

    add homework lesson5 level3 task16-24

---
+++ src/main/java/students/student_aleksandrs_mads/lesson_5/homework/level_3/Task_19/ForLoopDefinition4.java
@@ -0,0 +1,9 @@
+package students.student_aleksandrs_mads.lesson_5.homework.level_3.Task_19;
+
+class ForLoopDefinition4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/students/student_aleksandrs_mads/lesson_5/homework/level_3/Task_24/ForLoopDefinition8.java
@@ -0,0 +1,9 @@
+package students.student_aleksandrs_mads.lesson_5.homework.level_3.Task_24;
+
+class ForLoopDefinition8 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
