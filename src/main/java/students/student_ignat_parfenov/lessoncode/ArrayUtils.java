f7e825e85ca12049ea92705a640c9dd4051cdfbc
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-02 21:12:36 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lessoncode/ArrayUtils.java
+++ src/main/java/students/student_ignat_parfenov/lessoncode/ArrayUtils.java
@@ -1,5 +1,6 @@
 package students.student_ignat_parfenov.lessoncode;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class ArrayUtils {
@@ -21,5 +22,23 @@ public class ArrayUtils {
             System.out.println("Enter value for array element with index " + i);
             arrayToFill[i] = scanner.nextInt();
         }
+    }
+
+    static void fillArrayWithRandomNumbers(int[] arrayToFill) {
+        Random random = new Random();
+        for (int i = 0; i < arrayToFill.length; i++) {
+            arrayToFill[i] = random.nextInt(999);
+        }
+    }
+
+    static void printArrayToConsole(int[] arrayToPrint) {
+        for (int i = 0; i < arrayToPrint.length; i++) {
+            System.out.println("Array element value: " + arrayToPrint[i]);
+        }
+    }
+
+    static void findMaxNumberInArray(int[] array) {
+        int maxNumber = array[0];
+
     }
 }
