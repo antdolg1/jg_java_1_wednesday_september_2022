2ca799d7dae7dd0833cf186497f9d1b65f966194
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-03 13:21:45 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_middle/ArrayUtil.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_middle/ArrayUtil.java
@@ -1,10 +1,19 @@
 package students.student_ignat_parfenov.lesson_5.homework.level_5_middle;
 
+import java.util.Random;
+
 class ArrayUtil {
 
     public static int[] createArray(int arrayLength) {
         return new int[arrayLength];
     }
+
+    public void fillArrayWithRandomNumbers(int[] array) {
+        Random random = new Random();
+
+
+    }
+
 }
 
 
