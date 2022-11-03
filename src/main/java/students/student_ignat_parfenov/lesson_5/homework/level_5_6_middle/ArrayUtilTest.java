6d4cdbd1b0a293fa9c1a3e0038cdc1eb8ac91cda
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-03 14:18:05 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_6_middle/ArrayUtilTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_5/homework/level_5_6_middle/ArrayUtilTest.java
@@ -7,8 +7,7 @@ public class ArrayUtilTest {
     public static void main(String[] args) {
         ArrayUtilTest test = new ArrayUtilTest();
         test.shouldCreateArray();
-        test.shouldFindMaxNumber();
-        test.shouldFindMaxNumber();
+
     }
 
     public void shouldCreateArray() {
@@ -22,16 +21,5 @@ public class ArrayUtilTest {
             System.out.println("Current test is FAILED");
         }
     }
-
-    public void shouldFindMaxNumber() {
-        int[] array = {2, 56, 62, -12, 172};
-        int expectedResult = 172;
-//        int currentResult = findMaxNumber(array);
-//        if (expectedResult == currentResult) {
-//            System.out.println("Test of max number finding is DONE");
-//        } else {
-            System.out.println("Test is FAILED");
-        }
-    }
-
+}
 
