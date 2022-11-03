6ffd5588ecc031fb37a3b6e0094c9f28e2cc082d
Zanna Klimova <petraa@inbox.lv>
2022-11-03 11:03:04 +0200

    Add lesson 5, level 5,6 homework

---
+++ src/main/java/students/student_zanna_klimova/lesson_5/level_5/Array.java
@@ -0,0 +1,9 @@
+package students.student_zanna_klimova.lesson_5.level_5;
+
+public class Array {
+    public static void main(String[] args) {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        arrayUtil.getArrayLengthFromUser();
+        arrayUtil.newArray(5);
+    }
+}
--- src/main/java/students/student_zanna_klimova/lesson_5/level_5/ArrayUtilTest.java
+++ src/main/java/students/student_zanna_klimova/lesson_5/level_5/ArrayUtilTest.java
@@ -1,25 +1,52 @@
 package students.student_zanna_klimova.lesson_5.level_5;
 
-class ArrayUtilTest {
-        public static void main(String[] args) {
-            ArrayUtilTest test = new ArrayUtilTest();
-            test.shouldCreateArray();
+import teacher.lesson_5.homework.level_7_senior.task_41.ArrayUtil;
+
+public class ArrayUtilTest {
+    public static void main(String[] args) {
+        ArrayUtilTest arrayUtilTest = new ArrayUtilTest();
+        arrayUtilTest.shouldCreateArray();
+        arrayUtilTest.shouldFindMaxNumber();
+        arrayUtilTest.shouldFindMinNumber();
+
+    }
+    void shouldCreateArray() {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int arrayLength = 4;
+        int[] realResult = arrayUtil.newArray(arrayLength);
+        int expectedResult = 4;
+
+        if (realResult.length == expectedResult) {
+            System.out.println("Array length test OK");
+        } else {
+            System.out.println("Array length test FAIL");
         }
 
-        void shouldCreateArray() {
-            ArrayUtil arrayUtil = new ArrayUtil();
-            int arrayLength = 3;
-            int arrayLengthResult = 3;
-            int[] realArrayResult = arrayUtil.createArray(arrayLength);
-            checkArrayLength(realArrayResult, arrayLengthResult);
-        }
-
-        void checkArrayLength(int[] realArrayResult, int arrayLength) {
-            if (realArrayResult.length == arrayLength) {
-                System.out.println("Array length TEST = OK");
-            } else {
-                System.out.println("Array length TEST = FAIL");
-            }
-        }
     }
 
+    void shouldFindMaxNumber() {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] array = {11, 5, 10, 8, 5};
+        int expectedResult = 101;
+        int realResult = arrayUtil.findMaxInArray(array);
+
+        if (realResult == expectedResult) {
+            System.out.println("Max number test OK");
+        } else {
+            System.out.println("Max number test FAIL");
+        }
+    }
+    void shouldFindMinNumber() {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] array = {2, 8, 10, 3, 6};
+        int expectedResult = 2;
+        int realResult = arrayUtil.findMinInArray(array);
+
+        if (realResult == expectedResult) {
+            System.out.println("Min number test OK");
+        } else {
+            System.out.println("Min number test FAIL");
+        }
+
+    }
+}
