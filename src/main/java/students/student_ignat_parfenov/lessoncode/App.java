b132df68ff2fd98df7567ba1d86f6a03c3519366
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-03 12:52:40 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lessoncode/App.java
+++ src/main/java/students/student_ignat_parfenov/lessoncode/App.java
@@ -1,6 +1,22 @@
 package students.student_ignat_parfenov.lessoncode;
 
+public class Thing {
+
+    String name;
+    static String description;
+
+    void showName() {
+        System.out.println(name);
+    }
+
+    static void showDescription() {
+        System.out.println(description);
+    }
+}
+
 public class App {
 
-
+    public static void main(String[] args) {
+        
+    }
 }
