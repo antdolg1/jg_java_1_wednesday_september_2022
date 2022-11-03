e9ab2d0647f42148e62108141ed066039486e4bf
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-11-03 19:00:39 +0100

    Task_4

---
+++ src/main/java/students/student_jurij_hlebnikov/lesson_2/homework_leson_6/WhileDefinition4.java
@@ -0,0 +1,12 @@
+package students.student_jurij_hlebnikov.lesson_2.homework_leson_6;
+
+public class WhileDefinition4 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
