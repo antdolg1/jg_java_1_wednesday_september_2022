30d81cd3a33b0813ef8863589aa18b12bf92646e
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-03 15:29:05 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_1_intern/Task6.java
@@ -0,0 +1,17 @@
+package students.student_ignat_parfenov.lesson_6.homework.level_1_intern;
+
+public class Task6 {
+
+    class NumberUtils {
+
+        static boolean isEven(int number) {
+            if (number % 2 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+
+}
