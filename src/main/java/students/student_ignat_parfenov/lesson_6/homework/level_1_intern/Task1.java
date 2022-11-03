c4c6e9556a918254248c3a593ba242791c50184f
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-03 15:17:32 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_1_intern/Task1.java
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_1_intern/Task1.java
@@ -6,7 +6,7 @@ public class Task1 {
 
         public static void main(String[] args) {
             int i = 1;
-            while (i <= 100){
+            while (i <= 100) {
                 System.out.println(i);
                 i++;
             }
