2ffc65c903307cc14116ce0ec7ecf7a1c951272f
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-04 15:38:42 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetectorTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetectorTest.java
@@ -6,10 +6,26 @@ class DayOfTheWeekDetectorTest {
         DayOfTheWeekDetector dayOfTheWeekDetector = new DayOfTheWeekDetector();
         System.out.println(dayOfTheWeekDetector.findDayOfTheWeek(dayOfTheWeekDetector.getDayNumberFromUser()));
 //        dayOfTheWeekDetector.findDayOfTheWeek(dayOfTheWeekDetector.getDayNumberFromUser());
+        DayOfTheWeekDetectorTest test = new DayOfTheWeekDetectorTest();
+        test.shouldReturnMonday();
     }
 
     String findDayOfTheWeekTest(int dayNumber) {
         DayOfTheWeekDetector dayOfTheWeekDetector = new DayOfTheWeekDetector();
         return dayOfTheWeekDetector.findDayOfTheWeek(dayNumber);
+    }
+
+    public void shouldReturnMonday() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.findDayOfTheWeek(2);
+        checkTestResult("Monday".equals(dayOfTheWeek), "Tuesday");
+    }
+
+    private void checkTestResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " = OK!");
+        } else {
+            System.out.println(testName + " = FAIL!");
+        }
     }
 }
