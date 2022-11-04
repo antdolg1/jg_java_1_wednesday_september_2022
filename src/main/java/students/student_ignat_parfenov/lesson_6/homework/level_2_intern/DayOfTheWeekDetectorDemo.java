69debdef0f9868752a38de0180083c1b5acf4dd4
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-04 11:02:26 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,23 @@
+package students.student_ignat_parfenov.lesson_6.homework.level_2_intern;
+
+import java.util.Scanner;
+
+class DayOfTheWeekDetectorDemo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : ");
+        int dayNumber = scanner.nextInt();
+        switch (dayNumber) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Not correct day number");
+        }
+    }
+
+}
--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/Task7.java
+++
@@ -1,4 +0,0 @@
-package students.student_ignat_parfenov.lesson_6.homework.level_2_intern;
-
-public class Task7 {
-}
