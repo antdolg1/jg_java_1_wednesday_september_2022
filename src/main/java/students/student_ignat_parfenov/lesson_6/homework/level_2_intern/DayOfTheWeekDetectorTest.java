7d0d6060c038a2b620ecd5064e6f0549f0373dbe
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-04 12:26:18 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetectorTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetectorTest.java
@@ -2,4 +2,6 @@ package students.student_ignat_parfenov.lesson_6.homework.level_2_intern;
 
 class DayOfTheWeekDetectorTest {
 
+    DayOfTheWeekDetectorTest dayOfTheWeekDetectorTest = new DayOfTheWeekDetectorTest();
+
 }
