f71da8397c831b2291f2c8583a45f1aca628d61d
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-04 11:55:05 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetector.java
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetector.java
@@ -1,11 +1,17 @@
 package students.student_ignat_parfenov.lesson_6.homework.level_2_intern;
 
 import java.util.Scanner;
+
 class DayOfTheWeekDetector {
 
     public int getDayNumberFromUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input day number between 1 and 7 : ");
         return scanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
     }
 }
