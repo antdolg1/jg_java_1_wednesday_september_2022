e2729b60bc2d90e35186d0342b518aaeff0055d6
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-04 15:31:24 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetectorTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_2_intern/DayOfTheWeekDetectorTest.java
@@ -5,7 +5,7 @@ class DayOfTheWeekDetectorTest {
     public static void main(String[] args) {
         DayOfTheWeekDetector dayOfTheWeekDetector = new DayOfTheWeekDetector();
         System.out.println(dayOfTheWeekDetector.findDayOfTheWeek(dayOfTheWeekDetector.getDayNumberFromUser()));
-
+//        dayOfTheWeekDetector.findDayOfTheWeek(dayOfTheWeekDetector.getDayNumberFromUser());
     }
 
     String findDayOfTheWeekTest(int dayNumber) {
