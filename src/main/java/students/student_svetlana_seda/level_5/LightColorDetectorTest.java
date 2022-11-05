63285e2a7af98ecb284e75c5c21b76533e66a39e
java0711 <114607998+java0711@users.noreply.github.com>
2022-11-05 14:02:55 +0200

    Homework of lesson_4 - level 5 done

---
+++ src/main/java/students/student_svetlana_seda/level_5/LightColorDetectorTest.java
@@ -0,0 +1,29 @@
+package students.student_svetlana_seda.level_5;
+
+class LightColorDetectorTest {
+
+    public static void main(String[] args) {
+
+        LightColorDetectorTest lightColorDetectorTest = new LightColorDetectorTest();
+        // detectTest(); НАДО?
+        lightColorDetectorTest = new LightColorDetectorTest();
+        lightColorDetectorTest.detectTest();
+
+    }
+
+    private void detectTest() {
+
+        int waveLength = 600;
+        String expectedResult = "Orange";
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realResult = lightColorDetector.detect(waveLength);
+        if (realResult.equals(expectedResult)) {
+            System.out.println("LightColorDetectorTest is OK");
+        } else {
+            System.out.println("LightColorDetectorTest is FAIL");
+        }
+
+    }
+
+}
+
