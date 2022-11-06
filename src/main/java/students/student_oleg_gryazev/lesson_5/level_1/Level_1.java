16925e28d2c8fd46380cb8baa5ada1a40a0f687e
Evroleg <evroleg@gmail.com>
2022-11-06 15:28:52 +0200

    Why color detect?

---
+++ src/main/java/students/student_oleg_gryazev/lesson_5/level_1/Level_1.java
@@ -0,0 +1,65 @@
+package students.student_oleg_gryazev.lesson_5.level_1;
+
+public class Level_1 {
+}
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[1];
+    }
+}
+class ArrayDefinition2 {
+    public static void main(String[] args) {
+        byte [] numbers = new byte[10];
+    }
+}
+class ArrayDefinition3 {
+
+    public static void main(String[] args) {
+        boolean [] numbers = new boolean[10];
+    }
+}
+class ArrayDefinition4 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[]{1, 2, 3};
+    }
+}
+class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3 };
+    }
+}
+class ArrayDefinition6 {
+
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3} ;
+    }
+}
+class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[2];
+        System.out.println(numbers[-1]);
+    }
+}
+class ArrayLength2 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[2];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
+class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[2];
+        int arrayLength = numbers.length;
+        System.out.println(arrayLength);
+    }
+
+}
