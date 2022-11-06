07846353561dc7a5690f7c054b03d3affcf4aa80
iljamorozovs <ilja.morozovs@gmail.com>
2022-11-06 19:39:19 +0200

    change variable in while loop

--- src/main/java/students/student_ilja_morozovs/lesson_6/homework/level_4/Task_18/WhileInfiniteLoop.java
+++ src/main/java/students/student_ilja_morozovs/lesson_6/homework/level_4/Task_18/WhileInfiniteLoop.java
@@ -2,10 +2,10 @@ package students.student_ilja_morozovs.lesson_6.homework.level_4.Task_18;
 
 class WhileInfiniteLoop {
     public static void main(String[] args) {
-        boolean infiniteLoop = true;
-        int number = 0;
-        while (infiniteLoop) {
-            System.out.println(number++);
+        int numberOne = 0;
+        int numberTwo = numberOne + 1;
+        while (numberOne<numberTwo) {
+            System.out.println("Learning JAVA");
         }
     }
 }
