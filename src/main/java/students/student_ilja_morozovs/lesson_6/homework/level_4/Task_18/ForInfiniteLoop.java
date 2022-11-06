f2d288a28bf1e0e4bb607243045bceac9637c7f9
iljamorozovs <ilja.morozovs@gmail.com>
2022-11-06 19:36:34 +0200

    add infinite loop with "for" in ForInfiniteLoop

---
+++ src/main/java/students/student_ilja_morozovs/lesson_6/homework/level_4/Task_18/ForInfiniteLoop.java
@@ -0,0 +1,9 @@
+package students.student_ilja_morozovs.lesson_6.homework.level_4.Task_18;
+
+class ForInfiniteLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < i + 1; i++) {
+            System.out.println("Learning Java.");
+        }
+    }
+}
