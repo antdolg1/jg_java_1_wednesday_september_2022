ec54578810fe4a81325e0dcee6014a6ebcb4f359
java0711 <114607998+java0711@users.noreply.github.com>
2022-11-06 15:46:59 +0200

    Homework of Lesson_5 Task_1 - done - some corrections

--- src/main/java/students/student_svetlana_seda/level_1/ArrayDefinition1.java
+++ src/main/java/students/student_svetlana_seda/level_1/ArrayDefinition1.java
@@ -1,6 +1,6 @@
 package students.student_svetlana_seda.level_1;
 
-public class ArrayDefinition1 {
+class ArrayDefinition1 {
 
     public static void main(String[] args) {
         int[] numbers = new int[3];
