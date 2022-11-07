0b5f7499cb7a289e89b6c77b42c01dcb6d9116e2
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-07 12:16:26 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_3_junior/ArrayService.java
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_3_junior/ArrayService.java
@@ -1,11 +1,15 @@
 package students.student_ignat_parfenov.lesson_6.homework.level_3_junior;
 
+import java.util.Arrays;
+
 class ArrayService {
 
     int[] arr;
     int numberToSearch;
 
     boolean contains(int[] arr, int numberToSearch) {
-
+        boolean contains = Arrays.asList(arr).contains(Arrays.asList(numberToSearch));
+        System.out.println("Array contains the given number " + numberToSearch);
+        return contains;
     }
 }
