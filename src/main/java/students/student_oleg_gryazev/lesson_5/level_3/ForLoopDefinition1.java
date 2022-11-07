3f92db0eb9d91f746e654171e27a66a8ce7886fe
Evroleg <evroleg@gmail.com>
2022-11-07 16:03:47 +0200

    All tasks in level_3

---
+++ src/main/java/students/student_oleg_gryazev/lesson_5/level_3/ForLoopDefinition1.java
@@ -0,0 +1,71 @@
+package students.student_oleg_gryazev.lesson_5.level_3;
+
+public class ForLoopDefinition1 {                 // Task 16
+    public static void main(String[] args) {
+        for (int i=0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+class ForLoopDefinition2 {                      // 17
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+class ForLoopDefinition3 {                      // 18
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+class ForLoopDefinition4 {                      // 19
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+class ForLoopDefinition5 {                      // 20 21
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+class ForLoopDefinition6 {                      // 22
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+class ForLoopDefinition7 {                      // 23
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+class ForLoopDefinition8 {                      // 24
+
+    public static void main(String[] args) {
+
+        int [] arr = new int [100];
+
+        for(int i = 0; i< arr.length; i++)
+        arr[i] = i;
+        for (int i : arr)  {
+            System.out.println( i);
+        }
+    }
+
+}
