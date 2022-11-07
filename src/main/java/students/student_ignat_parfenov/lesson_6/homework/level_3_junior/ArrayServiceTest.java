36d9febe9ffe229a3f0bf4065757b058d8371e99
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-07 12:35:01 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_3_junior/ArrayServiceTest.java
@@ -0,0 +1,16 @@
+package students.student_ignat_parfenov.lesson_6.homework.level_3_junior;
+
+public class ArrayServiceTest {
+
+
+    public static void main(String[] args) {
+
+        ArrayServiceTest arrayServiceTest = new ArrayServiceTest();
+
+        private void isArrayContainsGivenNumber() {
+         int[] arr = {2, 5, 7, 9, 12};
+         int numberToSearch = 9;
+         ArrayService arrayService = new ArrayService();
+         boolean realResult = arrayService
+    }
+}
