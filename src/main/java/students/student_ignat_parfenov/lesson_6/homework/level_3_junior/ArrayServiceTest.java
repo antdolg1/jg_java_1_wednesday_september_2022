a238cff358c74680c36c62a02eb3672d3cff2bd2
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-07 15:35:56 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_3_junior/ArrayServiceTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_3_junior/ArrayServiceTest.java
@@ -6,16 +6,19 @@ public class ArrayServiceTest {
     public static void main(String[] args) {
 
         ArrayServiceTest arrayServiceTest = new ArrayServiceTest();
+        arrayServiceTest.isArrayContainsGivenNumber();
 
-        private void isArrayContainsGivenNumber() {
-         int[] arr = {2, 5, 7, 9, 12};
-         int numberToSearch = 9;
-         ArrayService arrayService = new ArrayService();
-         boolean realResult = arrayService.contains(arr, 9);
-         if (realResult) {
-             System.out.println("Test is DONE! Array consists the given number " + numberToSearch);
-         } else {
-             System.out.println("Test is FAILD! The given number " + numberToSearch + " doesn't consist in the array");
-         }
+    }
+
+    private void isArrayContainsGivenNumber() {
+        int[] arr = {2, 5, 7, 9, 12};
+        int numberToSearch = 9;
+        ArrayService arrayService = new ArrayService();
+        boolean realResult = arrayService.contains(arr, 9);
+        if (realResult) {
+            System.out.println("Test is DONE! Array consists the given number " + numberToSearch);
+        } else {
+            System.out.println("Test is FAILD! The given number " + numberToSearch + " doesn't consist in the array");
+        }
     }
 }
