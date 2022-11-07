0440869694662c903015e6da76ec7f7c959911fa
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-07 12:09:40 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_3_junior/ArrayService.java
+++ src/main/java/students/student_ignat_parfenov/lesson_6/homework/level_3_junior/ArrayService.java
@@ -1,4 +1,11 @@
 package students.student_ignat_parfenov.lesson_6.homework.level_3_junior;
 
 class ArrayService {
+
+    int[] arr;
+    int numberToSearch;
+
+    boolean contains(int[] arr, int numberToSearch) {
+
+    }
 }
