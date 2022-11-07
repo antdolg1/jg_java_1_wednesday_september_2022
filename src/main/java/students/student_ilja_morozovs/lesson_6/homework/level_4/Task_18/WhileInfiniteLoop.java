e64cd4574199eee98a410cc9d72b43e94e7947c1
iljamorozovs <ilja.morozovs@gmail.com>
2022-11-07 14:32:05 +0200

    change variable

--- src/main/java/students/student_ilja_morozovs/lesson_6/homework/level_4/Task_18/WhileInfiniteLoop.java
+++ src/main/java/students/student_ilja_morozovs/lesson_6/homework/level_4/Task_18/WhileInfiniteLoop.java
@@ -2,9 +2,8 @@ package students.student_ilja_morozovs.lesson_6.homework.level_4.Task_18;
 
 class WhileInfiniteLoop {
     public static void main(String[] args) {
-        int numberOne = 0;
-        int numberTwo = numberOne + 1;
-        while (numberOne<numberTwo) {
+        int i = 0;
+        while (i < i + 1) {
             System.out.println("Learning JAVA");
         }
     }
