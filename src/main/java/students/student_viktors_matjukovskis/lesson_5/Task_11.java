aa98c5bb5a26844ba9a4ef28518d778a86b6b3c4
viktors1989 <viktors@4k.lv>
2022-11-09 18:56:19 +0200

    Home work lesson5

---
+++ src/main/java/students/student_viktors_matjukovskis/lesson_5/Task_11.java
@@ -0,0 +1,22 @@
+package students.student_viktors_matjukovskis.lesson_5;
+
+import java.util.Scanner;
+
+public class Task_11 {
+
+    public static void main(String[] agrs){
+
+        int[] a = new int[3];
+        int i = 0;
+        Scanner in = new Scanner(System.in);
+
+        for (int element : a){
+        System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
+            a[i] = in.nextInt();
+            System.out.println(a[i] + " - ");
+            i++;
+        }
+        for (int element : a)
+            System.out.print(element + " ");
+    }
+}
