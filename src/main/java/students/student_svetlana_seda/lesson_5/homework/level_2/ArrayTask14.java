6de0d11a578fdbe172950181dbfc422f509f5b1c
java0711 <114607998+java0711@users.noreply.github.com>
2022-11-09 19:42:42 +0200

    Homework of Lesson_5 Level_2 - done

---
+++ src/main/java/students/student_svetlana_seda/lesson_5/homework/level_2/ArrayTask14.java
@@ -0,0 +1,16 @@
+package students.student_svetlana_seda.lesson_5.homework.level_2;
+
+class ArrayTask14 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+        numbers[0] = 4;
+        numbers[1] = 2;
+        numbers[2] = 45;
+
+        int averageNumber = (numbers[0] + numbers[1] + numbers[2]) / 3;
+        System.out.println("Average number is = " + averageNumber);
+    }
+
+}
---
+++ src/main/java/students/student_svetlana_seda/lesson_5/homework/level_2/ArrayTask15.java
@@ -0,0 +1,22 @@
+package students.student_svetlana_seda.lesson_5.homework.level_2;
+
+class ArrayTask15 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers[0] = 4;
+        numbers[1] = 2;
+        numbers[2] = 45;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+        numbers[0]+=2;
+        numbers[1]+=2;
+        numbers[2]+=2;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+    }
+
+}
