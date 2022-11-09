990974a843cf4d375c60a8d72ec6e921a32e4131
java0711 <114607998+java0711@users.noreply.github.com>
2022-11-09 17:10:07 +0200

    Homework of Lesson_5 Level_1 - done

---
+++ src/main/java/students/student_svetlana_seda/lesson_5/homework/level_1/ArrayDefinition6.java
@@ -0,0 +1,9 @@
+package students.student_svetlana_seda.lesson_5.homework.level_1;
+
+class ArrayDefinition6 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+
+}
