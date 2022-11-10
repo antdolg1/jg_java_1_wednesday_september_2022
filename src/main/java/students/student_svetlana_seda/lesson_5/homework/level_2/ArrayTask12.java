22522ce792b1d851be9ceec4ab5369c23e2c2148
java0711 <114607998+java0711@users.noreply.github.com>
2022-11-10 21:54:32 +0200

    Homework of lesson 5, Level 2, task 12 -corrected

--- src/main/java/students/student_svetlana_seda/lesson_5/homework/level_2/ArrayTask12.java
+++ src/main/java/students/student_svetlana_seda/lesson_5/homework/level_2/ArrayTask12.java
@@ -1,18 +1,24 @@
 package students.student_svetlana_seda.lesson_5.homework.level_2;
 
+import java.util.Random;
+
 class ArrayTask12 {
 
     public static void main(String[] args) {
+        Random random = new Random();
+
+        int number1 = random.nextInt(0, 28);
+        int number2 = random.nextInt(0, 28);
+        int number3 = random.nextInt(0, 28);
 
         int[] numbers = new int[3];
-        numbers[0] = 4;
-        numbers[1] = 2;
-        numbers[2] = 45;
+        numbers[0] = number1;
+        numbers[1] = number2;
+        numbers[2] = number3;
 
-        System.out.println(numbers[0]);
-        System.out.println(numbers[1]);
-        System.out.println(numbers[2]);
+        System.out.println("In the array, we have numbers: [" + numbers[0] + "], " + "[" + numbers[1] + "], [" + numbers[2] + "]! ");
 
     }
 
 }
+
