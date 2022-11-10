f88e42debb13463a319c7278c5d18cedbab595b6
java0711 <114607998+java0711@users.noreply.github.com>
2022-11-10 20:23:35 +0200

    Homework of Lesson_5 - Level_3 - done

---
+++ src/main/java/students/student_svetlana_seda/lesson_5/homework/level_3/ForLoopDefinition5.java
@@ -0,0 +1,11 @@
+package students.student_svetlana_seda.lesson_5.homework.level_3;
+
+class ForLoopDefinition5 {
+
+    public static void main(String[] args) {
+        for (int i = 3; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+
+}
