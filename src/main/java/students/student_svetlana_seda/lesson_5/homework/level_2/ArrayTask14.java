6dc92e2fe3a96c8e5327e91666962c8d4cf960ec
java0711 <114607998+java0711@users.noreply.github.com>
2022-11-12 14:55:10 +0200

    Homework of lesson 5, Level 2, task 13, 14, 15 -corrected

--- src/main/java/students/student_svetlana_seda/lesson_5/homework/level_2/ArrayTask14.java
+++ src/main/java/students/student_svetlana_seda/lesson_5/homework/level_2/ArrayTask14.java
@@ -1,13 +1,24 @@
 package students.student_svetlana_seda.lesson_5.homework.level_2;
 
-class ArrayTask14 {
+import java.util.Random;
 
+class ArrayTask14 {
     public static void main(String[] args) {
 
+        Random random = new Random();
+
+        int number1 = random.nextInt(0, 10);
+        int number2 = random.nextInt(0, 10);
+        int number3 = random.nextInt(0, 10);
+
         int[] numbers = new int[3];
-        numbers[0] = 4;
-        numbers[1] = 2;
-        numbers[2] = 45;
+        numbers[0] = number1;
+        numbers[1] = number2;
+        numbers[2] = number3;
+
+        System.out.println("First number is " + number1);
+        System.out.println("Second number is " + number2);
+        System.out.println("Third number is " + number3);
 
         int averageNumber = (numbers[0] + numbers[1] + numbers[2]) / 3;
         System.out.println("Average number is = " + averageNumber);
