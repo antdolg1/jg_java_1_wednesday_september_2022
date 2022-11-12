178a726258f94ffd48d56cb6d6a6b24831f82853
java0711 <114607998+java0711@users.noreply.github.com>
2022-11-12 14:55:10 +0200

    Homework of lesson 5, Level 2, task 13, 14, 15 -corrected

--- src/main/java/students/student_svetlana_seda/lesson_5/homework/level_2/ArrayTask15.java
+++ src/main/java/students/student_svetlana_seda/lesson_5/homework/level_2/ArrayTask15.java
@@ -1,18 +1,27 @@
 package students.student_svetlana_seda.lesson_5.homework.level_2;
 
+import java.util.Random;
+
 class ArrayTask15 {
     public static void main(String[] args) {
-        int[] numbers = new int[3];
-        numbers[0] = 4;
-        numbers[1] = 2;
-        numbers[2] = 45;
-        System.out.println(numbers[0]);
-        System.out.println(numbers[1]);
-        System.out.println(numbers[2]);
+        Random random = new Random();
 
-        numbers[0]+=2;
-        numbers[1]+=2;
-        numbers[2]+=2;
+        int number1 = random.nextInt(0, 10);
+        int number2 = random.nextInt(0, 10);
+        int number3 = random.nextInt(0, 10);
+
+        int[] numbers = new int[3];
+        numbers[0] = number1;
+        numbers[1] = number2;
+        numbers[2] = number3;
+
+        System.out.println("First number is " + number1);
+        System.out.println("Second number is " + number2);
+        System.out.println("Third number is " + number3);
+
+        numbers[0] += 2;
+        numbers[1] += 2;
+        numbers[2] += 2;
         System.out.println(numbers[0]);
         System.out.println(numbers[1]);
         System.out.println(numbers[2]);
