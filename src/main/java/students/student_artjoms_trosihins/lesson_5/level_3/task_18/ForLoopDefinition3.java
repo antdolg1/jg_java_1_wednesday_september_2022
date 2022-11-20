61bfcf0ed93bddc0f6c335891034f2d61588326d
ArtGobl1n <artjoms122@gmail.com>
2022-11-20 20:11:44 +0200

    Lesson 5 level 3 HW added

---
+++ src/main/java/students/student_artjoms_trosihins/lesson_5/level_3/task_18/ForLoopDefinition3.java
@@ -0,0 +1,10 @@
+package students.student_artjoms_trosihins.lesson_5.level_3.task_18;
+
+ class ForLoopDefinition3 {
+
+     public static void main(String[] args) {
+         for(int i = 0; i < 10; i++){
+             System.out.println(i);
+         }
+     }
+}
