ebe55fce3afa1c80ef7018c5445a1e7f1d3a31f8
amads <amads@ddc.lv>
2022-11-25 11:22:03 +0200

    add homework lesson6 level2 Task9

---
+++ src/main/java/students/student_aleksandrs_mads/lesson_6/homework/level_2/Task_9/DayOfTheWeekDetectorTest.java
@@ -0,0 +1,72 @@
+package students.student_aleksandrs_mads.lesson_6.homework.level_2.Task_9;
+
+class DayOfTheWeekDetectorTest {
+    public static void main(String[] args) {
+        DayOfTheWeekDetectorTest test = new DayOfTheWeekDetectorTest();
+        test.shouldReturnMonday();
+        test.shouldReturnTuesday();
+        test.shouldReturnWednesday();
+        test.shouldReturnThursday();
+        test.shouldReturnFriday();
+        test.shouldReturnSaturday();
+        test.shouldReturnSunday();
+        test.shouldReturnWrongDay();
+
+    }
+
+    public void shouldReturnMonday() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.dayNumber(1);
+        checkTestResult("Monday".equals(dayOfTheWeek), "Monday");
+    }
+
+    public void shouldReturnTuesday() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.dayNumber(2);
+        checkTestResult("Tuesday".equals(dayOfTheWeek), "Tuesday");
+    }
+
+    public void shouldReturnWednesday() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.dayNumber(3);
+        checkTestResult("Wednesday".equals(dayOfTheWeek), "Wednesday");
+    }
+
+    public void shouldReturnThursday() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.dayNumber(4);
+        checkTestResult("Thursday".equals(dayOfTheWeek), "Thursday");
+    }
+
+    public void shouldReturnFriday() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.dayNumber(5);
+        checkTestResult("Friday".equals(dayOfTheWeek), "Friday");
+    }
+
+    public void shouldReturnSaturday() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.dayNumber(6);
+        checkTestResult("Saturday".equals(dayOfTheWeek), "Saturday");
+    }
+
+    public void shouldReturnSunday() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.dayNumber(7);
+        checkTestResult("Sunday".equals(dayOfTheWeek), "Sunday");
+    }
+
+    public void shouldReturnWrongDay() {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String dayOfTheWeek = detector.dayNumber(99);
+        checkTestResult("Not correct day number".equals(dayOfTheWeek), "Not correct day number");
+    }
+
+    private void checkTestResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " = OK!");
+        } else {
+            System.out.println(testName + " = FAIL!");
+        }
+    }
+}
