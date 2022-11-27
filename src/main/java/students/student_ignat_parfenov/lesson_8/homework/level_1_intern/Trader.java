4f4a7bfbb519f8d8b61197b4218259043b930d7a
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-27 12:04:49 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_8/homework/level_1_intern/Trader.java
+++ src/main/java/students/student_ignat_parfenov/lesson_8/homework/level_1_intern/Trader.java
@@ -4,7 +4,7 @@ class Trader {
     private String fullName;
     private String city;
 
-    public Trader(String fullName, String city) {
+    Trader(String fullName, String city) {
         this.fullName = fullName;
         this.city = city;
     }
@@ -14,7 +14,7 @@ class Transaction {
     private Trader trader;
     private int amount;
 
-    public Transaction(Trader trader, int amount) {
+    Transaction(Trader trader, int amount) {
         this.trader = trader;
         this.amount = amount;
     }
