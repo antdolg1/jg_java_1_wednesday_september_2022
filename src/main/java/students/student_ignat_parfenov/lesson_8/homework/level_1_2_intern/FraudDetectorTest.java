d35a5c8870b78162e75e83aba1675262dc7e03e6
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-27 12:57:22 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_8/homework/level_1_2_intern/FraudDetectorTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_8/homework/level_1_2_intern/FraudDetectorTest.java
@@ -1,6 +1,9 @@
 package students.student_ignat_parfenov.lesson_8.homework.level_1_2_intern;
 
 public class FraudDetectorTest {
+    public static void main(String[] args) {
 
+        Trader trader = new Trader("Pokemon", "New York");
 
+    }
 }
