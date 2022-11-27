ee194be13a1315fcd852719194120f89407f63d8
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-11-27 15:30:07 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_8/homework/level_1_2_intern/FraudDetectorTest.java
+++ src/main/java/students/student_ignat_parfenov/lesson_8/homework/level_1_2_intern/FraudDetectorTest.java
@@ -5,15 +5,11 @@ public class FraudDetectorTest {
     public static void main(String[] args) {
 
         Trader trader1 = new Trader("Pokemon", "New York");
+        Transaction transaction1 = new Transaction(trader1, 55000);
         if (trader1.getFullName().equals("Pokemon")) {
             System.out.println("Transaction is interrupted. Test is FAILED");
-        } else {
-            System.out.println("Transaction is done. Test is OK");
-        }
-
-        Transaction transaction1 = new Transaction(trader1, 120000);
-        if (transaction1.getAmount() <= 100000) {
-            System.out.println("Transaction is declined. The sum is over 100000. Test is FAILED");
+        } else if ((transaction1.getAmount() > 1000000)) {
+            System.out.println("Transaction is interrupted. The sum of transaction is exceeded! Test is FAILED");
         } else {
             System.out.println("Transaction is APPLIED. Test is OK");
         }
