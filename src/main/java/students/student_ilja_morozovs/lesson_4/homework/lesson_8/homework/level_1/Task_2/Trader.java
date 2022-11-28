abe361efc0009477e5c0cbaf66443178d5a8276a
iljamorozovs <ilja.morozovs@gmail.com>
2022-11-28 09:03:56 +0200

    add constructor for Trader class

---
+++ src/main/java/students/student_ilja_morozovs/lesson_4/homework/lesson_8/homework/level_1/Task_2/Trader.java
@@ -0,0 +1,11 @@
+package students.student_ilja_morozovs.lesson_4.homework.lesson_8.homework.level_1.Task_2;
+
+class Trader {
+    private String fullName;
+    private String city;
+
+    public Trader(String fullName, String city) {
+        this.fullName = fullName;
+        this.city = city;
+    }
+}
