91c6a78217735b78572f7a3e01549d2ae9c94fcf
iljamorozovs <ilja.morozovs@gmail.com>
2022-11-28 09:01:57 +0200

    add variable

---
+++ src/main/java/students/student_ilja_morozovs/lesson_4/homework/lesson_8/homework/level_1/Task_1/FraudDetector.java
@@ -0,0 +1,7 @@
+package students.student_ilja_morozovs.lesson_4.homework.lesson_8.homework.level_1.Task_1;
+
+class FraudDetector {
+    boolean isFraud(Transaction t) {
+        return true;
+    }
+}
---
+++ src/main/java/students/student_ilja_morozovs/lesson_4/homework/lesson_8/homework/level_1/Task_1/Trader.java
@@ -0,0 +1,6 @@
+package students.student_ilja_morozovs.lesson_4.homework.lesson_8.homework.level_1.Task_1;
+
+class Trader {
+    private String fullName;
+    private String city;
+}
