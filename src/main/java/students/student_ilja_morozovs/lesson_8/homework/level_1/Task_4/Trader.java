cffbc2f044169da145451559a253a84114feec74
iljamorozovs <ilja.morozovs@gmail.com>
2022-11-28 09:20:37 +0200

    add constructor in Transaction class for variable

---
+++ src/main/java/students/student_ilja_morozovs/lesson_8/homework/level_1/Task_4/Trader.java
@@ -0,0 +1,27 @@
+package students.student_ilja_morozovs.lesson_8.homework.level_1.Task_4;
+
+class Trader {
+    private String fullName;
+    private String city;
+
+    public Trader(String fullName, String city) {
+        this.fullName = fullName;
+        this.city = city;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/students/student_ilja_morozovs/lesson_8/homework/level_1/Task_4/Transaction.java
@@ -0,0 +1,11 @@
+package students.student_ilja_morozovs.lesson_8.homework.level_1.Task_4;
+
+class Transaction {
+    private Trader trader;
+    private int amount;
+
+    public Transaction(Trader trader, int amount) {
+        this.trader = trader;
+        this.amount = amount;
+    }
+}
