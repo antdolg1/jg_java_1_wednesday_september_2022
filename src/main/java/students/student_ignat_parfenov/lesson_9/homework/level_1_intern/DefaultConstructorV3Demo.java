219f1cb09215481706957bd1ff8adf01c9f3e988
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-01 11:35:33 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_1_intern/DefaultConstructorV3Demo.java
@@ -0,0 +1,11 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_1_intern;
+
+public class DefaultConstructorV3Demo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructorV3 defaultConstructorV3 = new DefaultConstructorV3();
+
+        System.out.println(defaultConstructorV3);
+    }
+}
