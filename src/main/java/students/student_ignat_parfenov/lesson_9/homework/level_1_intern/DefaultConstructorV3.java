21d967dc52d5cb1f910f907bc4235dd304229260
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-01 11:32:05 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_1_intern/DefaultConstructorV3.java
@@ -0,0 +1,25 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_1_intern;
+
+public class DefaultConstructorV3 {
+
+    private String fullName;
+    private int age;
+    private boolean male;
+    private Boolean female;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public Boolean getFemale() {
+        return female;
+    }
+}
