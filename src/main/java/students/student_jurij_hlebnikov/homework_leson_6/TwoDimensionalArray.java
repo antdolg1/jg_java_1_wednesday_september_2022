83405db7be52f527afe36bb010111d8eb4ee7ca2
Jurij Hlebnikov <jurij.hlebnikov@gmail.com>
2022-12-03 17:10:46 +0100

    Task 21

--- src/main/java/students/student_jurij_hlebnikov/homework_leson_6/TwoDimensionalArray.java
+++ src/main/java/students/student_jurij_hlebnikov/homework_leson_6/TwoDimensionalArray.java
@@ -1,20 +1,25 @@
 package students.student_jurij_hlebnikov.homework_leson_6;
 
-class TwoDimensionalArray {
+class TwoDimensionalArray {             //Task 21
 
     public static void main(String[] args) {
 
-        int axisX = (int) (Math.random() * 10 + 1);
-        int axisY = (int) (Math.random() * 10 + 1);
+        TwoDimensionalArrayMethods twoDimensionalArray = new TwoDimensionalArrayMethods();
+
+        int sum = 0;
+        int axisX = twoDimensionalArray.mathRandom();
+        int axisY = twoDimensionalArray.mathRandom();
         int[][] array = new int[axisX][axisY];
 
         for (int x = 0; x < array.length; x++) {
             for (int y = 0; y < array[x].length; y++) {
                 array[x][y] = (int) (Math.random() * 10 + 1);
 
-                System.out.print(array[x][y] + " ");
+                System.out.print(array[x][y] + "    ");
+                sum += array[x][y];
             }
             System.out.println();
         }
+        System.out.println("Sum of all number is :" + sum);
     }
 }
---
+++ src/main/java/students/student_jurij_hlebnikov/homework_leson_6/TwoDimensionalArrayMethods.java
@@ -0,0 +1,13 @@
+package students.student_jurij_hlebnikov.homework_leson_6;
+
+class TwoDimensionalArrayMethods {
+
+    int random;
+
+
+    public int mathRandom() {
+        random = (int) (Math.random() * 10 + 1);
+        return random;
+    }
+
+}
