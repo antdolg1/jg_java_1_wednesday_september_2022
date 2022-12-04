3e8945b923d616cd86f8d4b1882b8966b5cd2cd4
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-04 14:30:37 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Player.java
@@ -0,0 +1,18 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_2_intern;
+
+class Player {
+
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+}
+
+class HumanPlayer extends Player {
+
+    HumanPlayer(String name) {
+        super(String name);
+
+    }
+}
