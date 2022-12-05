f1cec9e39d4a4df360f186d7e71a2ec2dff373a8
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-05 10:36:01 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/ComputerPlayer.java
@@ -0,0 +1,8 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_2_intern.Task7;
+
+class ComputerPlayer extends Player {
+
+    public ComputerPlayer(String name) {
+        super(name);
+    }
+}
