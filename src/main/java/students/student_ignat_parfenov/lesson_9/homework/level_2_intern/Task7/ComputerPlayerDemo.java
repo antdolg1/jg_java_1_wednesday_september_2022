58aff24dfe4baa4583b5249ab0dc43cb0e47fbb1
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-05 10:40:58 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/ComputerPlayerDemo.java
@@ -0,0 +1,13 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_2_intern.Task7;
+
+public class ComputerPlayerDemo {
+
+    public static void main(String[] args) {
+
+        Player player = new Player("Ivan");
+        ComputerPlayer computerPlayer = new ComputerPlayer("Ivan");
+
+        System.out.println(player);
+        System.out.println(computerPlayer);
+    }
+}
