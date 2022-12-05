73e2d8ec6cd5825075ab6f5a4ddeeb1db6810879
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-05 11:00:50 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/ComputerPlayer.java
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/ComputerPlayer.java
@@ -2,14 +2,16 @@ package students.student_ignat_parfenov.lesson_9.homework.level_2_intern.Task7;
 
 class ComputerPlayer extends Player {
 
-    private int expirience;
+    private int experience;
 
-    public ComputerPlayer(String name, int expirience) {
+    public ComputerPlayer(String name, int experience) {
         super(name);
     }
 
     @Override
     public String toString() {
-        return "ComputerPlayer{}";
+        return "ComputerPlayer{" +
+                "experience=" + experience +
+                '}';
     }
 }
