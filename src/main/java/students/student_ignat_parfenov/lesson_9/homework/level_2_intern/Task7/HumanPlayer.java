4f984a32f154af38397292d1272bb838e1d8cc48
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-05 10:31:33 +0200

    Add lesson code

--- src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/HumanPlayer.java
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/HumanPlayer.java
@@ -1,6 +1,6 @@
 package students.student_ignat_parfenov.lesson_9.homework.level_2_intern.Task7;
 
-public class HumanPlayer extends Player {
+class HumanPlayer extends Player {
 
     HumanPlayer(String name) {
         super(name);
