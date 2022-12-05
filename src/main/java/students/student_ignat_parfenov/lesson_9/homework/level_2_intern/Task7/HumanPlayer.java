ad592da40cf382c044b28d13f777fd077edaef4a
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-05 10:30:34 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/HumanPlayer.java
@@ -0,0 +1,9 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_2_intern.Task7;
+
+public class HumanPlayer extends Player {
+
+    HumanPlayer(String name) {
+        super(name);
+    }
+}
+
--- src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/Player.java
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_2_intern/Task7/Player.java
@@ -9,9 +9,3 @@ class Player {
     }
 }
 
-class HumanPlayer extends Player {
-
-    HumanPlayer(String name) {
-        super(name);
-    }
-}
