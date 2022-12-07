f41c6e8a8ad67fcdfcd0344a5caeec09e9a02ec8
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-07 19:08:29 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_3_junior/Task13/ElectricCar.java
@@ -0,0 +1,11 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_3_junior.Task13;
+
+class ElectricCar extends Car {
+
+    private int batteryCharge;
+
+    @Override
+    void accelerate() {
+
+    }
+}
---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_3_junior/Task13/PetrolCar.java
@@ -0,0 +1,11 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_3_junior.Task13;
+
+class PetrolCar extends Car {
+
+    private double petrolTankCapacity;
+
+    @Override
+    void accelerate() {
+
+    }
+}
