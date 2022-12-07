a28df0dffbf835408e9934a39fdbcfd106412e1c
joseip08 <114609331+joseip08@users.noreply.github.com>
2022-12-07 19:01:38 +0200

    Add lesson code

---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_3_junior/Human.java
@@ -0,0 +1,6 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_3_junior;
+
+public abstract class Human {
+
+    abstract boolean canReceiveLoan(Human human);
+}
---
+++ src/main/java/students/student_ignat_parfenov/lesson_9/homework/level_3_junior/LoanSystem.java
@@ -0,0 +1,5 @@
+package students.student_ignat_parfenov.lesson_9.homework.level_3_junior;
+
+public abstract class LoanSystem {
+
+}
