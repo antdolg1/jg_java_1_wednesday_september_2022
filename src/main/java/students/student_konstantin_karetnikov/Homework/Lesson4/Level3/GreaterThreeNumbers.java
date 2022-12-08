e9edac710d03bf2e659538310bf5a0926e949346
kskaretnikov <114775910+kskaretnikov@users.noreply.github.com>
2022-12-08 14:46:37 +0300

    Update GreaterThreeNumbers.java
    Greater --> Greatest

--- src/main/java/students/student_konstantin_karetnikov/Homework/Lesson4/Level3/GreaterThreeNumbers.java
+++ src/main/java/students/student_konstantin_karetnikov/Homework/Lesson4/Level3/GreaterThreeNumbers.java
@@ -2,7 +2,7 @@ package students.student_konstantin_karetnikov.Homework.Lesson4.Level3;
 
 import java.util.Scanner;
 
- class GreaterThreeNumbers {
+ class GreatestThreeNumbers {
     public static void main(String[] args) {
         System.out.print("Please enter first number: ");
         Scanner scanner = new Scanner(System.in);
@@ -14,15 +14,12 @@ import java.util.Scanner;
         scanner = new Scanner(System.in);
         int number3 = scanner.nextInt();
         if (number1 > number2 && number1 > number3) {
-            System.out.println("Greater number is " + number1);
-        }
-        else if (number2 > number1 && number2 > number3){
-            System.out.println("Greater number is " + number2);
-        }
-        else if (number3 > number1 && number3 > number2){
-            System.out.println("Greater number is " + number3);
-        }
-        else {
+            System.out.println("Greatest number is " + number1);
+        }         else if (number2 > number1 && number2 > number3){
+            System.out.println("Greatest number is " + number2);
+        }        else if (number3 > number1 && number3 > number2){
+            System.out.println("Greatest number is " + number3);
+        }        else {
             System.out.println("All number are equal");
         }
     }
