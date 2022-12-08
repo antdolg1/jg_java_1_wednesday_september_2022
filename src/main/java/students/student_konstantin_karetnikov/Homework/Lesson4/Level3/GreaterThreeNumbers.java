a2238cfe675f20a7ecce1be7fb83c66b04cb0487
kskaretnikov <114775910+kskaretnikov@users.noreply.github.com>
2022-12-08 14:34:47 +0300

    Update GreaterThreeNumbers.java
    Убрал скобки вокруг переменных. В других классах если встречу тоже уберу.

--- src/main/java/students/student_konstantin_karetnikov/Homework/Lesson4/Level3/GreaterThreeNumbers.java
+++ src/main/java/students/student_konstantin_karetnikov/Homework/Lesson4/Level3/GreaterThreeNumbers.java
@@ -14,13 +14,13 @@ import java.util.Scanner;
         scanner = new Scanner(System.in);
         int number3 = scanner.nextInt();
         if (number1 > number2 && number1 > number3) {
-            System.out.println("Greater number is " + (number1));
+            System.out.println("Greater number is " + number1);
         }
         else if (number2 > number1 && number2 > number3){
-            System.out.println("Greater number is " + (number2));
+            System.out.println("Greater number is " + number2);
         }
         else if (number3 > number1 && number3 > number2){
-            System.out.println("Greater number is " + (number3));
+            System.out.println("Greater number is " + number3);
         }
         else {
             System.out.println("All number are equal");
