c1605e291c2a1e4489e537e328653312128767b6
viktors1989 <viktors@4k.lv>
2022-12-08 20:40:00 +0200

    Revert "Home work lesson5 level_1 and level_2 completed"
    This reverts commit 5a1b67c2c29d43b5c98b41e8c92cff1b3fa0e3f4.

--- src/main/java/students/student_viktors_matjukovskis/lesson_10/Task_1.java
+++
@@ -1,4 +0,0 @@
-package students.student_viktors_matjukovskis.lesson_10;
-
-public class Task_1 {
-}
--- src/main/java/students/student_viktors_matjukovskis/lesson_5/Task_11.java
+++ src/main/java/students/student_viktors_matjukovskis/lesson_5/Task_11.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Task_11 {
 
-    public static void main(String[] agrs) {
+    public static void main(String[] agrs){
 
         int[] a = new int[3];
         int i = 0;
         Scanner in = new Scanner(System.in);
 
-        for (int element : a) {
-            System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
+        for (int element : a){
+        System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
             a[i] = in.nextInt();
             System.out.println(a[i] + " - ");
             i++;
