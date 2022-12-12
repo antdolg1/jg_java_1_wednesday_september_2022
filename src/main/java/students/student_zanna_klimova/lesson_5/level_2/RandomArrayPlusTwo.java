0d91628807fb333656e52148c1b778df7ea644d3
Zanna Klimova <petraa@inbox.lv>
2022-12-12 15:09:47 +0200

    Fixed mistakes

--- src/main/java/students/student_zanna_klimova/lesson_5/level_2/RandomArrayPlusTwo.java
+++ src/main/java/students/student_zanna_klimova/lesson_5/level_2/RandomArrayPlusTwo.java
@@ -1,15 +1,17 @@
 package students.student_zanna_klimova.lesson_5.level_2;
 
+import java.util.Arrays;
+
 public class RandomArrayPlusTwo {
     public static void main(String[] args) {
         int[] numbers = new int[3];
         for(int i = 0; i < numbers.length; i++) {
-            numbers[i] = ((int)(Math.random() * 100));
-            System.out.println(numbers[i]);
+            numbers[i] = ((int) (Math.random() * 100));
         }
-        System.out.println("_________________________________");
-        System.out.println((numbers[0] + 2));
-        System.out.println((numbers[1] + 2));
-        System.out.println((numbers[2] + 2));
+            System.out.println(Arrays.toString(numbers));
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = numbers[i] + 2;
+            }
+        System.out.println(Arrays.toString(numbers));
+        }
     }
-}
