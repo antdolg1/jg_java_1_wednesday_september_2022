f0818848b3952c16eedf7300b89e391d55757950
java0711 <114607998+java0711@users.noreply.github.com>
2022-12-19 22:00:29 +0200

    Homework of lesson 9, level 3

---
+++ src/main/java/students/student_svetlana_seda/lesson_9/homework/level_3/task_13/Car.java
@@ -0,0 +1,5 @@
+package students.student_svetlana_seda.lesson_9.homework.level_3.task_13;
+
+abstract class Car {
+    abstract void accelerate();
+}
---
+++ src/main/java/students/student_svetlana_seda/lesson_9/homework/level_3/task_13/ElectricCar.java
@@ -0,0 +1,12 @@
+package students.student_svetlana_seda.lesson_9.homework.level_3.task_13;
+
+class ElectricCar extends Car {
+
+    private int batteryCharge;
+
+    @Override
+    void accelerate() {
+
+    }
+}
+
